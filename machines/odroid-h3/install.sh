a03b5eaf7ba88cfbdf49bf05fea03b95ebe0a19b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-20 10:19:22 +0200

    fix: pass install arguments to common install script

--- machines/odroid-h3/install.sh
+++ machines/odroid-h3/install.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
+# shellcheck disable=SC2068
+
 set -euf
 cd "$(dirname "$0")"
 
 install_script_path="$(git rev-parse --show-toplevel)/.utils/install.sh"
 
 SOURCE_DIR="$(dirname "$0")/smtp4dev" \
-    bash "$install_script_path"
+    bash "$install_script_path" $@
 
 SOURCE_DIR="$(dirname "$0")/healthchecks" \
-    bash "$install_script_path"
+    bash "$install_script_path" $@
 
 SOURCE_DIR="$(dirname "$0")/unifi-network-application" \
-    bash "$install_script_path"
+    bash "$install_script_path" $@
