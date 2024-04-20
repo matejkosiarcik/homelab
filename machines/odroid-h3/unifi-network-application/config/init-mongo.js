574f844648157b5b693889b5ff2d017f45137dd6
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-20 21:53:52 +0200

    fix: mongodb password filepath

--- machines/odroid-h3/unifi-network-application/config/init-mongo.js
+++ machines/odroid-h3/unifi-network-application/config/init-mongo.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const dbName = 'unifi';
 const dbName2 = 'unifi_stat';
 const user = 'unifi';
-const password = fs.readFileSync('/.config/mongodb-password.txt', 'utf8').trim();
+const password = fs.readFileSync('/.homelab/mongodb-password.txt', 'utf8').trim();
 
 // This differs a little from the official README, because of authorization problems with the _stat database when using mongo's --auth
 // See discussion at https://github.com/linuxserver/docker-unifi-network-application/issues/29
