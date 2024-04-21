ec99320bfd85c0547b84dfcc3c32ecaa80149219
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-21 19:21:00 +0200

    chore: fix shellcheck errors

--- machines/raspberrypi-3b/pi-hole/backup/entrypoint.sh
+++ machines/raspberrypi-3b/pi-hole/backup/entrypoint.sh
@@ -5,9 +5,10 @@ crontab /app/schedule.cron
 
 rm -f /log/cron.log
 touch /log/cron.log
-printf '-- Container startup --\n' >>/log/cron.log
-sh /app/run.sh >>/log/cron.log 2>&1
-
-printf '-- Starting cron --\n' >>/log/cron.log
+{
+    printf '-- Container startup --\n'
+    sh /app/run.sh 2>&1
+    printf '-- Starting cron --\n'
+} >>/log/cron.log
 crond -b -L /log/cron.log # Add `-l 0` when debugging (note: alpine)
 tail -f /log/cron.log
