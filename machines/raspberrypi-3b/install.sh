12c469a7dbf8e3090074d2cae9f55b0de3831a90
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-21 19:07:14 +0200

    chore: revert raspi-3 install to real services

--- machines/raspberrypi-3b/install.sh
+++ machines/raspberrypi-3b/install.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$0")"
 
 install_script_path="$(git rev-parse --show-toplevel)/.utils/install.sh"
 
-# SOURCE_DIR="$(dirname "$0")/pi-hole" \
-#     bash "$install_script_path" $@
-
-SOURCE_DIR="$(dirname "$0")/debug" \
+SOURCE_DIR="$(dirname "$0")/pi-hole" \
     bash "$install_script_path" $@
+
+# SOURCE_DIR="$(dirname "$0")/debug" \
+#     bash "$install_script_path" $@
