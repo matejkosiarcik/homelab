a774bddae53934986573eb93788e0d96a6fd27aa
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-21 19:23:10 +0200

    chore: fix shellcheck errors

--- machines/odroid-h3/healthchecks/backup/entrypoint.sh
+++ machines/odroid-h3/healthchecks/backup/entrypoint.sh
@@ -6,9 +6,9 @@ crontab /app/schedule.cron
 rm -f /log/cron.log
 touch /log/cron.log
 {
-    printf '-- Container startup --\n'
+    printf '## Container startup ##\n'
     sh /app/run.sh 2>&1
-    printf '-- Starting cron --\n'
+    printf '## Starting cron ##\n'
 } >>/log/cron.log
 crond -b -L /log/cron.log # Add `-l 0` when debugging (note: alpine)
 tail -f /log/cron.log
