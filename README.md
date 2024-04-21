4c740c18e1884f72f3680f1c7766df17aa2716cf
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-21 19:00:10 +0200

    chore: fix lint problems

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ Below is a general format for this repository:
 
 - `/guides/` - General guides for things that are done once in a while, such as installing new machine
 - `/machines/` - Everything that should run on given machines
-  - `/[machine...]/` - All config specific for individual machine
-    - `/install.sh` - Install script
-    - `/startup.sh` - Startup script (should be run via cron at `@reboot`)
-    - `/[service...]/` - All config for individual service
-      - `/docker-compose*.yml` - Docker Compose files
-      - `/config/` - Directory for public config files and subdirectories
-      - `/private/` - Directory for private mount data (such as passwords)
-      - `/data/` - Directory for runtime data (contains subdirectories)
+    - `/[machine...]/` - All config specific for individual machine
+        - `/install.sh` - Install script
+        - `/startup.sh` - Startup script (should be run via cron at `@reboot`)
+        - `/[service...]/` - All config for individual service
+            - `/docker-compose*.yml` - Docker Compose files
+            - `/config/` - Directory for public config files and subdirectories
+            - `/private/` - Directory for private mount data (such as passwords)
+            - `/data/` - Directory for runtime data (contains subdirectories)
--- machines/odroid-h3/smtp4dev/debug_email.py
+++ machines/odroid-h3/smtp4dev/debug_email.py
@@ -12,17 +12,17 @@ from typing import List
 def main(argv: List[str]) -> int:
     # Parse arguments
     parser = argparse.ArgumentParser(prog="debug-email.py")
-    parser.add_argument("--host", type=str, default='localhost:8025', help="Host IP:Port or Domain:Port")
+    parser.add_argument("--host", type=str, default="localhost:8025", help="Host IP:Port or Domain:Port")
     args = parser.parse_args(argv)
 
     host = args.host
 
-    address_from = 'sender@test.test'
-    address_to = 'receiver@test.test'
-    subject = 'Test subject'
+    address_from = "sender@test.test"
+    address_to = "receiver@test.test"
+    subject = "Test subject"
 
     # Disabled because smtp4dev doesn't show Plain text emails well
-    # message_raw = f"""from: {address_from}\nto: {address_to}\nsubject: {subject}\n\nTest body line 1\nTest body line 2"""
+    # message_raw = f"from: {address_from}\nto: {address_to}\nsubject: {subject}\n\nTest body line 1\nTest body line 2"
     # with SMTP(host) as smtp:
     #     smtp.sendmail(
     #         from_addr=address_from,
@@ -31,11 +31,11 @@ def main(argv: List[str]) -> int:
     #     )
 
     # message = Message()
-    message =  MIMEMultipart('alternative')
-    message['From'] = address_from
-    message['To'] = address_to
-    message['Subject'] = subject
-    message.attach(MIMEText('Test body line 1<br>Test body line 2', 'html'))
+    message =  MIMEMultipart("alternative")
+    message["From"] = address_from
+    message["To"] = address_to
+    message["Subject"] = subject
+    message.attach(MIMEText("Test body line 1<br>Test body line 2", "html"))
 
     with SMTP(host) as smtp:
         smtp.send_message(message)
