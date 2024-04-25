f2caea1c2732b29ae95d42eb2868fe3c76d40124
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-25 20:19:47 +0200

    feat: add omada-controller to install script

--- machines/odroid-h3/install.sh
+++ machines/odroid-h3/install.sh
@@ -6,13 +6,16 @@ cd "$(dirname "$0")"
 
 install_script_path="$(git rev-parse --show-toplevel)/.utils/install.sh"
 
-SOURCE_DIR="$(dirname "$0")/smtp4dev" \
-    bash "$install_script_path" $@
-
 SOURCE_DIR="$(dirname "$0")/healthchecks" \
     bash "$install_script_path" $@
 
 SOURCE_DIR="$(dirname "$0")/homer" \
+    bash "$install_script_path" $@
+
+SOURCE_DIR="$(dirname "$0")/omada-controller" \
+    bash "$install_script_path" $@
+
+SOURCE_DIR="$(dirname "$0")/smtp4dev" \
     bash "$install_script_path" $@
 
 SOURCE_DIR="$(dirname "$0")/unifi-network-application" \
