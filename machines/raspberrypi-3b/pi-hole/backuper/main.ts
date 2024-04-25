cab7e96d5ee430ed507b89596ef680532784d311
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-25 20:52:29 +0200

    feat: update pi-hole backuper script

--- machines/raspberrypi-3b/pi-hole/backuper/main.ts
+++ machines/raspberrypi-3b/pi-hole/backuper/main.ts
@@ -2,45 +2,36 @@ import fs from 'fs/promises';
 import fsSync from 'fs';
 import path from 'path';
 import process from 'process';
-import { chromium } from 'playwright';
 import dotenv from 'dotenv';
+import { chromium } from 'playwright';
 import { expect } from 'playwright/test';
 
 (async () => {
+    // Env config
     if (fsSync.existsSync('.env')) {
         dotenv.config({ path: '.env' });
     }
+    const backupDir = process.env['BACKUP_DIR'] || (fsSync.existsSync('/.dockerenv') ? '/backup' : './data');
+    const browserPath = process.env['BROWSER_PATH'] || (fsSync.existsSync('/.dockerenv') ? '/usr/bin/chromium' : undefined);
+    const url = process.env['URL'] || 'http://localhost:8080';
+    const headless = process.env['HEADLESS'] !== '0';
+    const password = process.env['PASSWORD']!;
+    expect(password, 'PASSWORD unset').toBeTruthy();
 
-    const backupDir = process.env['BACKUP_DIR'] || '/backup';
-    const browserPath = process.env['BROWSER_PATH'] || undefined;
-
-    const url = process.env['URL'];
-    if (!url) {
-        throw new Error('URL unset');
-    }
-
-    const password = process.env['PASSWORD'];
-    if (!password) {
-        throw new Error('PASSWORD unset');
-    }
-
-    if (!fsSync.existsSync(backupDir)) {
-        await fs.mkdir(backupDir, { recursive: true });
-    }
     const backupDate = new Date().toISOString().replaceAll(':', '-').replaceAll('T', '_').replace(/\..+$/, '');
+    await fs.mkdir(backupDir, { recursive: true });
 
-    const browser = await chromium.launch({ headless: true, executablePath: browserPath });
+    const browser = await chromium.launch({ headless: headless, executablePath: browserPath });
     try {
         const page = await browser.newPage({ baseURL: url, strictSelectors: true, ignoreHTTPSErrors: true });
         page.setDefaultNavigationTimeout(10_000);
-        page.setDefaultTimeout(2000);
-        await page.goto('/admin/settings.php');
+        page.setDefaultTimeout(1000);
+        await page.goto('/admin/login.php');
 
         // Login
-        await page.waitForURL(/login/);
         await page.locator('form#loginform input#loginpw').fill(password);
         await page.locator('form#loginform button[type="submit"]').click();
-        await page.waitForURL('/admin/settings.php');
+        await page.waitForURL('/admin/index.php');
 
         // Navigate to proper place in settings
         await page.goto('/admin/settings.php?tab=teleporter');
@@ -51,11 +42,8 @@ import { expect } from 'playwright/test';
 
         // Handle download
         const download = await downloadPromise;
-        if (!/\.tar\.gz$/.test(download.suggestedFilename())) {
-            throw new Error(`Unknown extension for downloaded file: ${download.suggestedFilename()}`);
-        }
-        const downloadPath = path.join(backupDir, download.suggestedFilename().replace(/^.+(\.tar\.gz)$/, `${backupDate}-settings$1`));
-        await download.saveAs(downloadPath);
+        expect(download.suggestedFilename(), `Unknown extension for downloaded file: ${download.suggestedFilename()}`).toMatch(/\.tar\.gz/);
+        await download.saveAs(path.join(backupDir, `${backupDate}.tar.gz`));
     } finally {
         await browser.close();
     }
