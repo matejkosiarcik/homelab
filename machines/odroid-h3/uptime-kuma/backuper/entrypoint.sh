24761397544912612288cc6963161b48f79e3050
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-26 17:18:20 +0200

    feat: add uptime-kuma backuper scripts

---
+++ machines/odroid-h3/uptime-kuma/backuper/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -euf
+
+crontab /app/schedule.cron
+
+rm -f /log/cron.log
+touch /log/cron.log
+{
+    printf '## Container startup ##\n'
+    sh /app/run.sh 2>&1
+    printf '## Starting cron ##\n'
+} >>/log/cron.log
+crond -b -L /log/cron.log # Add `-l 0` when debugging (note: alpine)
+tail -f /log/cron.log
