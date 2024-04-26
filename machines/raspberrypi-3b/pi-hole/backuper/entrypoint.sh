65f9237b8f6ce7aa40b3ec49c90cc0b29e1ba0f7
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-26 21:00:57 +0200

    feat: update custom docker entrypoints

--- machines/raspberrypi-3b/pi-hole/backuper/entrypoint.sh
+++ machines/raspberrypi-3b/pi-hole/backuper/entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 set -euf
 
-crontab /app/schedule.cron
+rm -rf /app/.internal
+mkdir -p /app/.internal
 
 rm -f /log/cron.log
 touch /log/cron.log
+crontab /app/schedule.cron
+
+# Run script on startup
 {
     printf '## Container startup ##\n'
     sh /app/run.sh 2>&1
     printf '## Starting cron ##\n'
 } >>/log/cron.log
+
 crond -b -L /log/cron.log # Add `-l 0` when debugging (note: alpine)
+printf '1\n' >/app/.internal/status
 tail -F /log/cron.log
