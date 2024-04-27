e9d40d697422021e164a76b145feab27d2b3bb8d
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-04-27 15:38:56 +0200

    feat: extend startup scripts

--- machines/odroid-h3/startup.sh
+++ machines/odroid-h3/startup.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 set -euf
+
+# Add macvlan-shim "router" to be able to access containers from host
+# sudo ip link add macvlan-shim link eth0 type macvlan mode bridge
+# sudo ip addr add 10.1.6.x/32 dev macvlan-shim
+# sudo ip link set macvlan-shim up
+# sudo ip route add 10.1.11.0/24 dev macvlan-shim
