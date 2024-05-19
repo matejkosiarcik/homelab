ef1d4d80f82b2835011b7c8d9b181023067b32f0
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-05-19 23:17:02 +0200

    fix: logfile filename

--- .utils/preinstall-all.sh
+++ .utils/preinstall-all.sh
@@ -31,7 +31,7 @@ done
 # Default deployment location is "~/homelab"
 # Can be overriden by setting "DEST_DIR=..."
 dest_dir="${DEST_DIR-$HOME/homelab}"
-global_log_dir="$dest_dir/.log/$(date +"%Y-%m-%d_%H-%M-%S")"
+global_log_dir="$dest_dir/.log/$(date +"%Y-%m-%d_%H-%M-%S").txt"
 global_log_file="$global_log_dir/install.txt"
 mkdir -p "$dest_dir" "$global_log_dir"
 
