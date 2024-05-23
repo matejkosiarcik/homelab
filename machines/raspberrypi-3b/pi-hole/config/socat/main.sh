f0aa12e4d783420f2c57940a1528015a54fefb7e
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-05-23 20:04:21 +0200

    chore: split pihole socat entrypoint

--- machines/raspberrypi-3b/pi-hole/config/socat/main.sh
+++ machines/raspberrypi-3b/pi-hole/config/socat/main.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 set -euf
 
-rm -rf /app/.internal
-mkdir -p /app/.internal
-printf 'starting\n' >/app/.internal/status
-
 socat TCP-LISTEN:80,fork TCP:pihole-http-proxy:80 &
 socat TCP-LISTEN:443,fork TCP:pihole-http-proxy:443 &
 socat TCP-LISTEN:53,fork TCP:pihole-app:53 &
 socat UDP-LISTEN:53,fork UDP:pihole-app:53 &
-
-printf 'started\n' >/app/.internal/status
-sleep infinity
