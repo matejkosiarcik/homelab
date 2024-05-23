943a907fcf9cabaa1f6f84af9d13a48a5a47473b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-05-23 19:55:21 +0200

    chore: rename pihole-proxy -> pihole-http-proxy

--- machines/raspberrypi-3b/pi-hole/config/socat/entrypoint.sh
+++ machines/raspberrypi-3b/pi-hole/config/socat/entrypoint.sh
@@ -5,8 +5,8 @@ rm -rf /app/.internal
 mkdir -p /app/.internal
 printf 'starting\n' >/app/.internal/status
 
-socat TCP-LISTEN:80,fork TCP:pihole-proxy:80 &
-socat TCP-LISTEN:443,fork TCP:pihole-proxy:443 &
+socat TCP-LISTEN:80,fork TCP:pihole-http-proxy:80 &
+socat TCP-LISTEN:443,fork TCP:pihole-http-proxy:443 &
 socat TCP-LISTEN:53,fork TCP:pihole-app:53 &
 socat UDP-LISTEN:53,fork UDP:pihole-app:53 &
 
