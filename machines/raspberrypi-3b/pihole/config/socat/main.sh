fcb664cb34cbbe5d847d111fbb008df4c1ea3381
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-05-24 22:25:10 +0200

    chore: try to fix socat fork issues

--- machines/raspberrypi-3b/pihole/config/socat/main.sh
+++ machines/raspberrypi-3b/pihole/config/socat/main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euf
 
-socat TCP-LISTEN:80,fork TCP:pihole-http-proxy:80 &
-socat TCP-LISTEN:443,fork TCP:pihole-http-proxy:443 &
-socat TCP-LISTEN:53,fork TCP:pihole-app:53 &
-socat UDP-LISTEN:53,fork UDP:pihole-app:53 &
+socat TCP-LISTEN:80,fork,reuseaddr TCP:pihole-http-proxy:80 &
+socat TCP-LISTEN:443,fork,reuseaddr TCP:pihole-http-proxy:443 &
+socat TCP-LISTEN:53,fork,reuseaddr TCP:pihole-app:53 &
+socat UDP-LISTEN:53,fork,reuseaddr UDP:pihole-app:53 &
