ba561459519e2e8fa4261a5b444a9430c3e71eda
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-05-25 10:26:00 +0200

    chore: debug pihole socat IPs

--- machines/raspberrypi-3b/pihole/config/socat/main.sh
+++ machines/raspberrypi-3b/pihole/config/socat/main.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -euf
 
-socat TCP-LISTEN:80,fork,reuseaddr TCP:pihole-http-proxy:80 &
-socat TCP-LISTEN:443,fork,reuseaddr TCP:pihole-http-proxy:443 &
-socat TCP-LISTEN:53,fork,reuseaddr TCP:pihole-app:53 &
-socat UDP-LISTEN:53,fork,reuseaddr UDP:pihole-app:53 &
+# socat TCP-LISTEN:80,fork,reuseaddr TCP:pihole-http-proxy:80 &
+# socat TCP-LISTEN:443,fork,reuseaddr TCP:pihole-http-proxy:443 &
+# socat TCP-LISTEN:53,fork,reuseaddr TCP:pihole-app:53 &
+# socat UDP-LISTEN:53,fork,reuseaddr UDP:pihole-app:53 &
+
+socat TCP-LISTEN:80,fork,reuseaddr TCP:10.1.10.3:80 &
+socat TCP-LISTEN:443,fork,reuseaddr TCP:10.1.10.3:443 &
+socat TCP-LISTEN:53,fork,reuseaddr TCP:10.1.10.2:53 &
+socat UDP-LISTEN:53,fork,reuseaddr UDP:10.1.10.2:53 &
