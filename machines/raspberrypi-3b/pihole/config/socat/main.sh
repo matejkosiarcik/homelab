dd2ecddb19b69409ebe9fe8d8f9489ad171288bb
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-05-25 16:23:10 +0200

    feat: revert temporary pihole production changes

--- machines/raspberrypi-3b/pihole/config/socat/main.sh
+++ machines/raspberrypi-3b/pihole/config/socat/main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euf
 
-socat TCP4-LISTEN:80,fork,reuseaddr TCP4:10.1.10.3:80 &
-socat TCP4-LISTEN:443,fork,reuseaddr TCP4:10.1.10.3:443 &
-socat TCP4-LISTEN:53,fork,reuseaddr TCP4:10.1.10.2:53 &
-socat -T5 UDP4-LISTEN:53,fork,reuseaddr UDP4:10.1.10.2:53 &
+socat TCP4-LISTEN:80,fork,reuseaddr TCP4:pihole-http-proxy:80 &
+socat TCP4-LISTEN:443,fork,reuseaddr TCP4:pihole-http-proxy:443 &
+socat TCP4-LISTEN:53,fork,reuseaddr TCP4:pihole-app:53 &
+socat -T5 UDP4-LISTEN:53,fork,reuseaddr UDP4:pihole-app:53 &
