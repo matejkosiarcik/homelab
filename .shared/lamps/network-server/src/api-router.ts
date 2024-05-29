8994cf5d283def1874b430b465e3def5acb5e68f
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-05-29 20:16:28 +0200

    feat: watch for status in lamps http server

--- .shared/lamps/network-server/src/api-router.ts
+++ .shared/lamps/network-server/src/api-router.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import { lastStatus } from './status-watch.ts';
 
 export const apiRouter = express.Router();
 
 apiRouter.get('/status', async (_, response, next) => {
     try {
         const output = {
-            status: "off",
+            status: lastStatus ? 'on' : 'off',
         };
 
         response.status(200);
