c61e536a4372a71567ba9477473b715d37d5d4b5
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-05-31 23:00:13 +0200

    style: fix prettier

--- .shared/lamps/network-server/frontend/App.vue
+++ .shared/lamps/network-server/frontend/App.vue
@@ -1,33 +1,35 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { getStatus, changeStatus } from './utils/api.ts';
+import { ref } from "vue";
+import { getStatus, changeStatus } from "./utils/api.ts";
 
 const data = {
-    status: ref(false),
+  status: ref(false),
 };
 
 function setStatus(status: boolean) {
-    data.status.value = status;
-    document.title = `${data.status.value ? '⚡️' : '⏻'} Lamp ${data.status.value ? 'on ⚡️' : 'off ⏻'}`;
+  data.status.value = status;
+  document.title = `${data.status.value ? "⚡️" : "⏻"} Lamp ${
+    data.status.value ? "on ⚡️" : "off ⏻"
+  }`;
 }
 
 async function toggleButton() {
-    const newStatus = await changeStatus(!data.status.value);
-    setStatus(newStatus);
+  const newStatus = await changeStatus(!data.status.value);
+  setStatus(newStatus);
 }
 
 (async () => {
-    const initialStatus = await getStatus();
-    setStatus(initialStatus);
+  const initialStatus = await getStatus();
+  setStatus(initialStatus);
 })();
 </script>
 
 <template>
-    <main class="container">
-        <p>{{ data.status.value ? '⚡️ On ⚡️' : '💤 Off 💤' }}</p>
-        <button class="toggle-button" @click="toggleButton">
-            {{ data.status.value ? 'Off' : 'On' }}
-        </button>
-        <p>&nbsp;</p>
-    </main>
+  <main class="container">
+    <p>{{ data.status.value ? "⚡️ On ⚡️" : "💤 Off 💤" }}</p>
+    <button class="toggle-button" @click="toggleButton">
+      {{ data.status.value ? "Off" : "On" }}
+    </button>
+    <p>&nbsp;</p>
+  </main>
 </template>
