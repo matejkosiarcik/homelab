94a3ec32aa8cbf32359c3e9acbf5521305c0551d
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-06-14 21:55:33 +0200

    chore: fix prettier errors

--- components/lamps/network-server/frontend/App.vue
+++ components/lamps/network-server/frontend/App.vue
@@ -42,7 +42,11 @@ async function toggleButton() {
 <template>
   <main class="container">
     <p>{{ data.initialLoading.value ? '&nbsp;' : data.status.value ? '⚡️ On ⚡️' : '💤 Off 💤' }}</p>
-    <button class="toggle-button" :class="{ no_disabled: data.initialLoading.value }" @click="toggleButton" ref="button"
+    <button
+      class="toggle-button"
+      :class="{ no_disabled: data.initialLoading.value }"
+      @click="toggleButton"
+      ref="button"
       disabled>
       {{ data.initialLoading.value ? '&nbsp;' : data.status.value ? 'Off' : 'On' }}
     </button>
