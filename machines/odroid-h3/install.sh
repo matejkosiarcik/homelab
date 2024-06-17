9b1b33accb14c5b075c37bf6aa57211abd138874
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-06-17 18:33:57 +0200

    feat: unify install scripts

--- machines/odroid-h3/install.sh
+++ machines/odroid-h3/install.sh
@@ -1,32 +1,9 @@
 #!/bin/sh
 # shellcheck disable=SC2068
-
 set -euf
 
-# Prepare destination directory
+current_machine_dir="$(cd "$(dirname "$0")" && printf '%s\n' "$PWD")"
+export current_machine_dir
 
-bash "$(git rev-parse --show-toplevel)/.utils/preinstall-all.sh" $@
-
-# Install individual services
-# Note: Services ordered by priority and dependence on each other
-
-currdir="$(cd "$(dirname "$0")" && printf '%s\n' "$PWD")"
-install_script_path="$(git rev-parse --show-toplevel)/.utils/install-service.sh"
-
-SOURCE_DIR="$currdir/smtp4dev" \
-    bash "$install_script_path" $@
-
-SOURCE_DIR="$currdir/homer" \
-    bash "$install_script_path" $@
-
-SOURCE_DIR="$currdir/healthchecks" \
-    bash "$install_script_path" $@
-
-SOURCE_DIR="$currdir/uptime-kuma" \
-    bash "$install_script_path" $@
-
-SOURCE_DIR="$currdir/omada-controller" \
-    bash "$install_script_path" $@
-
-SOURCE_DIR="$currdir/unifi-controller" \
-    bash "$install_script_path" $@
+# shellcheck disable=SC2068
+bash "$(git rev-parse --show-toplevel)/.utils/install-machine.sh" $@
