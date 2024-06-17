de867200641b544688b3ee5b4435fae5974ae782
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-06-17 18:52:59 +0200

    chore: fix lint errors

--- .utils/install-machine.sh
+++ .utils/install-machine.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2068
 set -eufo pipefail
 # The reason to use `bash` instead of plain `sh` is that we require pipefail
 
@@ -16,5 +17,5 @@ bash "$(git rev-parse --show-toplevel)/.utils/preinstall-all.sh" $@
 
 ### Install all individual services ###
 
-find "$current_machine_dir" -mindepth 1 -maxdepth 1 -type d -not -name '.*' -print0 |\
+find "$current_machine_dir" -mindepth 1 -maxdepth 1 -type d -not -name '.*' -print0 |
     xargs -0 -n1 -J% bash "$(git rev-parse --show-toplevel)/.utils/install-service.sh" % $@
