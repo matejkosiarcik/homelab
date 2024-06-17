fa9a2bec574db1562c587a2a612d5fee0e3467a2
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-06-17 18:55:37 +0200

    chore: fix install script

--- .utils/install-machine.sh
+++ .utils/install-machine.sh
@@ -18,4 +18,4 @@ bash "$(git rev-parse --show-toplevel)/.utils/preinstall-all.sh" $@
 ### Install all individual services ###
 
 find "$current_machine_dir" -mindepth 1 -maxdepth 1 -type d -not -name '.*' -print0 |
-    xargs -0 -n1 -J% bash "$(git rev-parse --show-toplevel)/.utils/install-service.sh" % $@
+    xargs -0 -n1 -J% bash -c "SOURCE_DIR=% bash "$(git rev-parse --show-toplevel)/.utils/install-service.sh" $@"
