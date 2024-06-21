9b515359dbfd1adc275eb7161774c007acb2288a
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-06-21 19:28:50 +0200

    feat: use reusable shared startup scripts

--- machines/odroid-h3/startup.sh
+++ machines/odroid-h3/startup.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -euf
 
-# Add macvlan-shim "router" to be able to access containers from host
-# sudo ip link add macvlan-shim link eth0 type macvlan mode bridge
-# sudo ip addr add 10.1.6.x/32 dev macvlan-shim
-# sudo ip link set macvlan-shim up
-# sudo ip route add 10.1.11.0/24 dev macvlan-shim
+git_dir="$(cd "$(dirname "$0")" && git rev-parse --show-toplevel)"
+
+sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.x 10.1.11.0
