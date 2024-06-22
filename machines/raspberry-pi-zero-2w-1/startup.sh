573f8a29513f16a825ea6c499178ccbced59cc2e
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-06-22 23:07:31 +0200

    feat: enable swap on raspi zero

--- machines/raspberry-pi-zero-2w-1/startup.sh
+++ machines/raspberry-pi-zero-2w-1/startup.sh
@@ -3,5 +3,5 @@ set -euf
 
 git_dir="$(cd "$(dirname "$0")" && git rev-parse --show-toplevel)"
 
-sh "$git_dir/.utils/startup/disable-swap.sh"
+# sh "$git_dir/.utils/startup/disable-swap.sh"
 sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.6 10.1.16.0
