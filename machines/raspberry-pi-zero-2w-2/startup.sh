7cbedfd20f8c5cd57b44ecfc697d358e88bcc1cd
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-13 09:38:38 +0200

    docs: add comment why disable-swap is skipped on RPi zeros

--- machines/raspberry-pi-zero-2w-2/startup.sh
+++ machines/raspberry-pi-zero-2w-2/startup.sh
@@ -3,5 +3,7 @@ set -euf
 
 git_dir="$(cd "$(dirname "$0")" && git rev-parse --show-toplevel)"
 
-# sh "$git_dir/.utils/startup/disable-swap.sh"
+# Disable-Swap is skipped, because with it Pi frequently freezes during operation
+# Mainly during heavy load, such as docker-builds
+
 sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.7 10.1.17.0
