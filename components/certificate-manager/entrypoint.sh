4cac6eecff35a4a1ffd5f98a78d95c2fdc98d19c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-22 22:29:32 +0200

    feat: add new component for creating certificates periodically

---
+++ components/certificate-manager/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -euf
+
+rm -rf /app/.internal
+mkdir -p /app/.internal
+printf 'starting\n' >/app/.internal/status
+
+if [ -e '/log/cron.log' ]; then
+    rm -f /log/cron.log
+fi
+touch /log/cron.log
+
+# Run script initially
+CRON='0'
+export CRON
+printf 'HOST=%s\n' "$HOST" >>/app/.internal/cron.env
+printf 'ENV=%s\n' "$ENV" >>/app/.internal/cron.env
+sh /app/cron-wrapper.sh
+printf 'CRON=1\n' "$ENV" >>/app/.internal/cron.env
+
+# Run cron indefintely
+crontab /app/crontab.cron
+cron -L 15
+printf 'started\n' >/app/.internal/status
+tail -F /log/cron.log
