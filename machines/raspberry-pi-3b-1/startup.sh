4e2e2796b1136d70691ecb01a0011570102f2cc8
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-23 20:29:41 +0200

    chore: debug docker networks

--- machines/raspberry-pi-3b-1/startup.sh
+++ machines/raspberry-pi-3b-1/startup.sh
@@ -4,7 +4,7 @@ set -euf
 git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel)"
 
 sh "$git_dir/.utils/startup/disable-swap.sh"
-sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.4 10.1.10.0
+# sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.4 10.1.10.0
 # sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.104 10.1.10.0
 # sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.105 10.1.10.0
 
@@ -12,10 +12,10 @@ router_name="macvlan-shim2"
 sudo ip link add "$router_name" link eth0 type macvlan mode bridge
 sudo ip addr add "10.1.6.104/32" dev "$router_name"
 sudo ip link set "$router_name" up
-sudo ip route add "10.1.10.0/24" dev "$router_name"
+sudo ip route add "10.1.10.0/30" dev "$router_name"
 
 router_name="macvlan-shim3"
 sudo ip link add "$router_name" link eth0 type macvlan mode bridge
 sudo ip addr add "10.1.6.105/32" dev "$router_name"
 sudo ip link set "$router_name" up
-sudo ip route add "10.1.10.0/24" dev "$router_name"
+sudo ip route add "10.1.10.128/30" dev "$router_name"
