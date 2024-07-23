d1f6f1ef3a1602562d72f32fd43c9707d9a681b5
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-23 20:14:31 +0200

    chore: debug docker networks

--- machines/raspberry-pi-3b-1/startup.sh
+++ machines/raspberry-pi-3b-1/startup.sh
@@ -5,3 +5,5 @@ git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse -
 
 sh "$git_dir/.utils/startup/disable-swap.sh"
 sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.4 10.1.10.0
+sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.104 10.1.10.0
+sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.105 10.1.10.0
