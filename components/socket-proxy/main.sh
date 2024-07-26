b2e1918d2bd9e153a7e505de497c001558d9481c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-26 22:00:31 +0200

    refactor: rename variable

--- components/socket-proxy/main.sh
+++ components/socket-proxy/main.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 set -euf
 
-service="${HOMELAB_SERVICE-x}"
-
-if [ "$service" = 'pihole' ]; then
+if [ "${HOMELAB_SERVICE-x}" = 'pihole' ]; then
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:pihole-http-proxy:80 &
     socat TCP4-LISTEN:443,fork,reuseaddr TCP4:pihole-http-proxy:443 &
     socat TCP4-LISTEN:53,fork,reuseaddr TCP4:pihole-app:53 &
     socat -T5 UDP4-LISTEN:53,fork,reuseaddr UDP4:pihole-app:53 &
-elif [ "$service" = 'pihole2' ]; then
+elif [ "${HOMELAB_SERVICE-x}" = 'pihole2' ]; then
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:pihole2-http-proxy:80 &
     socat TCP4-LISTEN:443,fork,reuseaddr TCP4:pihole2-http-proxy:443 &
     socat TCP4-LISTEN:53,fork,reuseaddr TCP4:pihole2-app:53 &
     socat -T5 UDP4-LISTEN:53,fork,reuseaddr UDP4:pihole2-app:53 &
-elif [ "$service" = 'smtp4dev' ]; then
+elif [ "${HOMELAB_SERVICE-x}" = 'smtp4dev' ]; then
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:smtp4dev-http-proxy:80 &
     socat TCP4-LISTEN:443,fork,reuseaddr TCP4:smtp4dev-http-proxy:443 &
     socat TCP4-LISTEN:25,fork,reuseaddr TCP4:smtp4dev-app:25 &
-elif [ "$service" = 'homer' ]; then
+elif [ "${HOMELAB_SERVICE-x}" = 'homer' ]; then
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:homer-http-proxy:80 &
     socat TCP4-LISTEN:443,fork,reuseaddr TCP4:homer-http-proxy:443 &
-elif [ "$service" = 'lamp-controller' ]; then
+elif [ "${HOMELAB_SERVICE-x}" = 'lamp-controller' ]; then
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:lamp-controller-http-proxy:80 &
     socat TCP4-LISTEN:443,fork,reuseaddr TCP4:lamp-controller-http-proxy:443 &
 else
-    printf 'Unknown service "%s"\n' "$service" >&2
+    printf 'Unknown service "%s"\n' "${HOMELAB_SERVICE-UNKNOWN}" >&2
     exit 1
 fi
