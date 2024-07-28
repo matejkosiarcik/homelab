729a12bd285175846107e1078286b14557fd50f3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-28 15:39:36 +0200

    chore: adapt pihole script for new filepaths

--- machines/raspberry-pi-3b-1/install.sh
+++ machines/raspberry-pi-3b-1/install.sh
@@ -6,4 +6,4 @@ current_machine_dir="$(cd "$(dirname "$0")" && printf '%s\n' "$PWD")"
 export current_machine_dir
 
 # shellcheck disable=SC2068
-bash "$(git rev-parse --show-toplevel)/utils/install-machine.sh" $@
+bash "$(git rev-parse --show-toplevel)/utils/deployment-helpers/install-machine.sh" $@
