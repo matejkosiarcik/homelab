316b170c8cae8ce9d302271fbb19c021ec3d59ca
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-28 15:35:03 +0200

    chore: change filepaths inside init utils

--- machines/raspberry-pi-3b-1/pihole-main/init.sh
+++ machines/raspberry-pi-3b-1/pihole-main/init.sh
@@ -4,4 +4,4 @@ set -euf
 cd "$(dirname "$0")"
 
 # shellcheck disable=SC2068
-sh "$(git rev-parse --show-toplevel)/utils/init/apps/pihole/preinit.sh" $@
+sh "$(git rev-parse --show-toplevel)/utils/app-init/pihole/preinit.sh" $@
