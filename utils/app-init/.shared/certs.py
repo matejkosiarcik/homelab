1f78f340458971c140298e35eadb2895e899014a
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-28 15:43:37 +0200

    chore: update URLs for pihole

--- utils/app-init/.shared/certs.py
+++ utils/app-init/.shared/certs.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     dir_output = args.output
     os.makedirs(dir_output, exist_ok=True)
 
-    domain = "*.home"  # TODO: Pass domain in argument
+    domain = "*.home" # TODO: Pass domain in argument
     # TODO: Can you put multiple domains here? What is the syntax?
-    # Something like "pihole.home,*.pihole.home"
+    # Something like "pihole-main.home,*.pihole-main.home"
     openssl_subj = f"/C=SK/ST=Slovakia/L=Bratislava/O=Unknown/OU=Org/CN={domain}"
 
     subprocess.check_call(["openssl", "genrsa", "-out", path.join(dir_output, "server.key"), "4096"])
