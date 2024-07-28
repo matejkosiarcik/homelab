d68ac78b08374ec229fd65d28500d2ccd5dff411
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-28 15:33:47 +0200

    chore: adapt paths

--- machines/raspberry-pi-3b-1/pihole-main/preinit.sh
+++ machines/raspberry-pi-3b-1/pihole-main/preinit.sh
@@ -4,4 +4,4 @@ set -euf
 cd "$(dirname "$0")"
 
 # shellcheck disable=SC2068
-sh "$(git rev-parse --show-toplevel)/.utils/preinit/apps/pihole/preinit.sh" $@
+sh "$(git rev-parse --show-toplevel)/utils/preinit/apps/pihole/preinit.sh" $@
