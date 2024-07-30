289a6f1f232f4a1fec9b02e71bc66fe9a7b27ee9
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-30 22:03:00 +0200

    feat: update IPs for macvlan routers

--- machines/raspberry-pi-3b-1/startup.sh
+++ machines/raspberry-pi-3b-1/startup.sh
@@ -4,5 +4,4 @@ set -euf
 git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel)"
 
 sh "$git_dir/utils/machine-startup/disable-swap.sh"
-sh "$git_dir/utils/machine-startup/macvlan-router.sh" macvlan-shim-1 10.1.6.201 10.1.10.1
-sh "$git_dir/utils/machine-startup/macvlan-router.sh" macvlan-shim-2 10.1.6.202 10.1.10.2
+sh "$git_dir/utils/machine-startup/macvlan-router.sh" macvlan-shim-1 10.1.11.1 10.1.10.1
