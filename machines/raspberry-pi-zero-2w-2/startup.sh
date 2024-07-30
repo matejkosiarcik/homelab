b8aab44238d65ab2caa8a94b0e2d077b04a3f338
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-07-30 22:05:03 +0200

    docs: update comments

--- machines/raspberry-pi-zero-2w-2/startup.sh
+++ machines/raspberry-pi-zero-2w-2/startup.sh
@@ -3,7 +3,7 @@ set -euf
 
 git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel)"
 
-# Disable-Swap is skipped, because with it Pi frequently freezes during operation
+# Disable-Swap is skipped, because with it this RPi frequently freezes during operation
 # Mainly during heavy load, such as docker-builds
 
 sh "$git_dir/.utils/startup/macvlan-router.sh" 10.1.6.7 10.1.17.0
