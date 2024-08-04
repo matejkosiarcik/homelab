6326111f40b25ad85e1a50efdb32c63d4a474e7c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-04 15:29:11 +0200

    refactor: rename machine-startup-helpers

--- machines/raspberry-pi-zero-2w-2/startup.sh
+++ machines/raspberry-pi-zero-2w-2/startup.sh
@@ -5,4 +5,4 @@ git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse -
 
 # disable-swap.sh is skipped, because with it this RPi frequently freezes during heavy operation (such as docker-builds)
 
-sh "$git_dir/utils/machine-startup/macvlan-router.sh" macvlan-shim-1 10.1.21.1 10.1.20.1
+sh "$git_dir/utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-1 10.1.21.1 10.1.20.1
