b3ec20ba82978e3a26b3880f4c05a721534037db
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-14 20:27:24 +0200

    chore: sort socket-proxy service alphabetically

--- docker-images/custom/socket-proxy/main.sh
+++ docker-images/custom/socket-proxy/main.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 set -euf
 
-if [ "${HOMELAB_APP_TYPE-x}" = 'pihole' ]; then
+if [ "${HOMELAB_APP_TYPE-x}" = 'healthchecks' ]; then
+    # TODO: Also forward email SMTP port?
+    socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
+    socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
+elif [ "${HOMELAB_APP_TYPE-x}" = 'homer' ]; then
+    socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
+    socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
+elif [ "${HOMELAB_APP_TYPE-x}" = 'lamp-controller' ]; then
+    socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
+    socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
+elif [ "${HOMELAB_APP_TYPE-x}" = 'pihole' ]; then
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
     socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
     socat TCP4-LISTEN:53,fork,reuseaddr TCP4:main-app:53 &
@@ -10,16 +20,6 @@ elif [ "${HOMELAB_APP_TYPE-x}" = 'smtp4dev' ]; then
     socat TCP4-LISTEN:25,fork,reuseaddr TCP4:main-app:25 &
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
     # socat TCP4-LISTEN:143,fork,reuseaddr TCP4:main-app:143 &
-    socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
-elif [ "${HOMELAB_APP_TYPE-x}" = 'healthchecks' ]; then
-    # TODO: Also forward email SMTP port???
-    socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
-    socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
-elif [ "${HOMELAB_APP_TYPE-x}" = 'homer' ]; then
-    socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
-    socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
-elif [ "${HOMELAB_APP_TYPE-x}" = 'lamp-controller' ]; then
-    socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
     socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
 elif [ "${HOMELAB_APP_TYPE-x}" = 'uptime-kuma' ]; then
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
