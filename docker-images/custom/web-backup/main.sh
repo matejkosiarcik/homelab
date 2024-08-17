042ed068386906af8de5b7a90eb0ef919f8d8da1
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-17 22:51:05 +0200

    refactor: change nesting type of web-backup scripts

--- docker-images/custom/web-backup/main.sh
+++ docker-images/custom/web-backup/main.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -euf
 
-node "/app/dist/backup/$HOMELAB_APP_TYPE.js"
+node "/app/dist/$HOMELAB_APP_TYPE/backup.js"
--- docker-images/custom/web-backup/rollup.config.js
+++ docker-images/custom/web-backup/rollup.config.js
@@ -3,15 +3,42 @@ import typescript from '@rollup/plugin-typescript';
 export default [
     {
         input: [
-            'src/backup/omada-controller.ts',
-            'src/backup/pihole.ts',
-            'src/backup/unifi-controller.ts',
-            'src/backup/uptime-kuma.ts',
+            'src/omada-controller/backup.ts',
         ],
         output: {
-            dir: 'dist/backup',
+            dir: 'dist/omada-controller',
             format: 'esm',
         },
         plugins: [typescript()],
-    }
+    },
+    {
+        input: [
+            'src/pihole/backup.ts',
+        ],
+        output: {
+            dir: 'dist/pihole',
+            format: 'esm',
+        },
+        plugins: [typescript()],
+    },
+    {
+        input: [
+            'src/unifi-controller/backup.ts',
+        ],
+        output: {
+            dir: 'dist/unifi-controller',
+            format: 'esm',
+        },
+        plugins: [typescript()],
+    },
+    {
+        input: [
+            'src/uptime-kuma/backup.ts',
+        ],
+        output: {
+            dir: 'dist/uptime-kuma',
+            format: 'esm',
+        },
+        plugins: [typescript()],
+    },
 ];
