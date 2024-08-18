c926c65f040b0a06ca8ea5aa8145e4d7eb567b1b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-18 18:05:54 +0200

    feat: add pihole-guests

--- servers/raspberry-pi-4b-1/startup.sh
+++ servers/raspberry-pi-4b-1/startup.sh
@@ -6,3 +6,4 @@ git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse -
 sh "$git_dir/.utils/machine-startup-helpers/disable-swap.sh"
 sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-1 10.1.13.1 10.1.12.1
 sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-2 10.1.13.2 10.1.12.2
+sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-3 10.1.13.3 10.1.12.3
