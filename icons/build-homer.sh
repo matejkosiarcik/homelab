f99fbf883f6142cda88841b83e2fe7d730661047
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-18 22:58:52 +0200

    feat: use homer-icons instead of custom downloaded icons

--- icons/build-homer.sh
+++ icons/build-homer.sh
@@ -30,22 +30,25 @@ convert_image "$convert_options" "$input_dir/gitman-repositories/organizr/plugin
 convert_image "$convert_options" "$input_dir/gitman-repositories/organizr/plugins/images/tabs/netdata.png" "$outdir/netdata.png"
 convert_image "$convert_options" "$input_dir/gitman-repositories/organizr/plugins/images/tabs/pihole.png" "$outdir/pihole.png"
 convert_image "$convert_options" "$input_dir/gitman-repositories/organizr/plugins/images/tabs/speedtest-icon.png" "$outdir/speedtest.png"
-convert_image "$convert_options" "$input_dir/gitman-repositories/organizr/plugins/images/tabs/unifi.png" "$outdir/unifi.png"
 
 ### Kubernetes Icons ###
 
+### Homer icons ###
+
+convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/apple-alt.png" "$outdir/apple.png"
+convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/homer.png" "$outdir/homer.png"
+convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/omada.png" "$outdir/tp-link-omada.png"
+convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/tplink.png" "$outdir/tp-link.png"
+convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/svg/prometheus.svg" "$outdir/prometheus.png"
+convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/svg/ubiquiti.svg" "$outdir/unifi.png"
+convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/svg/uptimekuma.svg" "$outdir/uptime-kuma.png"
+
 ### Other Icons ###
 
-convert_image "$convert_options" "$input_dir/other/apple.svg.bin" "$outdir/apple.png"
-convert_image "$convert_options" "$input_dir/other/homer.png" "$outdir/homer.png"
 convert_image "$convert_options" "$input_dir/other/odroid.png" "$outdir/odroid.png"
-convert_image "$convert_options" "$input_dir/other/prometheus.svg.bin" "$outdir/prometheus.png"
 convert_image "$convert_options" "$input_dir/other/raspberry-pi.svg.bin" "$outdir/raspberry-pi.png"
 convert_image "$convert_options" "$input_dir/other/smtp4dev - custom.png" "$outdir/smtp4dev.png"
-convert_image "$convert_options" "$input_dir/other/tp-link-omada.svg.bin" "$outdir/tp-link-omada.png"
-convert_image "$convert_options" "$input_dir/other/tp-link.svg.bin" "$outdir/tp-link.png"
 convert_image "$convert_options" "$input_dir/other/upc.svg.bin" "$outdir/upc.png"
-convert_image "$convert_options" "$input_dir/other/uptime-kuma.svg.bin" "$outdir/uptime-kuma.png"
 
 ### Cleanup ###
 
