942ea2753d030216e41effbd52fecb7f60b08708
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-18 23:48:58 +0200

    refactor: rename startup-helpers utils directory

--- servers/raspberry-pi-3b-1/startup.sh
+++ servers/raspberry-pi-3b-1/startup.sh
@@ -3,5 +3,5 @@ set -euf
 
 git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel)"
 
-sh "$git_dir/.utils/machine-startup-helpers/disable-swap.sh"
-sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-1 10.1.11.1 10.1.10.1
+sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
+sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-1 10.1.11.1 10.1.10.1
--- servers/raspberry-pi-4b-1/startup.sh
+++ servers/raspberry-pi-4b-1/startup.sh
@@ -3,8 +3,8 @@ set -euf
 
 git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel)"
 
-sh "$git_dir/.utils/machine-startup-helpers/disable-swap.sh"
-sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-1 10.1.13.1 10.1.12.1
-sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-2 10.1.13.2 10.1.12.2
-sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-3 10.1.13.3 10.1.12.3
-sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-4 10.1.13.4 10.1.12.4
+sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
+sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-1 10.1.13.1 10.1.12.1
+sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-2 10.1.13.2 10.1.12.2
+sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-3 10.1.13.3 10.1.12.3
+sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-4 10.1.13.4 10.1.12.4
