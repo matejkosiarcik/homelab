6513edaaae2a010bf0b6d28f1350e53020db3a30
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-18 17:55:37 +0200

    feat: add pihole-spouse

--- servers/raspberry-pi-4b-1/startup.sh
+++ servers/raspberry-pi-4b-1/startup.sh
@@ -5,3 +5,4 @@ git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse -
 
 sh "$git_dir/.utils/machine-startup-helpers/disable-swap.sh"
 sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-1 10.1.13.1 10.1.12.1
+sh "$git_dir/.utils/machine-startup-helpers/macvlan-router.sh" macvlan-shim-2 10.1.13.2 10.1.12.2
