3b011dab877d387d91ab7342087f775732429062
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-19 21:57:36 +0200

    fix: proxy handling for redirects

--- docker-images/custom/http-proxy/entrypoint.sh
+++ docker-images/custom/http-proxy/entrypoint.sh
@@ -4,12 +4,14 @@ set -euf
 # Watch certificates in background
 inotifywait --monitor --event modify --format '%w%f' --include 'certificate\.crt' '/certs' | xargs -n1 sh -c 'sleep 1 && printf "Detected new certificates\n" && apachectl -k restart' - &
 
+printf '\n' >>/etc/apache2/envvars
+
 if [ "${HOMELAB_APP_TYPE-x}" = 'x' ]; then
     printf 'HOMELAB_APP_TYPE unset\n' >&2
     exit 1
 fi
 export HOMELAB_APP_TYPE
-printf "\nexport HOMELAB_APP_TYPE='%s'\n" "$HOMELAB_APP_TYPE" >>/etc/apache2/envvars
+printf "export HOMELAB_APP_TYPE='%s'\n" "$HOMELAB_APP_TYPE" >>/etc/apache2/envvars
 
 if [ "${HOMELAB_APP_TYPE-x}" = 'lamp-controller' ]; then
     HOMELAB_UPSTREAM_URL="http://app-network-server"
@@ -27,7 +29,15 @@ else
     HOMELAB_UPSTREAM_URL="http://main-app"
 fi
 export HOMELAB_UPSTREAM_URL
-printf "\nexport HOMELAB_UPSTREAM_URL='%s'\n" "$HOMELAB_UPSTREAM_URL" >>/etc/apache2/envvars
+printf "export HOMELAB_UPSTREAM_URL='%s'\n" "$HOMELAB_UPSTREAM_URL" >>/etc/apache2/envvars
+
+if [ "${HOMELAB_APP_TYPE-x}" = 'pihole' ]; then
+    APACHE_PROXY_PASS_MATCH_NEGATIVE='^/(\.proxy(/.*)?)?$'
+else
+    APACHE_PROXY_PASS_MATCH_NEGATIVE='^/\.proxy(/.*)?$'
+fi
+export APACHE_PROXY_PASS_MATCH_NEGATIVE
+printf "export APACHE_PROXY_PASS_MATCH_NEGATIVE='%s'\n" "$APACHE_PROXY_PASS_MATCH_NEGATIVE" >>/etc/apache2/envvars
 
 mkdir -p /log/access /log/error /log/forensic
 
