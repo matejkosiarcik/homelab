414416213d134a81fdd5e6752792ed9e0c758637
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-08-30 22:29:24 +0200

    refactor: prefix imports with "node:"

--- docker-images/custom/web-automation/src/.utils/main.ts
+++ docker-images/custom/web-automation/src/.utils/main.ts
@@ -1,6 +1,6 @@
-import fs from 'fs/promises';
-import os from 'os';
-import path from 'path';
+import fs from 'node:fs/promises';
+import os from 'node:os';
+import path from 'node:path';
 import { chromium, type Page } from 'playwright';
 import { getAppName, getErrorAttachmentDir, getIsHeadless, getTargetUrl } from "./utils.ts";
 
