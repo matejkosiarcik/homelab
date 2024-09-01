3b9791a3b776fcd00bc83c552b800a64cca0e300
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-01 09:38:17 +0200

    chore: set appname

--- .utils/deployment-helpers/.create-secrets-helpers/playwright/src/speedtest-tracker-app-key.ts
+++ .utils/deployment-helpers/.create-secrets-helpers/playwright/src/speedtest-tracker-app-key.ts
@@ -8,7 +8,7 @@ import { runAutomation } from './.utils/main.ts';
 (async () => {
     preprepare();
     process.env['URL'] = 'https://speedtest-tracker.dev';
-    process.env['HOMELAB_APP_NAME'] = ''; // Must set this to work with web-automation utils
+    process.env['HOMELAB_APP_NAME'] = 'N/A'; // Must set this to work with web-automation utils
 
     let argumentParser = yargs(hideBin(process.argv))
         .locale('en')
