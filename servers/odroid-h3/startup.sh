f709e8fb668bda7c71dab377b516593d246633b3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-02 10:31:39 +0200

    feat: add missing odroid virtual router IPs

--- servers/odroid-h3/startup.sh
+++ servers/odroid-h3/startup.sh
@@ -12,3 +12,5 @@ sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-4 10.1.17.4 
 sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-5 10.1.17.5 10.1.16.5
 sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-6 10.1.17.6 10.1.16.6
 sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-7 10.1.17.7 10.1.16.7
+sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-8 10.1.17.8 10.1.16.8
+sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-9 10.1.17.9 10.1.16.9
