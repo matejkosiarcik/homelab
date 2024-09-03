6b2cba077d2fdb5701aa0598f11528350303f5a2
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-03 14:34:09 +0200

    feat: add more logging to web-automation scripts

--- docker-images/custom/web-automation/main.sh
+++ docker-images/custom/web-automation/main.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -euf
 
+printf '%s - Starting %s for %s\n' "$(date '+%Y-%m-%d_%H-%M-%S')" "$HOMELAB_CONTAINER_VARIANT" "$HOMELAB_APP_NAME"
 node "/app/dist/$HOMELAB_APP_NAME/$HOMELAB_CONTAINER_VARIANT.js"
+printf '%s - Finished %s for %s\n' "$(date '+%Y-%m-%d_%H-%M-%S')" "$HOMELAB_CONTAINER_VARIANT" "$HOMELAB_APP_NAME"
