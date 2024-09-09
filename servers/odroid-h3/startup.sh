2b65bf0948ab5a520456fcdad8df4443c4a47231
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-09 17:20:04 +0200

    feat: add "seq" to startup shim routers

--- servers/odroid-h3/startup.sh
+++ servers/odroid-h3/startup.sh
@@ -5,12 +5,6 @@ git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse -
 
 sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
 
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-001 10.1.17.1 10.1.16.1
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-002 10.1.17.2 10.1.16.2
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-003 10.1.17.3 10.1.16.3
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-004 10.1.17.4 10.1.16.4
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-005 10.1.17.5 10.1.16.5
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-006 10.1.17.6 10.1.16.6
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-007 10.1.17.7 10.1.16.7
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-008 10.1.17.8 10.1.16.8
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-009 10.1.17.9 10.1.16.9
+seq 1 9 | while read -r index; do
+    sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" "macvlan-shim-$(printf '%03d' "$index")" "10.1.17.$index" "10.1.16.$index"
+done
--- servers/raspberry-pi-zero-2w-2/startup.sh
+++ servers/raspberry-pi-zero-2w-2/startup.sh
@@ -5,4 +5,6 @@ git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse -
 
 # disable-swap.sh is skipped, because with it this RPi frequently freezes during heavy operation (such as docker-builds)
 
-sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" macvlan-shim-001 10.1.21.1 10.1.20.1
+seq 1 1 | while read -r index; do
+    sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" "macvlan-shim-$(printf '%03d' "$index")" "10.1.21.$index" "10.1.20.$index"
+done
