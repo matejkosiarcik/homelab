c176ed8299afc82fae134dc5469697853dce757b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-13 13:50:36 +0200

    chore: limit parallelism on RPi3

--- servers/raspberry-pi-3b/helper.sh
+++ servers/raspberry-pi-3b/helper.sh
@@ -3,5 +3,9 @@ set -euf
 
 cd "$(dirname "$0")"
 
+# Limit build concurrency
+COMPOSE_PARALLEL_LIMIT=1
+export COMPOSE_PARALLEL_LIMIT
+
 # shellcheck disable=SC2068
 bash "$(git rev-parse --show-toplevel)/.utils/deployment-helpers/helper-server.sh" $@
