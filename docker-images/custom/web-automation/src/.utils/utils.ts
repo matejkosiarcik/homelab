5e54282ca5e18a269d8e361b119d2348ff93f9e9
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-13 12:33:07 +0200

    feat: expand web-automation utils

--- docker-images/custom/web-automation/src/.utils/utils.ts
+++ docker-images/custom/web-automation/src/.utils/utils.ts
@@ -4,6 +4,7 @@ import path from 'node:path';
 import dotenv from 'dotenv';
 import { expect } from 'chai';
 import { type Locator, type Page } from 'playwright';
+import { randomInt } from 'node:crypto';
 
 export function preprepare() {
     // Load .env file - Mostly useful for local (non-docker) debugging
@@ -93,4 +94,36 @@ export async function getVisibleLocator(page: Page, selector: string): Promise<L
         }
     }
     throw new Error(`Visible locator for selector '${selector}' not found`);
+}
+
+export async function delay(timeout: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, timeout);
+    })
+}
+
+export async function retry<T>(options: {
+    action: () => T | Promise<T>,
+    retries: number,
+    delay: number,
+}): Promise<T> {
+    let lastError: Error | undefined;
+    for (let currentRetry = 0; currentRetry <= options.retries; currentRetry += 1) {
+        try {
+            let value = options.action();
+            if (typeof value === 'object' && value && 'then' in value) {
+                value = await value;
+            }
+            return value;
+        } catch (error) {
+            lastError = error as Error;
+        }
+
+        if (currentRetry < options.retries) {
+            await delay(options.delay);
+        }
+    }
+    throw lastError;
 }
