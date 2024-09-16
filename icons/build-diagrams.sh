ac6c7a4f5d449f42c9b80919700291c7e7f6b672
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-16 13:31:30 +0200

    chore: update internal icons

--- icons/build-diagrams.sh
+++ icons/build-diagrams.sh
@@ -13,13 +13,14 @@ mkdir -p "$outdir"
 
 convert_options='magick -background none -bordercolor transparent INPUT_FILE -resize 256x256 -density 1200 OUTPUT_FILE'
 
-### Bash logo ###
+### Bash icon ###
 
 convert_image "$convert_options" "$input_dir/gitman-repositories/official-bash-logo/assets/Logos/Icons/SVG/512x512.svg" "$outdir/bash.png"
 
 ### OSA Icons ###
 
 convert_image "$convert_options" "$tmpdir/13_05_osa_icons_svg/osa_cloud.svg" "$outdir/cloud.png"
+convert_image "$convert_options" "$tmpdir/13_05_osa_icons_svg/osa_home.svg" "$outdir/home.png"
 convert_image "$convert_options" "$tmpdir/13_05_osa_icons_svg/osa_awareness.svg" "$outdir/lightbulb.png"
 convert_image "$convert_options" "$tmpdir/13_05_osa_icons_svg/osa_hub.svg" "$outdir/network.png"
 
@@ -43,6 +44,7 @@ convert_image "$convert_options" "$input_dir/gitman-repositories/kubernetes-comm
 
 convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/chromium.png" "$outdir/chromium.png"
 convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/docker-moby.png" "$outdir/docker.png"
+convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/azuredns.png" "$outdir/dns.png"
 convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/homer.png" "$outdir/homer.png"
 convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/letencrypt.png" "$outdir/lets-encrypt.png"
 convert_image "$convert_options" "$input_dir/gitman-repositories/homer-icons/png/minio.png" "$outdir/minio.png"
@@ -65,10 +67,6 @@ convert_image "$convert_options" "$input_dir/other/raspberry-pi.svg.bin" "$outdi
 convert_image "$convert_options" "$input_dir/other/smtp4dev - custom.png" "$outdir/smtp4dev.png"
 
 ### Combined icons ###
-
-convert_image_draft 'magick -background none -bordercolor transparent INPUT_FILE -resize 256x256 -border 32 -density 1200 OUTPUT_FILE' "$input_dir/gitman-repositories/homer-icons/png/chromium.png" "$tmpdir/chromium.png"
-convert_image_draft 'magick -background none -bordercolor transparent INPUT_FILE -resize 256x256 -border 64 -density 1200 OUTPUT_FILE' "$input_dir/other/playwright.svg.bin" "$tmpdir/playwright.png"
-convert_image "magick $tmpdir/chromium.png $tmpdir/playwright.png -gravity Center -geometry 256x256+80+96 -composite -resize 256x256 OUTPUT_FILE" '' "$outdir/chromium+playwright.png"
 
 convert_image_draft 'magick -background none -bordercolor transparent INPUT_FILE -resize 256x256 -border 32 -density 1200 OUTPUT_FILE' "$tmpdir/13_05_osa_icons_svg/osa_server.svg" "$tmpdir/server-1.png"
 convert_image_draft 'magick -background none -bordercolor transparent INPUT_FILE -resize 96x96 -border 32 -density 1200 OUTPUT_FILE' "$tmpdir/13_05_osa_icons_svg/osa_server.svg" "$tmpdir/server-2.png"
