21a4cc3f8895459c76a506fefedf122188d37a00
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-19 18:57:18 +0200

    feat: add new smb app

---
+++ docker-images/custom/samba/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -euf
+
+SAMBA_GROUP="$SAMBA_USERNAME-group"
+
+cat /etc/samba/smb.conf | \
+    sed "s~#smb-title#~$SAMBA_TITLE~;s~#smb-user#~$SAMBA_USERNAME~;s~#smb-group#~$SAMBA_GROUP~" | \
+    sponge /etc/samba/smb.conf
+
+groupadd "$SAMBA_GROUP"
+useradd -M -s /sbin/nologin "$SAMBA_USERNAME"
+usermod -a -G "$SAMBA_GROUP" "$SAMBA_USERNAME"
+printf '%s\n%s\n' "$SAMBA_PASSWORD" "$SAMBA_PASSWORD" | smbpasswd -s -a "$SAMBA_USERNAME"
+
+testparm -s
+
+smbd --foreground --no-process-group
