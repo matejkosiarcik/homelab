73e6ec7d9f0dfd27eed9eedbc28f3bca9bf16ea6
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-22 20:45:32 +0200

    build: simplify rollup config

--- docker-images/custom/web-automation/rollup.config.js
+++ docker-images/custom/web-automation/rollup.config.js
@@ -1,66 +1,21 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default [
-    {
-        input: [
-            'src/home-assistant/backup.ts',
-        ],
-        output: {
-            dir: 'dist/home-assistant',
-            format: 'esm',
-        },
-        plugins: [typescript()],
+const files = {
+    'home-assistant': ['backup.ts'],
+    'omada-controller': ['backup.ts'],
+    'pihole': ['backup.ts', 'update-gravity.ts'],
+    'speedtest-tracker': ['admin-setup.ts'],
+    'unifi-controller': ['backup.ts'],
+    'uptime-kuma': ['admin-setup.ts', 'backup.ts'],
+};
+
+const exportArray = Object.entries(files).map(([directory, files]) => ({
+    input: files.map((file) => `src/${directory}/${file}`),
+    output: {
+        dir: `dist/${directory}`,
+        format: 'esm',
     },
-    {
-        input: [
-            'src/omada-controller/backup.ts',
-        ],
-        output: {
-            dir: 'dist/omada-controller',
-            format: 'esm',
-        },
-        plugins: [typescript()],
-    },
-    {
-        input: [
-            'src/pihole/backup.ts',
-            'src/pihole/update-gravity.ts',
-        ],
-        output: {
-            dir: 'dist/pihole',
-            format: 'esm',
-        },
-        plugins: [typescript()],
-    },
-    {
-        input: [
-            'src/speedtest-tracker/admin-setup.ts',
-        ],
-        output: {
-            dir: 'dist/speedtest-tracker',
-            format: 'esm',
-        },
-        plugins: [typescript()],
-    },
-    {
-        input: [
-            'src/unifi-controller/backup.ts',
-        ],
-        output: {
-            dir: 'dist/unifi-controller',
-            format: 'esm',
-        },
-        plugins: [typescript()],
-    },
-    {
-        input: [
-            'src/uptime-kuma/admin-setup.ts',
-            'src/uptime-kuma/backup.ts',
-        ],
-        output: {
-            dir: 'dist/uptime-kuma',
-            format: 'esm',
-        },
-        plugins: [typescript()],
-    },
-];
+    plugins: [typescript()],
+}));
+
+export default exportArray;
