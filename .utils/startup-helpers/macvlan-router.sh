cedd27b1979ff07b22a35dd2f6add4f03acdab9a
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-25 15:45:44 +0200

    fix: dynamically select network interface name

--- .utils/startup-helpers/macvlan-router.sh
+++ .utils/startup-helpers/macvlan-router.sh
@@ -11,6 +11,20 @@ router_name="$1"
 external_ip="$2"
 internal_docker_ip="$3"
 
+interface="$(sh <<EOF
+printf 'eth0\nenp1s0\n' | while read -r interface; do
+    found='1'
+    ip link show "$interface" >/dev/null 2>/dev/null || found='0'
+    if [ "$found" -eq 1 ]; then
+        printf '%s\n' "$interface"
+        break
+    fi
+done
+EOF
+)"
+
+printf 'Found network interface %s\n' "$interface"
+
 # Add macvlan-shim "router" to be able to access containers from host
 sudo ip link add "$router_name" link eth0 type macvlan mode bridge
 sudo ip addr add "$external_ip/32" dev "$router_name"
