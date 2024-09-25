4170300c133f96110d04419456f18c56f1c3967e
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-25 16:52:53 +0200

    chore: fix macvlan router names

--- servers/raspberry-pi-3b/startup.sh
+++ servers/raspberry-pi-3b/startup.sh
@@ -6,5 +6,5 @@ git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse -
 sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
 
 seq 1 1 | while read -r index; do
-    sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" "macvlan-shim-$(printf '%03d' "$index")" "10.1.11.$index" "10.1.10.$index"
+    sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" "macvlan-$(printf '%03d' "$index")" "10.1.11.$index" "10.1.10.$index"
 done
