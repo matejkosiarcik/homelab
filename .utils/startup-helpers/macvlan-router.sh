87dc818b45f6970a3bd5bd97faee880797247ca1
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-25 15:58:12 +0200

    chore: fix shell lint errors

--- .utils/startup-helpers/macvlan-router.sh
+++ .utils/startup-helpers/macvlan-router.sh
@@ -11,15 +11,16 @@ router_name="$1"
 external_ip="$2"
 internal_docker_ip="$3"
 
-interface="$(sh <<EOF
-printf 'eth0\nenp1s0\n' | while read -r interface; do
-    found='1'
-    ip link show "$interface" >/dev/null 2>/dev/null || found='0'
-    if [ "$found" -eq 1 ]; then
-        printf '%s\n' "$interface"
-        break
-    fi
-done
+interface="$(
+    sh <<EOF
+    printf 'eth0\nenp1s0\n' | while read -r interface; do
+        found='1'
+        ip link show "$interface" >/dev/null 2>/dev/null || found='0'
+        if [ "$found" -eq 1 ]; then
+            printf '%s\n' "$interface"
+            break
+        fi
+    done
 EOF
 )"
 
