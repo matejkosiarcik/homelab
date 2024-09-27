342ca21801491cb359b1d6f6610d91d9599e5eb0
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-27 10:49:17 +0200

    feat: update web-automation internal scripts

--- docker-images/custom/web-automation/main.sh
+++ docker-images/custom/web-automation/main.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -euf
 
+if [ "$CRON" != 1 ]; then
+    printf '%s - Skipping initial run\n' "$(date '+%Y-%m-%d_%H-%M-%S')"
+    exit
+fi
+
 printf '%s - Starting %s for %s\n' "$(date '+%Y-%m-%d_%H-%M-%S')" "$HOMELAB_CONTAINER_VARIANT" "$HOMELAB_APP_NAME"
 node "/homelab/dist/$HOMELAB_APP_NAME/$HOMELAB_CONTAINER_VARIANT.js"
 printf '%s - Finished %s for %s\n' "$(date '+%Y-%m-%d_%H-%M-%S')" "$HOMELAB_CONTAINER_VARIANT" "$HOMELAB_APP_NAME"
