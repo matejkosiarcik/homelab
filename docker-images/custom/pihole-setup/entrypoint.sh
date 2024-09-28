9c65038bc62fcf0595e1643f8adad70564b148b0
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-28 16:30:08 +0200

    chore: extend pihole-setup start-interval

--- docker-images/custom/pihole-setup/entrypoint.sh
+++ docker-images/custom/pihole-setup/entrypoint.sh
@@ -4,7 +4,10 @@ set -euf
 mkdir -p /homelab/.internal
 printf 'starting\n' >/homelab/.internal/status.txt
 
+sleep 10
+printf '%s - Starting setup\n' "$(date '+%Y-%m-%d_%H-%M-%S')"
 docker exec "$HOMELAB_PIHOLE_CONTAINER" sh /homelab/setup.sh
+printf '%s - Finished setup\n' "$(date '+%Y-%m-%d_%H-%M-%S')"
 
 printf 'started\n' >/homelab/.internal/status.txt
 sleep infinity
