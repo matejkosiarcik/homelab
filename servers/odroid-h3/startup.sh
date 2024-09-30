917c6c0b3dd6d8ef31f064ab4737e412e4cd6a0f
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-09-30 22:10:25 +0200

    feat: reorder subnets assigned to devices
    This should better reflect device order alphabetically

--- servers/odroid-h3/startup.sh
+++ servers/odroid-h3/startup.sh
@@ -6,7 +6,7 @@ git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse -
 sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
 
 seq 1 255 | while read -r index; do
-    sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" "macvlan-$(printf '%03d' "$index")" "10.1.17.$index" "10.1.16.$index"
+    sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" "macvlan-$(printf '%03d' "$index")" "10.1.11.$index" "10.1.10.$index"
 done
 
 sh "$git_dir/.utils/startup-helpers/rfkill.sh"
