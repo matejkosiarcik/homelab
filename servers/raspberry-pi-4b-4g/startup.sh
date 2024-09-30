1fd724edb8fc0ab5df77aab5d1f32a6791d042db
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-01 00:02:07 +0200

    fix: startup scripts git_dir

--- servers/raspberry-pi-4b-4g/startup.sh
+++ servers/raspberry-pi-4b-4g/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -euf
 
-git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel)"
+# git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel)"
+git_dir="$HOME/git/homelab"
 
 sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
 
