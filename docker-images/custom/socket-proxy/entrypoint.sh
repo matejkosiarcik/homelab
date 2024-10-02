4bd2c876da6ad77ac713cb4db200e2bfe44ae7c2
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-02 21:39:57 +0200

    chore: debug unifi-controller ports

--- docker-images/custom/socket-proxy/entrypoint.sh
+++ docker-images/custom/socket-proxy/entrypoint.sh
@@ -76,10 +76,15 @@ elif [ "$HOMELAB_APP_TYPE" = 'tvheadend' ]; then
     socat TCP4-LISTEN:9982,fork,reuseaddr TCP4:tvheadend:9982 &
 elif [ "$HOMELAB_APP_TYPE" = 'unifi-controller' ]; then
     # HTTP/S ports
-    socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy-admin:80 &
-    socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy-admin:443 &
-    socat TCP4-LISTEN:81,fork,reuseaddr TCP4:http-proxy-portal:80 &
-    socat TCP4-LISTEN:444,fork,reuseaddr TCP4:http-proxy-portal:443 &
+    # socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy-admin:80 &
+    # socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy-admin:443 &
+    # socat TCP4-LISTEN:81,fork,reuseaddr TCP4:http-proxy-portal:80 &
+    # socat TCP4-LISTEN:444,fork,reuseaddr TCP4:http-proxy-portal:443 &
+    socat TCP4-LISTEN:443,fork,reuseaddr TCP4:unifi-network-application:443 &
+    socat TCP4-LISTEN:8080,fork,reuseaddr TCP4:unifi-network-application:8080 &
+    socat TCP4-LISTEN:8443,fork,reuseaddr TCP4:unifi-network-application:8443 &
+    socat TCP4-LISTEN:8843,fork,reuseaddr TCP4:unifi-network-application:8843 &
+    socat TCP4-LISTEN:8880,fork,reuseaddr TCP4:unifi-network-application:8880 &
     # Other ports
     socat -T30 UDP4-LISTEN:1900,fork,reuseaddr UDP4:unifi-network-application:1900 &
     socat -T30 UDP4-LISTEN:3478,fork,reuseaddr UDP4:unifi-network-application:3478 &
