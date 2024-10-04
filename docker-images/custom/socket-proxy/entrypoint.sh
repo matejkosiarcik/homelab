03ded760d59a15853cdd7858b05327975d9b9d00
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-05 00:18:48 +0200

    style: fix shell lint error

--- docker-images/custom/socket-proxy/entrypoint.sh
+++ docker-images/custom/socket-proxy/entrypoint.sh
@@ -4,7 +4,6 @@ set -euf
 mkdir -p /homelab/.internal
 printf 'starting\n' >/homelab/.internal/status.txt
 
-
 if [ "$HOMELAB_APP_TYPE" = 'changedetection' ]; then
     socat TCP4-LISTEN:80,fork,reuseaddr TCP4:http-proxy:80 &
     socat TCP4-LISTEN:443,fork,reuseaddr TCP4:http-proxy:443 &
