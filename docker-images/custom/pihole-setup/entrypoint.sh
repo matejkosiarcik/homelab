caaee38cb4d05c2380489d036035ba0fd4fe78ed
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-07 15:50:32 +0200

    refactor: rename variables for custom setup

--- docker-images/custom/pihole-setup/entrypoint.sh
+++ docker-images/custom/pihole-setup/entrypoint.sh
@@ -6,13 +6,13 @@ printf 'starting\n' >/homelab/.internal/status.txt
 
 # Wait for target container to start
 timeout 30s sh <<EOF
-while [ "$(docker ps --filter "name=^$HOMELAB_NTFY_CONTAINER\$" | grep -c -- "$HOMELAB_NTFY_CONTAINER" || true)" -eq '0' ]; do
+while [ "$(docker ps --filter "name=^$HOMELAB_NTFY_CONTAINER\$" | grep -c -- "$HOMELAB_SETUP_TARGET_CONTAINER" || true)" -eq '0' ]; do
     sleep 1
 done
 EOF
 
 printf '%s - Starting setup\n' "$(date '+%Y-%m-%d_%H-%M-%S')"
-docker exec "$HOMELAB_PIHOLE_CONTAINER" sh /homelab/setup.sh
+docker exec "$HOMELAB_SETUP_TARGET_CONTAINER" sh /homelab/setup.sh
 printf '%s - Finished setup\n' "$(date '+%Y-%m-%d_%H-%M-%S')"
 
 printf 'started\n' >/homelab/.internal/status.txt
