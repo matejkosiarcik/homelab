6de28020091713e824d1e236de66325ba236e597
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-12 15:39:14 +0200

    chore: try to loop container infinite sleep

--- docker-images/custom/minio-client/entrypoint.sh
+++ docker-images/custom/minio-client/entrypoint.sh
@@ -7,4 +7,7 @@ printf 'starting\n' >/homelab/.internal/status.txt
 sh /homelab/main.sh
 
 printf 'started\n' >/homelab/.internal/status.txt
-while true; do sleep infinity; done
+while true; do
+    sleep infinity
+    printf '"sleep infinity" somehow exited?' >&2
+done
--- docker-images/custom/socket-proxy/entrypoint.sh
+++ docker-images/custom/socket-proxy/entrypoint.sh
@@ -128,4 +128,7 @@ else
 fi
 
 printf 'started\n' >/homelab/.internal/status.txt
-while true; do sleep infinity; done
+while true; do
+    sleep infinity
+    printf '"sleep infinity" somehow exited?' >&2
+done
