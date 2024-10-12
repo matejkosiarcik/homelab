31de881b83f6c0a6f8b2bcd2584afae96a9768de
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-12 22:53:30 +0200

    chore: debug macvlans

--- .utils/startup-helpers/custom-router.sh
+++ .utils/startup-helpers/custom-router.sh
@@ -1,34 +1,34 @@
-#!/bin/sh
-router_name=foo
-found_interface=eth0
-external_ip=10.1.100.3
+# #!/bin/sh
+# router_name=foo
+# found_interface=eth0
+# external_ip=10.1.100.3
 
-# Add macvlan-shim "router" to be able to access containers from host
-sudo ip link add "$router_name" link "$found_interface" type macvlan mode bridge
-sudo ip addr add "$external_ip/24" dev "$router_name"
-sudo ip link set "$router_name" up
-# sudo ip route add "$internal_docker_ip/24" dev "$router_name"
+# # Add macvlan-shim "router" to be able to access containers from host
+# sudo ip link add "$router_name" link "$found_interface" type macvlan mode bridge
+# sudo ip addr add "$external_ip/24" dev "$router_name"
+# sudo ip link set "$router_name" up
+# # sudo ip route add "$internal_docker_ip/24" dev "$router_name"
 
-WANIF='eth0.26'
-LANIF='foo'
+# WANIF='eth0.26'
+# LANIF='foo'
 
-# enable ip forwarding in the kernel
-# echo 'Enabling Kernel IP forwarding...'
-# sudo printf '1\n' >/proc/sys/net/ipv4/ip_forward
+# # enable ip forwarding in the kernel
+# # echo 'Enabling Kernel IP forwarding...'
+# # sudo printf '1\n' >/proc/sys/net/ipv4/ip_forward
 
-# flush rules and delete chains
-echo 'Flushing rules and deleting existing chains...'
-sudo iptables -F
-sudo iptables -X
+# # flush rules and delete chains
+# echo 'Flushing rules and deleting existing chains...'
+# sudo iptables -F
+# sudo iptables -X
 
-# enable masquerading to allow LAN internet access
-echo 'Enabling IP Masquerading and other rules...'
-sudo iptables -t nat -A POSTROUTING -o $LANIF -j MASQUERADE
-sudo iptables -A FORWARD -i $LANIF -o $WANIF -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i $WANIF -o $LANIF -j ACCEPT
+# # enable masquerading to allow LAN internet access
+# echo 'Enabling IP Masquerading and other rules...'
+# sudo iptables -t nat -A POSTROUTING -o $LANIF -j MASQUERADE
+# sudo iptables -A FORWARD -i $LANIF -o $WANIF -m state --state RELATED,ESTABLISHED -j ACCEPT
+# sudo iptables -A FORWARD -i $WANIF -o $LANIF -j ACCEPT
 
-sudo iptables -t nat -A POSTROUTING -o $WANIF -j MASQUERADE
-sudo iptables -A FORWARD -i $WANIF -o $LANIF -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i $LANIF -o $WANIF -j ACCEPT
+# sudo iptables -t nat -A POSTROUTING -o $WANIF -j MASQUERADE
+# sudo iptables -A FORWARD -i $WANIF -o $LANIF -m state --state RELATED,ESTABLISHED -j ACCEPT
+# sudo iptables -A FORWARD -i $LANIF -o $WANIF -j ACCEPT
 
-echo 'Done.'
+# echo 'Done.'
--- servers/raspberry-pi-4b-4g/startup.sh
+++ servers/raspberry-pi-4b-4g/startup.sh
@@ -7,3 +7,4 @@ git_dir="$HOME/git/homelab"
 sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
 sh "$git_dir/.utils/startup-helpers/rfkill.sh"
 sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" 'macvlan-shim' '10.1.17.0' '10.1.16.0'
+sh "$git_dir/.utils/startup-helpers/macvlan-router2.sh" 'macvlan-shim2' '10.1.12.4' '10.1.12.3'
