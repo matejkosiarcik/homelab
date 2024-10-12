ed10a6075f83389043ade7442515b4dd82e18f1c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-12 23:03:36 +0200

    chore: debug macvlans

---
+++ .utils/startup-helpers/add-vlan.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -euf
+# This script sets up macvlan-shim "router" to be able to access containers running in macvlan network from current host
+
+if [ "$#" -lt 2 ]; then
+    printf 'Not enough arguments\n' >&2
+    exit 1
+fi
+
+router_name="$1"
+external_ip="$2"
+internal_docker_ip="$3"
+
+# Get appropriate network interface
+has_eth0="$(ip link show eth0 >/dev/null 2>/dev/null || printf '0\n')"
+has_enp1s0="$(ip link show enp1s0 >/dev/null 2>/dev/null || printf '0\n')"
+found_interface=''
+if [ "$has_eth0" = '' ]; then
+    found_interface='eth0'
+elif [ "$has_enp1s0" = '' ]; then
+    found_interface='enp1s0'
+fi
+if [ "$found_interface" = '' ]; then
+    printf 'No suitable network interface found\n'
+    exit 1
+fi
+
+printf 'Found network interface %s\n' "$found_interface"
+
+sudo ip link add link "$found_interface" name vlan12 type vlan id 12
+sudo ip address add 10.2.12.128/30 dev vlan12
+sudo ip link set vlan12 up
