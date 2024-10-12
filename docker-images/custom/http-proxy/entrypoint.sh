79226e930f5fee82b4b81be67c32d3613f0ef8d3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-12 12:59:53 +0200

    feat: try to add secondary http-proxy to unifi-controller

--- docker-images/custom/http-proxy/entrypoint.sh
+++ docker-images/custom/http-proxy/entrypoint.sh
@@ -102,7 +102,7 @@ elif [ "$HOMELAB_APP_TYPE" = 'speedtest-tracker' ]; then
 elif [ "$HOMELAB_APP_TYPE" = 'tvheadend' ]; then
     PROXY_UPSTREAM_URL="http://tvheadend:9981"
 elif [ "$HOMELAB_APP_TYPE" = 'unifi-controller' ]; then
-    if [ "$HOMELAB_CONTAINER_VARIANT" = 'admin' ]; then
+    if [ "$HOMELAB_CONTAINER_VARIANT" = 'admin' ] || [ "$HOMELAB_CONTAINER_VARIANT" = 'admin-raw' ]; then
         PROXY_UPSTREAM_URL="https://unifi-network-application:8443"
     elif [ "$HOMELAB_CONTAINER_VARIANT" = 'portal' ]; then
         PROXY_UPSTREAM_URL="https://unifi-network-application:8444"
@@ -160,6 +160,8 @@ printf "export PROXY_HTTPS_PORT='%s'\n" "$PROXY_HTTPS_PORT" >>/etc/apache2/envva
 
 # Set PROXY_FORCE_HTTPS
 if [ "$HOMELAB_APP_TYPE" = 'ntfy' ]; then
+    PROXY_FORCE_HTTPS='false'
+elif [ "$HOMELAB_APP_TYPE" = 'unifi-controller' ] && [ "$HOMELAB_CONTAINER_VARIANT" = 'admin-raw' ]; then
     PROXY_FORCE_HTTPS='false'
 else
     PROXY_FORCE_HTTPS='true'
