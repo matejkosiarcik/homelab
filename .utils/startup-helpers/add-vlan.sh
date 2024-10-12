2e1353fc36627e1544c29bd5e19d4dc5f2f789ec
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-12 23:15:51 +0200

    chore: debug macvlans

--- .utils/startup-helpers/add-vlan.sh
+++ .utils/startup-helpers/add-vlan.sh
@@ -2,14 +2,14 @@
 set -euf
 # This script sets up macvlan-shim "router" to be able to access containers running in macvlan network from current host
 
-if [ "$#" -lt 2 ]; then
-    printf 'Not enough arguments\n' >&2
-    exit 1
-fi
+# if [ "$#" -lt 1 ]; then
+#     printf 'Not enough arguments\n' >&2
+#     exit 1
+# fi
 
-router_name="$1"
-external_ip="$2"
-internal_docker_ip="$3"
+# router_name="$1"
+# external_ip="$2"
+# internal_docker_ip="$3"
 
 # Get appropriate network interface
 has_eth0="$(ip link show eth0 >/dev/null 2>/dev/null || printf '0\n')"
