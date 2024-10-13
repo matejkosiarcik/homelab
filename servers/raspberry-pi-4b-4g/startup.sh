3ed2b8368105d177db25a4606a766c8841d08c44
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-13 16:55:22 +0200

    feat: update startup scripts network adapters

--- servers/raspberry-pi-4b-4g/startup.sh
+++ servers/raspberry-pi-4b-4g/startup.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 set -euf
 
-# git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel)"
-git_dir="$HOME/git/homelab"
+git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel || printf '%s\n' "$HOME/git/homelab")"
 
 sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
 sh "$git_dir/.utils/startup-helpers/rfkill.sh"
 # sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" 'macvlan-shim' '10.1.17.0' '10.1.16.0'
-# sh "$git_dir/.utils/startup-helpers/macvlan-router2.sh" 'macvlan-shim2' '10.1.12.4' '10.1.12.3'
-# sh "$git_dir/.utils/startup-helpers/add-vlan.sh"
 
 seq 1 255 | while read -r i; do
     sh "$git_dir/.utils/startup-helpers/ethbridge.sh" "ethbridge-$i" "10.1.16.$i"
