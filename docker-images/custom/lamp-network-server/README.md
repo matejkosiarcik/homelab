4514bccf6bbc8808678f537865ceee2385db23b7
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-10-28 10:24:39 +0100

    chore: remove source code for lamp server

--- docker-images/custom/lamp-network-server/README.md
+++
@@ -1,4 +0,0 @@
-# Network server
-
-This is a network interface for lamps.
-Backend _Express_, frontend with _VueJs_.
--- docker-images/custom/lamp-network-server/frontend/index.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import ToastPlugin from 'vue-toast-notification';
-import App from './App.vue'
-
-const app = createApp(App)
-app.use(ToastPlugin.default);
-app.mount('#app')
