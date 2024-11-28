504d67e1efc8bfd4ab7d622ad751469ce55ad469
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-11-28 19:09:18 +0100

    feat: add unbound configs

---
+++ .utils/startup-helpers/unbound.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -euf
+# This script starts native unbound
+
+if [ "$#" -lt 1 ]; then
+    printf 'Not enough arguments\n' >&2
+    exit 1
+fi
+
+config_file="$1"
+# nohup sudo unbound -c "$config_file" &
+sudo unbound -c "$config_file"
--- servers/raspberry-pi-4b-4g/startup.sh
+++ servers/raspberry-pi-4b-4g/startup.sh
@@ -10,3 +10,6 @@ sh "$git_dir/.utils/startup-helpers/rfkill.sh"
 seq 1 255 | while read -r i; do
     sh "$git_dir/.utils/startup-helpers/ethbridge.sh" "ethbridge-$i" "10.1.16.$i"
 done
+
+sh "$git_dir/.utils/startup-helpers/unbound.sh" "$git_dir/server/.current/unbound-default.conf"
+sh "$git_dir/.utils/startup-helpers/unbound.sh" "$git_dir/server/.current/unbound-open.conf"
