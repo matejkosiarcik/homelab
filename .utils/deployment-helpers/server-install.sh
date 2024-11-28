1516c9ee57a4127e75ebecbb9f93fe3899ae22c3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-11-28 21:31:41 +0100

    feat: expand server install

--- .utils/deployment-helpers/server-install.sh
+++ .utils/deployment-helpers/server-install.sh
@@ -19,20 +19,21 @@ fi
 ### Unbound config ###
 
 sudo killall unbound
+sudo mkdir -p /root/config
 
-if [ "$(sudo find "$server_dir/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
+if [ "$(sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
     printf 'Remove old unbound configs\n' >&2
-    sudo find '/root' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
+    sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
         sudo rm -f "$file"
     done
 fi
 if [ "$(find "$server_dir/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
     printf 'Copy new unbound configs\n' >&2
     find "$server_dir/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
-        sudo cp "$file" "/root/$(basename "$file")"
-        sudo chown root:root "/root/$(basename "$file")"
+        sudo cp "$file" "/root/config/$(basename "$file")"
+        sudo chown root:root "/root/config/$(basename "$file")"
     done
 fi
 
 tmpfile="$(mktemp)"
-sudo find '/root' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | sed -E 's~$~ rw,~' | sudo sponge /etc/apparmor.d/local/usr.sbin.unbound
+sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | sed -E 's~$~ rw,~' | sudo sponge /etc/apparmor.d/local/usr.sbin.unbound
