ea4557e3b0cf90873d4798cfc804d209260c5294
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-11-28 22:42:23 +0100

    feat: move unbound configs into regular user home directory

--- .utils/deployment-helpers/server-install.sh
+++ .utils/deployment-helpers/server-install.sh
@@ -23,19 +23,19 @@ fi
 sudo killall unbound
 sudo mkdir -p /root/config
 
-if [ "$(sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
+if [ "$(find "$HOME/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
     printf 'Remove old unbound configs\n' >&2
-    sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
+    sudo find "$HOME/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
         sudo rm -f "$file"
     done
 fi
 if [ "$(find "$server_dir/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
     printf 'Copy new unbound configs\n' >&2
     find "$server_dir/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
-        sudo cp "$file" "/root/config/$(basename "$file")"
-        sudo chown root:root "/root/config/$(basename "$file")"
+        cp "$file" "$HOME/config/$(basename "$file")"
+        sudo chown root:root "$HOME/config/config/$(basename "$file")"
     done
 fi
 
-sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | sed -E 's~$~ rw,~' | sudo sponge /etc/apparmor.d/local/usr.sbin.unbound
+sudo find "$HOME/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | sed -E 's~$~ rw,~' | sudo sponge /etc/apparmor.d/local/usr.sbin.unbound
 sudo systemctl restart apparmor
