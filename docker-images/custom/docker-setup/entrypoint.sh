4cfa20ae9a318637cd3b23e4c6825f5f1186ee13
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-12-01 16:21:41 +0100

    chore: debug output for pihole-setup

--- docker-images/custom/docker-setup/entrypoint.sh
+++ docker-images/custom/docker-setup/entrypoint.sh
@@ -4,8 +4,6 @@ set -euf
 mkdir -p /homelab/.internal
 printf 'starting\n' >/homelab/.internal/status.txt
 
-sleep 5
-
 # Wait for target container to start
 timeout 50s sh <<EOF
 if [ "$(docker ps --quiet --filter "name=^$HOMELAB_SETUP_TARGET_CONTAINER\$" --filter "status=running" | wc -l)" -eq '1' ]; then
@@ -14,12 +12,13 @@ if [ "$(docker ps --quiet --filter "name=^$HOMELAB_SETUP_TARGET_CONTAINER\$" --f
 fi
 printf 'Waiting for container %s\n' "$HOMELAB_SETUP_TARGET_CONTAINER" >&2
 while [ "$(docker ps --quiet --filter "name=^$HOMELAB_SETUP_TARGET_CONTAINER\$" --filter "status=running" | wc -l)" -ne '1' ]; do
+    printf 'Container %s in cycle not found\n' "$HOMELAB_SETUP_TARGET_CONTAINER" >&2
     sleep 1
 done
 printf 'Found container %s after waiting\n' "$HOMELAB_SETUP_TARGET_CONTAINER" >&2
 EOF
 
-sleep 10
+sleep 5
 
 printf '%s - Starting setup\n' "$(date '+%Y-%m-%d_%H-%M-%S')"
 docker exec "$HOMELAB_SETUP_TARGET_CONTAINER" sh /homelab/setup.sh
