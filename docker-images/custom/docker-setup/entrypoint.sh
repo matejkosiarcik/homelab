6d7c6764ed7986c79a409b085143cadfb560f726
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-12-01 14:04:06 +0100

    fix: make pihole-setup script more reliable

--- docker-images/custom/docker-setup/entrypoint.sh
+++ docker-images/custom/docker-setup/entrypoint.sh
@@ -5,17 +5,19 @@ mkdir -p /homelab/.internal
 printf 'starting\n' >/homelab/.internal/status.txt
 
 # Wait for target container to start
-timeout 30s sh <<EOF
-if [ "$(docker ps --filter "name=^$HOMELAB_SETUP_TARGET_CONTAINER\$" | grep -c -- "$HOMELAB_SETUP_TARGET_CONTAINER" || true)" -ne '0' ]; then
+timeout 45s sh <<EOF
+if [ "$(docker ps --filter "name=^$HOMELAB_SETUP_TARGET_CONTAINER\$" --filter "status=running" | grep -c -- "$HOMELAB_SETUP_TARGET_CONTAINER" || true)" -eq '1' ]; then
     return 0
 fi
 printf 'Waiting for container %s\n' "$HOMELAB_SETUP_TARGET_CONTAINER" >&2
-while [ "$(docker ps --filter "name=^$HOMELAB_SETUP_TARGET_CONTAINER\$" | grep -c -- "$HOMELAB_SETUP_TARGET_CONTAINER" || true)" -eq '0' ]; do
+while [ "$(docker ps --filter "name=^$HOMELAB_SETUP_TARGET_CONTAINER\$" --filter "status=running" | grep -c -- "$HOMELAB_SETUP_TARGET_CONTAINER" || true)" -ne '1' ]; do
     sleep 1
 done
 printf 'Container found\n' >&2
 sleep 1
 EOF
+
+sleep 5
 
 printf '%s - Starting setup\n' "$(date '+%Y-%m-%d_%H-%M-%S')"
 docker exec "$HOMELAB_SETUP_TARGET_CONTAINER" sh /homelab/setup.sh
