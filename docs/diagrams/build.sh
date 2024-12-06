c032f787b38c37545cce4d535f5cd7a3be09ef9f
Matej Košiarčik <matej.kosiarcik@gmail.com>
2024-12-06 22:50:28 +0100

    docs: prepare dark more for mermaid diagrams

--- docs/diagrams/build.sh
+++ docs/diagrams/build.sh
@@ -7,10 +7,12 @@ else
     only_diagram="$1"
 fi
 
+diagrams_dir="$(dirname "$0")"
+
 build_diagram() {
     # $1 - diagram name
-    mkdir -p "$(dirname "out/$1")"
-    mmdc --scale 2 --input "src/$1.mmd" --output "out/$1.png"
+    mkdir -p "$(dirname "$diagrams_dir/out/$1")"
+    mmdc --scale 2 --input "$diagrams_dir/src/$1.mmd" --output "$diagrams_dir/out/$1.png" --cssFile "$diagrams_dir/style.css"
 }
 
 if [ "$only_diagram" = '' ]; then
