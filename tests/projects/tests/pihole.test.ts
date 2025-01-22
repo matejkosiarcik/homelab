dbed255e924bea96b2524ed7676b3f7f2683a37d
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-01-22 23:05:00 +0100

    test: rework service tests

--- tests/projects/tests/pihole.test.ts
+++ tests/projects/tests/pihole.test.ts
@@ -3,45 +3,32 @@ import axios from 'axios';
 import { expect, test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import { getEnv } from '../../utils/utils';
-import { apps } from '../../utils/instances';
+import { apps } from '../../utils/apps';
 
-test.describe('PiHole', () => {
-    for (const pihole of apps.pihole) {
-        const piholeKey = URL.parse(pihole.url)!.hostname.replace(/\..*$/, '').replaceAll('-', '_').toUpperCase();
+test.describe(apps.pihole.title, () => {
+    for (const instance of apps.pihole.instances) {
+        const piholeKey = URL.parse(instance.url)!.hostname.replace(/\..*$/, '').replaceAll('-', '_').toUpperCase();
 
-        test.describe(pihole.title, () => {
-            test('Successful login', async ({ page }) => {
-                await page.goto(pihole.url);
-                await page.waitForURL(`${pihole.url}/admin/login.php`);
+        test.describe(instance.title, () => {
+            test('UI: Successful login', async ({ page }) => {
+                await page.goto(instance.url);
+                await page.waitForURL(`${instance.url}/admin/login.php`);
                 await page.locator('form#loginform input#loginpw').fill(getEnv(`${piholeKey}_PASSWORD`));
                 await page.locator('form#loginform button[type=submit]').click();
                 await page.waitForURL(/\/admin(?:\/?|\/index\.php)$/);
             });
 
-            test('Unsuccessful login', async ({ page }) => {
-                await page.goto(pihole.url);
-                await page.waitForURL(`${pihole.url}/admin/login.php`);
+            test('UI: Unsuccessful login', async ({ page }) => {
+                await page.goto(instance.url);
+                await page.waitForURL(`${instance.url}/admin/login.php`);
                 await page.locator('form#loginform input#loginpw').fill(faker.string.alpha(10));
                 await page.locator('form#loginform button[type=submit]').click();
                 await page.waitForSelector('.login-box-msg.has-error >> text="Wrong password!"', { timeout: 10_000 });
-                expect(page.url()).toStrictEqual(`${pihole.url}/admin/login.php`);
+                expect(page.url()).toStrictEqual(`${instance.url}/admin/login.php`);
             });
 
-            test('Query Unsecure HTTP', async () => {
-                const response = await axios.get(pihole.url.replace(/^https/, 'http'), { httpsAgent: new https.Agent({ rejectUnauthorized: false }), maxRedirects: 0, validateStatus: () => true });
-                expect(response.status, 'Response Status').toStrictEqual(302);
-                expect(response.headers['location'], 'Header Location').toStrictEqual(pihole.url);
-            });
-
-            test('Query Unsecure HTTP with random subpage', async () => {
-                const subpage = `/${faker.string.alpha(10)}`;
-                const response = await axios.get(`${pihole.url.replace(/^https/, 'http')}${subpage}`, { httpsAgent: new https.Agent({ rejectUnauthorized: false }), maxRedirects: 0, validateStatus: () => true });
-                expect(response.status, 'Response Status').toStrictEqual(302);
-                expect(response.headers['location'], 'Header Location').toStrictEqual(`${pihole.url}${subpage}`);
-            });
-
-            test('Query HTTPS', async () => {
-                const response = await axios.get(pihole.url, { httpsAgent: new https.Agent({ rejectUnauthorized: false }), maxRedirects: 999 });
+            test('API: HTTPS root', async () => {
+                const response = await axios.get(instance.url, { httpsAgent: new https.Agent({ rejectUnauthorized: false }), maxRedirects: 999 });
                 expect(response.status, 'Response Status').toStrictEqual(200);
             });
         });
