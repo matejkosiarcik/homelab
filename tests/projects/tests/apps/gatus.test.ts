836052c25997a4b88b1c964f3f0d44aa2c2935f7
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-01-23 21:35:00 +0100

    test: add tests for gatus

---
+++ tests/projects/tests/apps/gatus.test.ts
@@ -0,0 +1,97 @@
+import https from 'node:https';
+import axios from 'axios';
+import { expect, test } from '@playwright/test';
+import { apps } from '../../../utils/apps';
+import { getEnv } from '@/utils/utils';
+import { faker } from '@faker-js/faker';
+
+test.describe(apps.gatus.title, () => {
+    for (const instance of apps.gatus.instances) {
+        const gatusEnv = URL.parse(instance.url)!.hostname.replace(/\..*$/, '').replaceAll('-', '_').toUpperCase();
+
+        test.describe(instance.title, () => {
+            test('UI: Open', async ({ page }) => {
+                await page.goto(instance.url);
+            });
+
+            test('API: HTTPS root', async () => {
+                const response = await axios.get(instance.url, {
+                    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+                    maxRedirects: 999,
+                    validateStatus: () => true
+                });
+                expect(response.status, 'Response Status').toStrictEqual(200);
+            });
+
+            const proxyStatusVariants = [
+                {
+                    title: 'missing credentials',
+                    auth: undefined as unknown as { username: string, password: string },
+                    status: 401,
+                },
+                {
+                    title: 'wrong credentials',
+                    auth: {
+                        username: 'proxy-status',
+                        password: faker.string.alphanumeric(10),
+                    },
+                    status: 401,
+                },
+                {
+                    title: 'successful',
+                    auth: {
+                        username: 'proxy-status',
+                        password: getEnv(`${gatusEnv}_PROXY_STATUS_PASSWORD`),
+                    },
+                    status: 200,
+                },
+            ];
+            for (const variant of proxyStatusVariants) {
+                test(`API: Proxy status (${variant.title})`, async () => {
+                    const response = await axios.get(`${instance.url}/.apache/status`, {
+                        auth: variant.auth,
+                        maxRedirects: 999,
+                        validateStatus: () => true,
+                        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+                    });
+                    expect(response.status, 'Response Status').toStrictEqual(variant.status);
+                });
+            }
+
+            const prometheusVariants = [
+                {
+                    title: 'missing credentials',
+                    auth: undefined as unknown as { username: string, password: string },
+                    status: 401,
+                },
+                {
+                    title: 'wrong credentials',
+                    auth: {
+                        username: 'prometheus',
+                        password: faker.string.alphanumeric(10),
+                    },
+                    status: 401,
+                },
+                {
+                    title: 'successful',
+                    auth: {
+                        username: 'prometheus',
+                        password: getEnv(`${gatusEnv}_PROMETHEUS_PASSWORD`),
+                    },
+                    status: 200,
+                },
+            ];
+            for (const variant of prometheusVariants) {
+                test(`API: Prometheus metrics (${variant.title})`, async () => {
+                    const response = await axios.get(`${instance.url}/metrics`, {
+                        auth: variant.auth,
+                        maxRedirects: 999,
+                        validateStatus: () => true,
+                        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+                    });
+                    expect(response.status, 'Response Status').toStrictEqual(variant.status);
+                });
+            }
+        });
+    }
+});
