f2fa10b78b388a77bc0d664b19e5814cc2817052
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-02 22:33:48 +0100

    chore: remove playwright script for speedtest-tracker APP_KEY

--- .utils/deployment-helpers/.create-secrets-helpers/playwright/postinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -euf
-
-rm -f "$PWD/src/.utils"
-ln -f -s "$(git rev-parse --show-toplevel)/docker-images/custom/web-automation/src/.utils" "$PWD/src/.utils"
-
-if [ "${PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD-0}" != '1' ]; then
-    npx playwright install chromium
-fi
