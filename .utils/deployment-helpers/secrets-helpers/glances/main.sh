9632152fb1fd509f7d5f1d94334c92a1064ce07a
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-05 19:34:54 +0100

    feat: update secrets scripts for glances and dozzle

--- .utils/deployment-helpers/secrets-helpers/glances/main.sh
+++ .utils/deployment-helpers/secrets-helpers/glances/main.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 set -euf
 
+currdir="$(dirname "$0")"
+
 if [ "$#" -lt 2 ]; then
     printf 'Not enough arguments\n' >&2
-    printf 'Required: <input-file> <output-file>\n' >&2
+    printf 'Required: <password> <output-file>\n' >&2
     exit 1
 fi
-glances_password_input_file="$1"
-glances_password_output_file="$2"
+password="$1"
+output_file="$2"
 
-glances_script_file="$(tail -n +2 <"$(dirname "$0")/entrypoint.sh")"
-docker run -e "PASSWORD=$(cat "$glances_password_input_file")" --rm --entrypoint sh nicolargo/glances:latest-full -c "$glances_script_file" | tail -n 1 >"$glances_password_output_file"
+glances_script_file="$(tail -n +2 <"$currdir/entrypoint.sh")"
+docker run --rm --env "PASSWORD=$password" --entrypoint sh nicolargo/glances:latest-full -c "$glances_script_file" | tail -n 1 | sed -E 's~^.+: ~~' >"$output_file"
