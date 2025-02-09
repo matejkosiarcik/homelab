61515cf2b15af44e926d6e3157de14d4952771c4
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-09 11:49:46 +0100

    test: add test for DNS resolution for AAAA entries

--- tests/projects/tests/apps/pihole.test.ts
+++ tests/projects/tests/apps/pihole.test.ts
@@ -68,18 +68,20 @@ test.describe(apps.pihole.title, () => {
             }
 
             for (const transportVariant of ['tcp', 'udp'] as const) {
-                test(`DNS: ${transportVariant.toUpperCase()}`, async () => {
-                    // Get domain for DNS server for a given variant
-                    const piholeDnsDomain = instance.url.replace(/^https?:\/\//, '');
+                for (const ipVariant of ['A', 'AAAA'] as const) {
+                    test(`DNS: ${transportVariant.toUpperCase()} ${ipVariant}`, async () => {
+                        // Get domain for DNS server for a given variant
+                        const piholeDnsDomain = instance.url.replace(/^https?:\/\//, '');
 
-                    // Get IP address
-                    const piholeDnsIps = await nodeDns.resolve(piholeDnsDomain);
-                    expect(piholeDnsIps, 'Pihole DNS address resolution').toHaveLength(1);
+                        // Get IP address
+                        const piholeDnsIps = await nodeDns.resolve(piholeDnsDomain);
+                        expect(piholeDnsIps, 'Pihole DNS address resolution').toHaveLength(1);
 
-                    // Resolved external domain
-                    const ips = await dnsLookup('example.com', transportVariant, 'A', piholeDnsIps[0]);
-                    expect(ips, 'Domain should be resolved').not.toHaveLength(0);
-                });
+                        // Resolved external domain
+                        const ips = await dnsLookup('example.com', transportVariant, ipVariant, piholeDnsIps[0]);
+                        expect(ips, 'Domain should be resolved').not.toHaveLength(0);
+                    });
+                }
             }
         });
     }
--- tests/projects/tests/apps/unbound.test.ts
+++ tests/projects/tests/apps/unbound.test.ts
@@ -10,18 +10,20 @@ test.describe(apps.unbound.title, () => {
         test.describe(instance.title, () => {
             for (const transportVariant of ['tcp', 'udp'] as const) {
                 for (const dnsVariant of ['default', 'open'] as const) {
-                    test(`DNS: ${transportVariant.toUpperCase()} ${_.capitalize(dnsVariant)}`, async () => {
-                        // Get domain for DNS server for a given variant
-                        const unboundDnsDomain = instance.url.replace(/\.(.+)$/, `-${dnsVariant}.$1`);
+                    for (const ipVariant of ['A', 'AAAA'] as const) {
+                        test(`DNS: ${transportVariant.toUpperCase()} ${ipVariant} ${_.capitalize(dnsVariant)}`, async () => {
+                            // Get domain for DNS server for a given variant
+                            const unboundDnsDomain = instance.url.replace(/\.(.+)$/, `-${dnsVariant}.$1`);
 
-                        // Get IP address
-                        const unboundDnsIps = await nodeDns.resolve(unboundDnsDomain);
-                        expect(unboundDnsIps, 'Pihole DNS address resolution').toHaveLength(1);
+                            // Get IP address
+                            const unboundDnsIps = await nodeDns.resolve(unboundDnsDomain);
+                            expect(unboundDnsIps, 'Pihole DNS address resolution').toHaveLength(1);
 
-                        // Resolve external domain
-                        const ips = await dnsLookup('example.com', transportVariant, 'A', unboundDnsIps[0]);
-                        expect(ips, 'Domain should be resolved').not.toHaveLength(0);
-                    });
+                            // Resolve external domain
+                            const ips = await dnsLookup('example.com', transportVariant, ipVariant, unboundDnsIps[0]);
+                            expect(ips, 'Domain should be resolved').not.toHaveLength(0);
+                        });
+                    }
                 }
             }
         });
