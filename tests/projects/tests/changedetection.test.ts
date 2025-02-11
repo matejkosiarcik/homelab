f725e23060e6533b3f8367fe107da5c0f1380832
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-11 20:19:49 +0100

    chore: add semicolon

--- tests/projects/tests/changedetection.test.ts
+++ tests/projects/tests/changedetection.test.ts
@@ -27,7 +27,7 @@ test.describe(apps.changedetection.title, () => {
                 await page.locator('form input[type="password"][name="password"]').fill(getEnv(instance.url, 'PASSWORD'));
                 await page.locator('form button[type="submit"]:has-text("Login")').click();
                 await page.waitForURL(instance.url);
-                await page.goto(`${instance.url}/settings#general`)
+                await page.goto(`${instance.url}/settings#general`);
                 expect(page.url()).toStrictEqual(`${instance.url}/settings#general`);
             });
 
