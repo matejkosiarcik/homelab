0c3a287c94fa39c9f67141b284d5f40e4623bbf8
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-16 15:01:29 +0100

    test: add tests for motioneye embed URL

--- tests/projects/tests/motioneye.test.ts
+++ tests/projects/tests/motioneye.test.ts
@@ -40,33 +40,54 @@ test.describe(apps.motioneye.title, () => {
             ];
             for (const variant of users) {
                 if (!variant.random) {
-                    test(`UI: Successful login - User ${variant.username}`, async ({ page }) => {
+                    test(`UI: Successful full login - User ${variant.username}`, async ({ page }) => {
                         await page.goto(instance.url);
                         await page.waitForURL(`${instance.url}/`);
                         await page.locator('form input[name="username"]').fill(variant.username);
                         await page.locator('form input[name="password"]').fill(getEnv(instance.url, `${variant.username}_PASSWORD`));
                         await page.locator('.button:has-text("Login")').click();
                         await page.waitForURL(`${instance.url}/`);
-                        await expect(page.locator('.page .camera-frame .camera')).toBeVisible();
+                        await expect(page.locator('img.camera')).toBeVisible();
+                        await expect(page.locator('.settings-top-bar .icon.settings-button')).toBeVisible();
+                    });
+
+                    test(`UI: Successful embed open - User ${variant.username}`, async ({ page }) => {
+                        await page.goto(`${instance.url}/picture/1/frame/`);
+                        await page.waitForURL(`${instance.url}/picture/1/frame/`);
+                        await page.locator('form input[name="username"]').fill(variant.username);
+                        await page.locator('form input[name="password"]').fill(getEnv(instance.url, `${variant.username}_PASSWORD`));
+                        await page.locator('.button:has-text("Login")').click();
+                        await page.waitForURL(`${instance.url}/picture/1/frame/`);
+                        await expect(page.locator('.admin .icon.settings-button')).not.toBeVisible();
+                        await expect(page.locator('img.camera')).toBeVisible();
                     });
                 }
 
                 if (variant.username === 'user') {
-                    test(`UI: Successful image open - User ${variant.username}`, async ({ page }) => {
+                    test(`UI: Successful stream open - User ${variant.username}`, async ({ page }) => {
                         await page.setExtraHTTPHeaders({ Authorization: `Basic ${Buffer.from(`${variant.username}:${getEnv(instance.url, `${variant.username}_PASSWORD`)}`).toString('base64')}` });
                         await page.goto(`${instance.url}:9081`, { waitUntil: 'commit' });
                         await expect(page.locator('body > img')).toBeVisible({ timeout: 5000 });
                     });
                 }
 
-                test(`UI: Unsuccessful login - ${variant.random ? 'Random user' : `User ${variant.username}`}`, async ({ page }) => {
+                test(`UI: Unsuccessful full login - ${variant.random ? 'Random user' : `User ${variant.username}`}`, async ({ page }) => {
                     await page.goto(instance.url);
                     await page.waitForURL(`${instance.url}/`);
                     await page.locator('form input[name="username"]').fill(variant.username);
                     await page.locator('form input[name="password"]').fill(faker.string.alpha(10));
                     await page.locator('.button:has-text("Login")').click();
                     await page.waitForURL(`${instance.url}/`);
-                    await expect(page.locator('.page .camera-frame .camera')).not.toBeVisible();
+                    await expect(page.locator('.login-dialog-error:has-text("Invalid credentials.")')).toBeVisible();
+                });
+
+                test(`UI: Unsuccessful embed open - ${variant.random ? 'Random user' : `User ${variant.username}`}`, async ({ page }) => {
+                    await page.goto(`${instance.url}/picture/1/frame/`);
+                    await page.waitForURL(`${instance.url}/picture/1/frame/`);
+                    await page.locator('form input[name="username"]').fill(variant.username);
+                    await page.locator('form input[name="password"]').fill(faker.string.alpha(10));
+                    await page.locator('.button:has-text("Login")').click();
+                    await page.waitForURL(`${instance.url}/picture/1/frame/`);
                     await expect(page.locator('.login-dialog-error:has-text("Invalid credentials.")')).toBeVisible();
                 });
 
@@ -93,7 +114,7 @@ test.describe(apps.motioneye.title, () => {
                 expect(response.status, 'Response Status').toStrictEqual(401);
             });
 
-            test(`API: Unsuccessful last picture open - No user`, async () => {
+            test(`API: Unsuccessful snapshot open - No user`, async () => {
                 const response = await axios.get(`${instance.url}/picture/1/current`, {
                     maxRedirects: 999,
                     validateStatus: () => true,
