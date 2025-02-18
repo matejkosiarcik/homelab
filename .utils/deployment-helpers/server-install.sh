3085ba77a0541a19509722bcc342815ccc8d3433
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-18 20:38:44 +0100

    fix: unbound config locations

--- .utils/deployment-helpers/server-install.sh
+++ .utils/deployment-helpers/server-install.sh
@@ -7,7 +7,7 @@ server_dir="$git_dir/servers/.current"
 ### General config ###
 
 mkdir -p "$HOME/config" "$HOME/.log"
-sudo mkdir -p /root/config /root/.log
+sudo mkdir -p /root/config/unbound /root/.log
 
 if [ -f "$server_dir/config/startup.sh" ]; then
     printf 'Copy startup script\n' >&2
@@ -23,17 +23,17 @@ fi
 
 sudo killall unbound || true
 
-if [ "$(sudo find "/root/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
+if [ "$(sudo find "/root/config/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
     printf 'Remove old unbound configs\n' >&2
     sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
         sudo rm -f "$file"
     done
 fi
-if [ "$(find "$server_dir/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
+if [ "$((find "$server_dir/other-apps/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' || true) | wc -l)" -ge '1' ]; then
     printf 'Copy new unbound configs\n' >&2
-    find "$server_dir/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
-        sudo cp "$file" "/root/config/$(basename "$file")"
-        sudo chown root:root "/root/config/$(basename "$file")"
+    find "$server_dir/other-apps/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
+        sudo cp "$file" "/root/config/unbound/$(basename "$file")"
+        sudo chown root:root "/root/config/unbound/$(basename "$file")"
     done
     if [ -f '/etc/apparmor.d/local/usr.sbin.unbound' ]; then
         sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | sed -E 's~$~ rw,~' | sudo sponge '/etc/apparmor.d/local/usr.sbin.unbound'
--- .utils/startup-helpers/unbound.sh
+++ .utils/startup-helpers/unbound.sh
@@ -8,7 +8,7 @@ if [ "$#" -lt 1 ]; then
 fi
 
 config_file="$1"
-log_file="/root/.log/$(basename "$config_file" .conf).log"
+log_file="/root/.log/unbound/$(basename "$config_file" .conf).log"
 
 sudo mkdir -p "$(dirname "$log_file")"
 sudo rm -f "$log_file"
@@ -16,4 +16,4 @@ sudo touch "$log_file"
 sudo chown root:root "$log_file"
 sudo chmod a+rw "$log_file"
 
-sudo unbound -v -c "/root/config/$(basename "$config_file")" &
+sudo unbound -v -c "/root/config/unbound/$(basename "$config_file")" &
