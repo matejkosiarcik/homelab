7e64a0996accacd91cb56a7afb2fe134acc6f3db
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-18 19:45:13 +0100

    feat: move unbound configs to new other-apps directory

--- servers/raspberry-pi-4b-4g/config/startup-always.sh
+++ servers/raspberry-pi-4b-4g/config/startup-always.sh
@@ -3,6 +3,6 @@ set -euf
 
 git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel 2>/dev/null || printf '%s\n' "$HOME/git/homelab")"
 
-find "$git_dir/servers/.current/config" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
+(find "$git_dir/servers/.current/other-apps/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' || true) | while read -r file; do
     sh "$git_dir/.utils/startup-helpers/unbound.sh" "$(basename "$file")"
 done
