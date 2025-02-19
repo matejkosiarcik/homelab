0c4d7fc63d364db15011fba12cce8b17370fb5e2
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-19 19:51:42 +0100

    feat: change location for system-wide unbound install location

--- .utils/deployment-helpers/server-install.sh
+++ .utils/deployment-helpers/server-install.sh
@@ -7,10 +7,12 @@ server_dir="$git_dir/servers/.current"
 ### General config ###
 
 mkdir -p "$HOME/config" "$HOME/.log"
-sudo mkdir -p /root/config /root/.log
+sudo mkdir -p /homelab/config /homelab/log
+sudo chmod a+rwx /homelab /homelab/config /homelab/log # TODO: Remove permissions after homelab user is created
 
 if [ "$( (find "$server_dir/other-apps/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' || true) | wc -l)" -ge '1' ]; then
-    sudo mkdir -p /root/config/unbound
+    sudo mkdir -p /homelab/config/unbound /homelab/log/unbound
+    sudo chmod a+rwx /homelab/config/unbound /homelab/log/unbound # TODO: Remove permissions after homelab user is created
 fi
 
 if [ -f "$server_dir/config/startup.sh" ]; then
@@ -27,21 +29,21 @@ fi
 
 sudo killall unbound || true
 
-if [ "$(sudo find "/root/config/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
+if [ "$(sudo find "/homelab/config/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | wc -l)" -ge '1' ]; then
     printf 'Remove old unbound configs\n' >&2
-    sudo find '/root/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
+    sudo find '/homelab/config' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
         sudo rm -f "$file"
     done
 fi
 if [ "$( (find "$server_dir/other-apps/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' || true) | wc -l)" -ge '1' ]; then
     printf 'Copy new unbound configs\n' >&2
     find "$server_dir/other-apps/unbound" -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | while read -r file; do
-        sudo cp "$file" "/root/config/unbound/$(basename "$file")"
-        sudo chown root:root "/root/config/unbound/$(basename "$file")"
-        sudo chmod a+rw "/root/config/unbound/$(basename "$file")"
+        sudo cp "$file" "/homelab/config/unbound/$(basename "$file")"
+        sudo chown root:root "/homelab/config/unbound/$(basename "$file")"
+        sudo chmod a+rw "/homelab/config/unbound/$(basename "$file")"
     done
     if [ -f '/etc/apparmor.d/local/usr.sbin.unbound' ]; then
-        sudo find '/root/config/unbound' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | sed -E 's~$~ rw,~' | sudo sponge '/etc/apparmor.d/local/usr.sbin.unbound'
+        sudo find '/homelab/config/unbound' -mindepth 1 -maxdepth 1 -type f -name 'unbound-*.conf' | sed -E 's~$~ rw,~' | sudo sponge '/etc/apparmor.d/local/usr.sbin.unbound'
+        sudo systemctl restart apparmor
     fi
-    sudo systemctl restart apparmor
 fi
--- .utils/startup-helpers/unbound.sh
+++ .utils/startup-helpers/unbound.sh
@@ -7,20 +7,18 @@ if [ "$#" -lt 1 ]; then
     exit 1
 fi
 
-sudo mkdir -p /root/config/unbound /root/.log/unbound
-
 config_file="$1"
 
-log_file="/root/.log/unbound/$(basename "$config_file" .conf).log"
-sudo mkdir -p "$(dirname "$log_file")"
+log_file="/homelab/log/unbound/$(basename "$config_file" .conf).log"
 sudo rm -f "$log_file"
 sudo touch "$log_file"
-sudo chown root:root "$log_file"
-sudo chmod a+rw "$log_file"
+sudo chown root:root "$log_file" # TODO: Remove permissions after homelab user is created
+sudo chmod a+rw "$log_file" # TODO: Remove permissions after homelab user is created
 
-socket_file="/root/config/unbound/$(basename "$config_file" .conf).sock"
+socket_file="/homelab/config/unbound/$(basename "$config_file" .conf).sock"
+sudo rm -f "$log_file"
 sudo touch "$socket_file"
-sudo chown root:root "$socket_file"
-sudo chmod a+rwx "$socket_file"
+sudo chown root:root "$socket_file" # TODO: Remove permissions after homelab user is created
+sudo chmod a+rwx "$socket_file" # TODO: Remove permissions after homelab user is created
 
-sudo unbound -v -c "/root/config/unbound/$(basename "$config_file")" &
+sudo unbound -v -c "/homelab/config/unbound/$(basename "$config_file")" &
