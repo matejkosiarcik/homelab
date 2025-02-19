25b71a785221938ab798e054369bc6a44bbe52c9
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-02-19 20:00:34 +0100

    chore: format shell errors

--- .utils/startup-helpers/unbound.sh
+++ .utils/startup-helpers/unbound.sh
@@ -13,12 +13,12 @@ log_file="/homelab/log/unbound/$(basename "$config_file" .conf).log"
 sudo rm -f "$log_file"
 sudo touch "$log_file"
 sudo chown root:root "$log_file" # TODO: Remove permissions after homelab user is created
-sudo chmod a+rw "$log_file" # TODO: Remove permissions after homelab user is created
+sudo chmod a+rw "$log_file"      # TODO: Remove permissions after homelab user is created
 
 socket_file="/homelab/config/unbound/$(basename "$config_file" .conf).sock"
 sudo rm -f "$log_file"
 sudo touch "$socket_file"
 sudo chown root:root "$socket_file" # TODO: Remove permissions after homelab user is created
-sudo chmod a+rwx "$socket_file" # TODO: Remove permissions after homelab user is created
+sudo chmod a+rwx "$socket_file"     # TODO: Remove permissions after homelab user is created
 
 sudo unbound -v -c "/homelab/config/unbound/$(basename "$config_file")" &
