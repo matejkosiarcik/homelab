8056d0aa33963f3afec831042a9e9b2ea9c19b5d
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-03-09 19:48:18 +0100

    test: remove proxy tests for openspeedtest

--- tests/projects/tests/openspeedtest.test.ts
+++ tests/projects/tests/openspeedtest.test.ts
@@ -1,12 +1,12 @@
 import { expect, test } from '@playwright/test';
 import { apps } from '../../utils/apps';
-import { createApiRootTest, createProxyTests, createTcpTest } from '../../utils/tests';
+import { createApiRootTest, createTcpTest } from '../../utils/tests';
 
 test.describe(apps.openspeedtest.title, () => {
     for (const instance of apps.openspeedtest.instances) {
         test.describe(instance.title, () => {
             // NOTE: HTTP->HTTPS not tested because redirect is disable because of speed variance
-            createProxyTests(instance.url);
+            // Proxy tests are skipped, because openspeedtest has no proxy
             createApiRootTest(instance.url);
 
             for (const port of [80, 443]) {
