638519176721a1770f20d10082e1e432db436b0d
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-03-12 18:32:20 +0100

    test: prepare secrets for prometheus app

--- tests/secrets.sh
+++ tests/secrets.sh
@@ -140,6 +140,8 @@ load_password() {
     printf 'PIHOLE_2_SECONDARY_PROXY_STATUS_PASSWORD=%s\n' "$(load_password pihole-2-secondary http-proxy status)"
     printf 'PIHOLE_2_SECONDARY_PROXY_PROMETHEUS_PASSWORD=%s\n' "$(load_password pihole-2-secondary http-proxy prometheus)"
 
+    printf 'PROMETHEUS_ADMIN_PASSWORD=%s\n' "$(load_password prometheus app admin)"
+    printf 'PROMETHEUS_PROMETHEUS_PASSWORD=%s\n' "$(load_password prometheus app admin)"
     printf 'PROMETHEUS_PROXY_STATUS_PASSWORD=%s\n' "$(load_password prometheus http-proxy status)"
     printf 'PROMETHEUS_PROXY_PROMETHEUS_PASSWORD=%s\n' "$(load_password prometheus http-proxy prometheus)"
 
