d88d95bc0f9ed4642241516812f938edab36ebcd
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-03-15 11:05:35 +0100

    test: improve gatus test reliability

--- tests/projects/tests/gatus.test.ts
+++ tests/projects/tests/gatus.test.ts
@@ -56,20 +56,22 @@ test.describe(apps.gatus.title, () => {
                     test(`UI: Successful open - User ${variant.username}`, async ({ page }) => {
                         await page.setExtraHTTPHeaders({ Authorization: `Basic ${Buffer.from(`${variant.username}:${getEnv(instance.url, 'ADMIN_PASSWORD')}`).toString('base64')}` });
                         await page.goto(instance.url);
-                        await expect(page.locator('#results .endpoint-group').first()).toBeVisible({ timeout: 10_000 });
+                        await expect(page.locator('.animate-spin')).toBeVisible();
+                        await expect(page.locator('.animate-spin')).not.toBeVisible({ timeout: 20_000 });
+                        await expect(page.locator('#results .endpoint-group').first()).toBeVisible();
                     });
                 }
 
                 test(`UI: Unsuccessful open - ${variant.random ? 'Random user' : `User ${variant.username}`}`, async ({ page }) => {
                     await page.setExtraHTTPHeaders({ Authorization: `Basic ${Buffer.from(`${variant.username}:${faker.string.alphanumeric(10)}`).toString('base64')}` });
                     await page.goto(instance.url);
-                    await expect(page.locator('#results .endpoint-group').first()).not.toBeVisible({ timeout: 10_000 });
+                    await expect(page.locator('#results .endpoint-group').first()).not.toBeVisible();
                 });
             }
 
             test('UI: Unsuccessful open - No user', async ({ page }) => {
                 await page.goto(instance.url);
-                await expect(page.locator('#results .endpoint-group').first()).not.toBeVisible({ timeout: 10_000 });
+                await expect(page.locator('#results .endpoint-group').first()).not.toBeVisible();
             });
         });
     }
