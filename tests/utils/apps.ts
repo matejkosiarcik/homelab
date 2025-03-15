6ab6f19e7da31741f75d3fb99e30223bbb5a0314
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-03-15 18:23:40 +0100

    feat: remove glances from raspberry-pi-3b

--- tests/utils/apps.ts
+++ tests/utils/apps.ts
@@ -44,7 +44,6 @@ export const apps = {
         title: 'Glances',
         instances: [
             { url: 'https://glances-odroid-h3.home', title: 'Glances - Odroid H3' },
-            { url: 'https://glances-raspberry-pi-3b.home', title: 'Glances - Raspberry Pi 3B' },
             { url: 'https://glances-raspberry-pi-4b-2g.home', title: 'Glances - Raspberry Pi 4B 2GB' },
             { url: 'https://glances-raspberry-pi-4b-4g.home', title: 'Glances - Raspberry Pi 4B 4GB' },
         ],
