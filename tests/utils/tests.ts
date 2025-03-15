769b1975ec4b26dd166b90ab7c84f8becb23c1c4
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-03-15 10:57:34 +0100

    test: format test file

--- tests/utils/tests.ts
+++ tests/utils/tests.ts
@@ -7,7 +7,7 @@ import { axios, getEnv } from './utils';
 export function createTcpTest(url: string, port: number, subtitle?: string | undefined) {
     return test(`TCP: Connect to port ${port}${subtitle ? ` ${subtitle}` : ''}`, async () => {
         const host = url.replace(/^.*?:\/\//, '');
-    const socket = new net.Socket();
+        const socket = new net.Socket();
         const promiseSocket = new PromiseSocket(socket);
         await promiseSocket.connect(port, host);
         await promiseSocket.end();
