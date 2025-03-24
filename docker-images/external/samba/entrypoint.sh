c24004eb91a51e59012599233ecc321929cfbd71
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-03-24 20:01:54 +0100

    fix: handle existing samba group/user in entrypoint

--- docker-images/external/samba/entrypoint.sh
+++ docker-images/external/samba/entrypoint.sh
@@ -6,9 +6,13 @@ samba_file="/homelab/smb/smb-$SAMBA_CONFIG.conf"
 
 sed "s~#smb-title#~$SAMBA_TITLE~;s~#smb-user#~$SAMBA_USERNAME~;s~#smb-group#~$SAMBA_GROUP~" <"$samba_file" >/etc/samba/smb.conf
 
-groupadd "$SAMBA_GROUP"
-useradd -M -s /sbin/nologin "$SAMBA_USERNAME"
-usermod -a -G "$SAMBA_GROUP" "$SAMBA_USERNAME"
+if ! grep -Eq "^$SAMBA_GROUP:" /etc/group; then
+    groupadd "$SAMBA_GROUP"
+fi
+if ! grep -Eq "^$SAMBA_USERNAME:" /etc/passwd; then
+    useradd -M -s /sbin/nologin "$SAMBA_USERNAME"
+    usermod -a -G "$SAMBA_GROUP" "$SAMBA_USERNAME"
+fi
 printf '%s\n%s\n' "$SAMBA_PASSWORD" "$SAMBA_PASSWORD" | smbpasswd -s -a "$SAMBA_USERNAME"
 
 testparm -s
