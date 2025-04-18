cb52fba2cec7467975e2e90a67d46b1f8c93b896
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-04-18 21:00:38 +0200

    feat: prepare cron jobs for server updates

---
+++ servers/macbook-pro-2012/config/update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -euf
+
+cd "$(dirname "$(dirname "$(realpath "$0")")")" >/dev/null 2>&1
+git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel 2>/dev/null || printf '%s\n' "$HOME/git/homelab")"
+
+sh "$git_dir/.utils/others/update.sh"
