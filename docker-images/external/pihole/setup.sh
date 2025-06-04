2da79eb95fc29eb18c04ddf5782e4e6eebef16f0
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-06-04 22:56:07 +0200

    feat: guard against locked database in pihole script

--- docker-images/external/pihole/setup.sh
+++ docker-images/external/pihole/setup.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
 set -euf
 
+sql() {
+    command="$1"
+    i=0
+    while [ "$i" -lt '3' ]; do
+        i="$((i + 1))"
+        status='0'
+        pihole-FTL sqlite3 /etc/pihole/gravity.db "$command" || {
+            status="$?"
+            # Guard against "Error: stepping, database is locked (5)"
+            if [ "$status" = '5' ]; then
+                sleep 1
+                continue
+            fi
+        }
+        if [ "$status" -eq '0' ]; then
+            break
+        fi
+        if [ "$i" -ge '3' ] && [ "$status" -ne '0' ]; then
+            printf 'There was an error during SQL command %s\n' "$command" >&2
+            exit "$status"
+        fi
+    done
+}
+
 # Set custom local domains
 custom_domains="[$(sed -E 's~#.*$~~;s~  ~ ~g;s~^ +~~;s~ +$~~' </homelab/custom-domains.txt | grep -vE '^ *$' | sed -E 's~^(.*)$~"\1"~' | tr '\n' ',' | sed -E 's~,$~~;s~,~, ~g')]"
 printf 'Custom domains: %s\n' "$custom_domains"
@@ -20,7 +44,7 @@ printf 'Database found\n'
 # Wait for database tables to be ready
 db_log='0'
 while true; do
-    count="$(pihole-FTL sqlite3 /etc/pihole/gravity.db "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='gravity';")"
+    count="$(sql "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='gravity';")"
     if [ "$count" -gt '0' ]; then
         break
     fi
@@ -33,31 +57,31 @@ done
 printf 'Main table found\n'
 
 # Wipe other existing entities
-pihole-FTL sqlite3 /etc/pihole/gravity.db 'DELETE FROM client_by_group;'
-pihole-FTL sqlite3 /etc/pihole/gravity.db 'DELETE FROM [group] WHERE id!=0;'
-pihole-FTL sqlite3 /etc/pihole/gravity.db 'DELETE FROM client;'
+sql 'DELETE FROM client_by_group;'
+sql 'DELETE FROM [group] WHERE id!=0;'
+sql 'DELETE FROM client;'
 
 # Add custom open group
 default_group_id='0'
-pihole-FTL sqlite3 /etc/pihole/gravity.db "INSERT INTO [group] (enabled, name, date_added, date_modified, description) VALUES (1, 'Open', 0, 0, 'The group without adblocking');"
-open_group_id="$(pihole-FTL sqlite3 /etc/pihole/gravity.db "SELECT id FROM [group] WHERE name='Open';")"
+sql "INSERT INTO [group] (enabled, name, date_added, date_modified, description) VALUES (1, 'Open', 0, 0, 'The group without adblocking');"
+open_group_id="$(sql "SELECT id FROM [group] WHERE name='Open';")"
 
 # Custom clients
 unbound_default_1_ip='10.1.10.1'
-pihole-FTL sqlite3 /etc/pihole/gravity.db "INSERT INTO client (ip, date_added, date_modified, comment) VALUES ('$unbound_default_1_ip', 0, 0, 'Unbound 1 default');"
-unbound_default_1_id="$(pihole-FTL sqlite3 /etc/pihole/gravity.db "SELECT id FROM client WHERE ip='$unbound_default_1_ip';")"
+sql "INSERT INTO client (ip, date_added, date_modified, comment) VALUES ('$unbound_default_1_ip', 0, 0, 'Unbound 1 default');"
+unbound_default_1_id="$(sql "SELECT id FROM client WHERE ip='$unbound_default_1_ip';")"
 unbound_open_1_ip='10.1.10.2'
-pihole-FTL sqlite3 /etc/pihole/gravity.db "INSERT INTO client (ip, date_added, date_modified, comment) VALUES ('$unbound_open_1_ip', 0, 0, 'Unbound 1 open');"
-unbound_open_1_id="$(pihole-FTL sqlite3 /etc/pihole/gravity.db "SELECT id FROM client WHERE ip='$unbound_open_1_ip';")"
+sql "INSERT INTO client (ip, date_added, date_modified, comment) VALUES ('$unbound_open_1_ip', 0, 0, 'Unbound 1 open');"
+unbound_open_1_id="$(sql "SELECT id FROM client WHERE ip='$unbound_open_1_ip';")"
 unbound_default_2_ip='10.1.16.1'
-pihole-FTL sqlite3 /etc/pihole/gravity.db "INSERT INTO client (ip, date_added, date_modified, comment) VALUES ('$unbound_default_2_ip', 0, 0, 'Unbound 2 default');"
-unbound_default_2_id="$(pihole-FTL sqlite3 /etc/pihole/gravity.db "SELECT id FROM client WHERE ip='$unbound_default_2_ip';")"
+sql "INSERT INTO client (ip, date_added, date_modified, comment) VALUES ('$unbound_default_2_ip', 0, 0, 'Unbound 2 default');"
+unbound_default_2_id="$(sql "SELECT id FROM client WHERE ip='$unbound_default_2_ip';")"
 unbound_open_2_ip='10.1.16.2'
-pihole-FTL sqlite3 /etc/pihole/gravity.db "INSERT INTO client (ip, date_added, date_modified, comment) VALUES ('$unbound_open_2_ip', 0, 0, 'Unbound 2 open');"
-unbound_open_2_id="$(pihole-FTL sqlite3 /etc/pihole/gravity.db "SELECT id FROM client WHERE ip='$unbound_open_2_ip';")"
+sql "INSERT INTO client (ip, date_added, date_modified, comment) VALUES ('$unbound_open_2_ip', 0, 0, 'Unbound 2 open');"
+unbound_open_2_id="$(sql "SELECT id FROM client WHERE ip='$unbound_open_2_ip';")"
 
 # Assign clients to groups
-pihole-FTL sqlite3 /etc/pihole/gravity.db "UPDATE client_by_group SET group_id=$default_group_id WHERE client_id=$unbound_default_1_id;"
-pihole-FTL sqlite3 /etc/pihole/gravity.db "UPDATE client_by_group SET group_id=$default_group_id WHERE client_id=$unbound_default_2_id;"
-pihole-FTL sqlite3 /etc/pihole/gravity.db "UPDATE client_by_group SET group_id=$open_group_id WHERE client_id=$unbound_open_1_id;"
-pihole-FTL sqlite3 /etc/pihole/gravity.db "UPDATE client_by_group SET group_id=$open_group_id WHERE client_id=$unbound_open_2_id;"
+sql "UPDATE client_by_group SET group_id=$default_group_id WHERE client_id=$unbound_default_1_id;"
+sql "UPDATE client_by_group SET group_id=$default_group_id WHERE client_id=$unbound_default_2_id;"
+sql "UPDATE client_by_group SET group_id=$open_group_id WHERE client_id=$unbound_open_1_id;"
+sql "UPDATE client_by_group SET group_id=$open_group_id WHERE client_id=$unbound_open_2_id;"
