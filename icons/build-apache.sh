fa79da6bd8b9b2ea3af337ea48395e14e7962877
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-06-04 21:54:08 +0200

    feat: add docker-cache-proxy and prometheus favicons

--- icons/build-apache.sh
+++ icons/build-apache.sh
@@ -20,16 +20,6 @@ default_convert_options='magick -background none INPUT_FILE -resize RESOLUTION -
 ### UniFi Controller ###
 convert_image_full "$input_dir/gitman-repositories/homer-icons/svg/ubiquiti.svg" "$output_dir/unifi-controller/favicon.png"
 
-### Unbound ###
-# PNG
-convert_image_full "$input_dir/gitman-repositories/dashboard-icons/svg/unbound.svg" "$output_dir/unbound/favicon.png"
-# ICO
-convert_image_draft 'magick -background none INPUT_FILE -resize 16x16 -density 1200 OUTPUT_FILE' "$output_dir/unbound/favicon.png" "$tmpdir/unbound-16.png"
-optimize_image "$tmpdir/unbound-16.png"
-convert_image_draft 'magick -background none INPUT_FILE -resize 32x32 -density 1200 OUTPUT_FILE' "$output_dir/unbound/favicon.png" "$tmpdir/unbound-32.png"
-optimize_image "$tmpdir/unbound-32.png"
-convert_ico "$tmpdir/unbound-16.png $tmpdir/unbound-32.png" "$output_dir/unbound/favicon.ico"
-
 ### Certbot ###
 # PNG
 magick -size "$default_image_size" xc:#ffffffef "$tmpdir/lets-encrypt-background.png"
@@ -45,6 +35,36 @@ optimize_image "$tmpdir/lets-encrypt-16.png"
 convert_image_draft 'magick -background none INPUT_FILE -resize 32x32 -density 1200 OUTPUT_FILE' "$output_dir/certbot/favicon.png" "$tmpdir/lets-encrypt-32.png"
 optimize_image "$tmpdir/lets-encrypt-32.png"
 convert_ico "$tmpdir/lets-encrypt-16.png $tmpdir/lets-encrypt-32.png" "$output_dir/certbot/favicon.ico"
+
+### Docker Cache Proxy ###
+# PNG
+convert_image_full "$input_dir/gitman-repositories/homer-icons/png/docker-moby.png" "$output_dir/docker-cache-proxy/favicon.png"
+# ICO
+convert_image_draft 'magick -background none INPUT_FILE -resize 16x16 -density 1200 OUTPUT_FILE' "$output_dir/docker-cache-proxy/favicon.png" "$tmpdir/docker-16.png"
+optimize_image "$tmpdir/docker-16.png"
+convert_image_draft 'magick -background none INPUT_FILE -resize 32x32 -density 1200 OUTPUT_FILE' "$output_dir/docker-cache-proxy/favicon.png" "$tmpdir/docker-32.png"
+optimize_image "$tmpdir/docker-32.png"
+convert_ico "$tmpdir/docker-16.png $tmpdir/docker-32.png" "$output_dir/docker-cache-proxy/favicon.ico"
+
+### Prometheus ###
+# PNG
+convert_image_full "$input_dir/gitman-repositories/homer-icons/svg/prometheus.svg" "$output_dir/prometheus/favicon.png"
+# ICO
+convert_image_draft 'magick -background none INPUT_FILE -resize 16x16 -density 1200 OUTPUT_FILE' "$output_dir/prometheus/favicon.png" "$tmpdir/prometheus-16.png"
+optimize_image "$tmpdir/prometheus-16.png"
+convert_image_draft 'magick -background none INPUT_FILE -resize 32x32 -density 1200 OUTPUT_FILE' "$output_dir/prometheus/favicon.png" "$tmpdir/prometheus-32.png"
+optimize_image "$tmpdir/prometheus-32.png"
+convert_ico "$tmpdir/prometheus-16.png $tmpdir/prometheus-32.png" "$output_dir/prometheus/favicon.ico"
+
+### Unbound ###
+# PNG
+convert_image_full "$input_dir/gitman-repositories/dashboard-icons/svg/unbound.svg" "$output_dir/unbound/favicon.png"
+# ICO
+convert_image_draft 'magick -background none INPUT_FILE -resize 16x16 -density 1200 OUTPUT_FILE' "$output_dir/unbound/favicon.png" "$tmpdir/unbound-16.png"
+optimize_image "$tmpdir/unbound-16.png"
+convert_image_draft 'magick -background none INPUT_FILE -resize 32x32 -density 1200 OUTPUT_FILE' "$output_dir/unbound/favicon.png" "$tmpdir/unbound-32.png"
+optimize_image "$tmpdir/unbound-32.png"
+convert_ico "$tmpdir/unbound-16.png $tmpdir/unbound-32.png" "$output_dir/unbound/favicon.ico"
 
 ### Cleanup ###
 
