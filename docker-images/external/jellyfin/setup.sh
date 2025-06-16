f1f26fbbc471d23297abc9bc47105f74c61f94a5
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-06-16 21:22:18 +0200

    chore: fix shell formatting

--- docker-images/external/jellyfin/setup.sh
+++ docker-images/external/jellyfin/setup.sh
@@ -7,9 +7,7 @@ done
 
 sleep 5
 
-cat '/homelab/data/system.xml' |
-    sed 's~<EnableMetrics>false</EnableMetrics>~<EnableMetrics>true</EnableMetrics>~' |
-    sponge '/homelab/data/system.xml'
+sed 's~<EnableMetrics>false</EnableMetrics>~<EnableMetrics>true</EnableMetrics>~' <'/homelab/data/system.xml' | sponge '/homelab/data/system.xml'
 
 if ! grep '<EnableMetrics>true</EnableMetrics>' <'/homelab/data/system.xml' >/dev/null; then
     printf 'Failed to enable metrics\n' >&2
