fae8f58f11fda57678283fac68a6acbd1f19dd23
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-06-22 19:20:25 +0200

    feat: add script to fill in healthchecks

---
+++ .utils/setup-helpers/healthchecks/main.ts
@@ -0,0 +1,138 @@
+import fs from 'node:fs';
+import fsx from 'node:fs/promises';
+import dotenv from 'dotenv';
+import axios from 'axios';
+import { assert } from 'simple-assert';
+
+type Healthcheck = {
+    grace: number,
+    name: string,
+    schedule: string,
+    slug: string,
+    uuid: string,
+};
+
+const existingHealthecks: Healthcheck[] = [];
+const createdHealthchecks: Partial<Healthcheck>[] = [];
+
+async function addHealthcheck(healthcheck: Healthcheck): Promise<void> {
+    const newHealthcheck: Partial<Healthcheck> = {
+        slug: healthcheck.slug,
+        name: healthcheck.name,
+        schedule: healthcheck.schedule,
+        grace: 600, // 10 minutes
+    };
+
+    const existingHealtheck = existingHealthecks.find((el) => el.slug === newHealthcheck.slug);
+    if (existingHealtheck) {
+        if (existingHealtheck.schedule !== newHealthcheck.schedule || existingHealtheck.grace !== newHealthcheck.grace || existingHealtheck.name !== newHealthcheck.name) {
+            await (async () => {
+                console.log(`Updating healthcheck ${newHealthcheck.slug}`);
+                const response = await axios.post(`/checks/${existingHealtheck.uuid}`, newHealthcheck);
+                assert(response.status === 200, `Failed to update healthcheck\nStatus: ${response.status}\nBody: ${JSON.stringify(response.data)}`);
+            })();
+        }
+        createdHealthchecks.push(newHealthcheck);
+        return;
+    }
+
+    await (async () => {
+        console.log(`Creating healthcheck ${newHealthcheck.slug}`);
+        const response = await axios.post('/checks/', newHealthcheck, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        assert(response.status === 201, `Failed to create healthcheck\nStatus: ${response.status}\nBody: ${JSON.stringify(response.data)}`);
+    })();
+    createdHealthchecks.push(newHealthcheck);
+}
+
+(async () => {
+    if (fs.existsSync('.secrets.env')) {
+        dotenv.config({ path: '.secrets.env' });
+    }
+
+    axios.defaults.headers.common['X-Api-Key'] = process.env['HEALTHCHECKS_API_KEY'] || '';
+    axios.defaults.baseURL = 'https://healthchecks.home.matejkosiarcik.com/api/v3';
+    axios.defaults.validateStatus = () => true;
+
+    await (async () => {
+        const response = await axios.get('/checks');
+        assert(response.status === 200, `Failed to fetch list of healthchecks\nStatus: ${response.status}\nBody: ${JSON.stringify(response.data)}`);
+        const body = response.data as { checks: Healthcheck[] };
+        existingHealthecks.push(...body.checks);
+    })();
+
+    const otherHealthchecks = [
+        {
+            name: 'Speedtest Tracker - App',
+            cron: '17 */4 * * *',
+        },
+        {
+            name: 'Renovatebot - App',
+            cron: '00 01 * * *',
+        },
+    ];
+    for (const healthcheck of otherHealthchecks) {
+        const slug = healthcheck.name.toLocaleLowerCase().replaceAll(' ', '-').replaceAll(/\-+/g, '-');
+        await addHealthcheck({ slug: slug, schedule: healthcheck.cron, name: healthcheck.name, uuid: '', grace: 0, });
+    }
+
+    const certificatorHealthchecks = [
+        'ActualBudget',
+        'Certbot',
+        'ChangeDetection',
+        'DockerHub Cache Proxy',
+        'Dozzle',
+        'Gatus-1',
+        'Gatus-2',
+        'Glances Odroid H3',
+        'Glances Odroid H4 Ultra',
+        'Glances Raspberry Pi 3B',
+        'Glances Raspberry Pi 4B 2GB',
+        'Glances Raspberry Pi 4B 4GB',
+        'Gotify',
+        'Healthchecks',
+        'Homeassistant',
+        'Homepage',
+        'Jellyfin',
+        'Minio',
+        'MotionEye',
+        'Netalertx',
+        'Ntfy',
+        'Omada Controller',
+        'Openspeedtest',
+        'PiHole [1 Primary]',
+        'PiHole [1 Secondary]',
+        'PiHole [2 Primary]',
+        'PiHole [2 Secondary]',
+        'Prometheus',
+        'Smtp4dev',
+        'Speedtest Tracker',
+        'TVHeadend',
+        'Unbound [1 Default]',
+        'Unbound [1 Open]',
+        'Unbound [2 Default]',
+        'Unbound [2 Open]',
+        'Unifi Controller',
+        'Uptime Kuma',
+        'Vaultwarden',
+        'Vikunja',
+    ];
+    for (const _name of certificatorHealthchecks) {
+        const name = `${_name} - Certificator`;
+        const slug = name.toLocaleLowerCase().replaceAll(/[ \[\]]/g, '-').replaceAll(/\-+/g, '-').replace(/-+$/, '').replace(/^-+/, '');
+        await addHealthcheck({ slug: slug, schedule: '30 01 * * *', name: name, uuid: '', grace: 0, });
+    }
+
+    for (const healthcheck of existingHealthecks) {
+        if (!createdHealthchecks.find((el) => el.slug === healthcheck.slug)) {
+            console.log(`Deleting healthcheck ${healthcheck.slug}`);
+            await (async () => {
+                const response = await axios.delete(`/checks/${healthcheck.uuid}`);
+                assert(response.status === 200, `Failed to delete healthcheck\nStatus: ${response.status}\nBody: ${JSON.stringify(response.data)}`);
+            })();
+        }
+    }
+})();
