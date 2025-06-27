5a31321c61684282a5d52c8bbff1bfda39dc14fe
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-06-27 20:57:25 +0200

    chore: debug pihole

--- docker-images/external/pihole/setup.sh
+++ docker-images/external/pihole/setup.sh
@@ -98,8 +98,8 @@ sql "UPDATE client_by_group SET group_id=$open_group_id WHERE client_id=$unbound
 sql "UPDATE client_by_group SET group_id=$open_group_id WHERE client_id=$unbound_open_2_id;"
 
 # Configuration workaround for homepage
-sessions=32
-threads=6
+sessions=64
+threads=32
 # if [ "$(uname -m)" = 'x86_64' ]; then
 #     sessions=64
 #     threads=16
