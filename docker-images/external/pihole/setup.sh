3a3019fd5ef006d4a3bf69c821f8b1b645d436bd
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-06-28 13:35:01 +0200

    chore: debug pihole homepage

--- docker-images/external/pihole/setup.sh
+++ docker-images/external/pihole/setup.sh
@@ -106,7 +106,7 @@ sql "UPDATE client_by_group SET group_id=$open_group_id WHERE client_id=$unbound
 # fi
 pihole-FTL --config webserver.threads 64
 pihole-FTL --config webserver.api.max_sessions 32
-pihole-FTL --config webserver.domain "$(printf '%s' "$HOMELAB_APP_EXTERNAL_DOMAIN" | sed -E 's~\..+$~~')"
+pihole-FTL --config webserver.domain "$HOMELAB_APP_NAME"
 pihole-FTL --config webserver.api.allow_destructive false
 pihole-FTL --config dns.rateLimit.count 10000
 pihole-FTL --config dns.rateLimit.interval 60
