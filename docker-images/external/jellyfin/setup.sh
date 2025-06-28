54aad6ac5d6515b4102b79ebd5a6b4c94f8df468
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-06-28 22:12:39 +0200

    feat: add optional configurable setup delay for jellyfin setup script

--- docker-images/external/jellyfin/setup.sh
+++ docker-images/external/jellyfin/setup.sh
@@ -5,7 +5,11 @@ while [ "$(find /homelab/data -maxdepth 1 -type f -name 'system.xml' | wc -l)" -
     sleep 1
 done
 
-sleep 5
+while [ ! -e '/homelab/data/system.xml' ]; do
+    sleep 1
+done
+
+sleep "${HOMELAB_SETUP_DELAY-5}"
 
 sed 's~<EnableMetrics>false</EnableMetrics>~<EnableMetrics>true</EnableMetrics>~' <'/homelab/data/system.xml' | sponge '/homelab/data/system.xml'
 
