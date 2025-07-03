df0cadc34c427787538945d073261f1c2b1de076
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-03 22:11:25 +0200

    feat: remove old main.sh files

--- servers/macbook-pro-2012/docker-apps/samba-data/main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -euf
-
-cd "$(dirname "$0")"
-
-# shellcheck disable=SC2068
-bash "$(git rev-parse --show-toplevel)/.utils/deployment-helpers/docker-app-main.sh" $@
--- servers/macbook-pro-2012/main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -euf
-
-cd "$(dirname "$0")"
-
-# shellcheck disable=SC2068
-python3 "$(git rev-parse --show-toplevel)/.utils/deployment-helpers/server-main.py" $@
