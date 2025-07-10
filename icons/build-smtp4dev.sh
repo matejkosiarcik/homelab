fe97a3330ace5d4f87eaafb577ed061a28ef0567
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-10 08:23:10 +0200

    feat: add smtp4dev favicon.png

--- icons/build-smtp4dev.sh
+++ icons/build-smtp4dev.sh
@@ -10,6 +10,11 @@ tmpdir=''
 # shellcheck source=/dev/null
 . "$(dirname "$0")/.build-utils.sh"
 
+# shellcheck disable=SC2034
+default_image_size='64x64'
+# shellcheck disable=SC2034
+default_convert_options='magick -background none -bordercolor transparent INPUT_FILE -resize RESOLUTION -density 1200 OUTPUT_FILE'
+
 ### Favicon ###
 
 convert_image_draft 'magick -background none INPUT_FILE -resize 16x16 -density 1200 OUTPUT_FILE' "$input_dir/other/smtp4dev-favicon.png" "$tmpdir/favicon-16.png"
@@ -17,6 +22,8 @@ optimize_image "$tmpdir/favicon-16.png"
 convert_image_draft 'magick -background none INPUT_FILE -resize 32x32 -density 1200 OUTPUT_FILE' "$input_dir/other/smtp4dev-favicon.png" "$tmpdir/favicon-32.png"
 optimize_image "$tmpdir/favicon-32.png"
 convert_ico "$tmpdir/favicon-16.png $tmpdir/favicon-32.png" "$output_dir/favicon.ico"
+
+convert_image_full "$input_dir/other/smtp4dev-favicon.png" "$output_dir/favicon.png"
 
 ### Cleanup ###
 
