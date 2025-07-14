1306790548829ca40d66b71d7f4f810eaa3fc720
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-14 23:30:27 +0200

    chore: debug owntracks

---
+++ docker-images/custom/owntracks-decryptor/src/main.ts
@@ -0,0 +1,29 @@
+import fs from 'node:fs';
+import express, { Request, Response } from 'express';
+import dotevn from 'dotenv';
+
+if (fs.existsSync('.env')) {
+    dotevn.config({ path: '.env', quiet: true });
+}
+
+const app = express();
+
+// Healthcheck
+app.get('/.health', (_: Request, response: Response) => {
+    response.sendStatus(200);
+});
+
+app.get('/pub', async (request: Request, response: Response) => {
+    try {
+        console.log('Received:', request);
+        response.status(200);
+        response.send({});
+    } catch (error) {
+        console.error('Favicon error:', error);
+        response.sendStatus(500);
+    }
+});
+
+app.listen(8080, () => {
+    console.log('Server started.');
+});
