3adc838ea91fb8b6d2a11be48b7696d161bf8942
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-27 12:10:53 +0200

    refactor: sort build script

--- icons/build.sh
+++ icons/build.sh
@@ -2,7 +2,7 @@
 set -euf
 
 sh "$(dirname "$0")/build-diagrams.sh"
+sh "$(dirname "$0")/build-favicons.sh"
 sh "$(dirname "$0")/build-healthchecks.sh"
 sh "$(dirname "$0")/build-homepage.sh"
-sh "$(dirname "$0")/build-favicons.sh"
 sh "$(dirname "$0")/build-smtp4dev.sh"
