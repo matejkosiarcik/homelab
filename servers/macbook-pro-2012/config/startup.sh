1e71835b562230a190967e8f6a525f0e7e9c9aad
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-27 10:00:24 +0200

    fix: startup cron location

--- servers/macbook-pro-2012/config/startup.sh
+++ servers/macbook-pro-2012/config/startup.sh
@@ -4,7 +4,7 @@ set -euf
 git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel 2>/dev/null || printf '%s\n' "$HOME/git/homelab")"
 
 # sh "$git_dir/utils/startup-helpers/disable-swap.sh"
-sh "$git_dir/.utils/startup-helpers/rfkill.sh"
+sh "$git_dir/utils/startup-helpers/rfkill.sh"
 
 seq 1 255 | while read -r i; do
     sh "$git_dir/.utils/startup-helpers/create-eth-interface-bridge.sh" "$i" "10.1.18.$i"
