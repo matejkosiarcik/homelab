d2f6690eec02b800a185ce1dbfa6d3850689342d
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-27 10:00:02 +0200

    fix: startup cron location

--- servers/odroid-h4-ultra/config/startup.sh
+++ servers/odroid-h4-ultra/config/startup.sh
@@ -3,7 +3,7 @@ set -euf
 
 git_dir="$(cd "$(dirname "$(realpath "$0")")" >/dev/null 2>&1 && git rev-parse --show-toplevel 2>/dev/null || printf '%s\n' "$HOME/git/homelab")"
 
-# sh "$git_dir/.utils/startup-helpers/disable-swap.sh"
+# sh "$git_dir/utils/startup-helpers/disable-swap.sh"
 sh "$git_dir/.utils/startup-helpers/rfkill.sh"
 # sh "$git_dir/.utils/startup-helpers/macvlan-router.sh" 'macvlan-shim' '10.1.21.0' '10.1.20.0'
 
