bc0c59cb869fb4968f3c442a2002e4073a2ed533
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-28 18:28:48 +0200

    refactor: rename directory for status

--- docker-images/external/minio-client/entrypoint.sh
+++ docker-images/external/minio-client/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -euf
 
-mkdir -p /homelab/.internal
-printf 'starting\n' >/homelab/.internal/status.txt
+mkdir -p /homelab/.status
+printf 'starting\n' >/homelab/.status/status.txt
 
 sh /homelab/main.sh
 
-printf 'started\n' >/homelab/.internal/status.txt
+printf 'started\n' >/homelab/.status/status.txt
 while true; do
     sleep infinity
     printf '"sleep infinity" somehow exited?' >&2
