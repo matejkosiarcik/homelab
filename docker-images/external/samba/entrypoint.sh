395e114d7a9c397bdd41d179fba43fdeb1c70e99
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-29 18:17:55 +0200

    feat: fix read_only mode in some containers

--- docker-images/external/samba/entrypoint.sh
+++ docker-images/external/samba/entrypoint.sh
@@ -4,14 +4,15 @@ set -euf
 SAMBA_GROUP="$SAMBA_USERNAME-group"
 samba_file="/homelab/config/$SAMBA_CONFIG.conf"
 
-sed "s~#smb-title#~$SAMBA_TITLE~;s~#smb-user#~$SAMBA_USERNAME~;s~#smb-group#~$SAMBA_GROUP~" <"$samba_file" >/etc/samba/smb.conf
+sed "s~#smb-title#~$SAMBA_TITLE~;s~#smb-user#~$SAMBA_USERNAME~;s~#smb-group#~$SAMBA_GROUP~" <"$samba_file" >/homelab/config/out/smb.conf
 
 if ! grep -Eq "^$SAMBA_GROUP:" /etc/group; then
-    groupadd "$SAMBA_GROUP"
+    printf 'Group %s not available\n' "$SAMBA_GROUP" >&2
+    exit 1
 fi
 if ! grep -Eq "^$SAMBA_USERNAME:" /etc/passwd; then
-    useradd -M -s /sbin/nologin "$SAMBA_USERNAME"
-    usermod -a -G "$SAMBA_GROUP" "$SAMBA_USERNAME"
+    printf 'User %s not available\n' "$SAMBA_GROUP" >&2
+    exit 1
 fi
 printf '%s\n%s\n' "$SAMBA_PASSWORD" "$SAMBA_PASSWORD" | smbpasswd -s -a "$SAMBA_USERNAME"
 
@@ -24,4 +25,4 @@ sleep 1
 
 nohup /homelab/bin/samba_statusd &
 nohup /homelab/bin/samba_exporter &
-smbd --foreground --no-process-group
+smbd --foreground --no-process-group --configfile='/homelab/config/out/smb.conf'
