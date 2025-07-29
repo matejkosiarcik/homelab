8fc038f0e7eb482438dd4d7e3df7da0b3595f835
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-07-29 18:17:39 +0200

    feat: expose btoh owntracks frontend and backend

--- docker-images/custom/apache/entrypoint.sh
+++ docker-images/custom/apache/entrypoint.sh
@@ -84,7 +84,14 @@ elif [ "$HOMELAB_APP_TYPE" = 'openwebui' ]; then
 elif [ "$HOMELAB_APP_TYPE" = 'openspeedtest' ]; then
     PROXY_UPSTREAM_URL="http://app:3000" # HTTPS endpoint is also available, but plain HTTP results in better performance
 elif [ "$HOMELAB_APP_TYPE" = 'owntracks' ]; then
-    PROXY_UPSTREAM_URL="http://app-frontend"
+    if [ "$HOMELAB_CONTAINER_VARIANT" = 'frontend' ]; then
+        PROXY_UPSTREAM_URL="http://app-frontend"
+    elif [ "$HOMELAB_CONTAINER_VARIANT" = 'backend' ]; then
+        PROXY_UPSTREAM_URL="http://app-backend:8083"
+    else
+        printf 'Unknown HOMELAB_CONTAINER_VARIANT: %s for HOMELAB_APP_TYPE: %s\n' "${HOMELAB_CONTAINER_VARIANT-N/A}" "$HOMELAB_APP_TYPE"
+        exit 1
+    fi
 elif [ "$HOMELAB_APP_TYPE" = 'pihole' ]; then
     PROXY_UPSTREAM_URL="http://app"
 elif [ "$HOMELAB_APP_TYPE" = 'prometheus' ]; then
