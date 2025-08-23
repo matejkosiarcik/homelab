ebe31c743ccac3cfbb5fcb131ae8bb144c50fe46
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-08-23 13:36:57 +0200

    chore: fix indentation

--- docker-images/external/unifi-network-application/init-mongo.sh
+++ docker-images/external/unifi-network-application/init-mongo.sh
@@ -17,12 +17,12 @@ fi
 use ${MONGO_AUTHSOURCE}
 db.auth("${MONGO_INITDB_ROOT_USERNAME}", "${MONGO_INITDB_ROOT_PASSWORD}")
 db.createUser({
-  user: "${MONGO_USER}",
-  pwd: "${MONGO_PASSWORD}",
-  roles: [
-    { db: "${MONGO_DBNAME}", role: "dbOwner" },
-    { db: "${MONGO_DBNAME}_stat", role: "dbOwner" },
-    { db: "${MONGO_DBNAME}_audit", role: "dbOwner" }
-  ]
+    user: "${MONGO_USER}",
+    pwd: "${MONGO_PASSWORD}",
+    roles: [
+        { db: "${MONGO_DBNAME}", role: "dbOwner" },
+        { db: "${MONGO_DBNAME}_stat", role: "dbOwner" },
+        { db: "${MONGO_DBNAME}_audit", role: "dbOwner" }
+    ]
 })
 EOF
