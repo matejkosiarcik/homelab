cd1c1111920ac580b24df149c602d60624cc3e2a
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-08-23 13:10:56 +0200

    fix: mongodb init script

--- docker-images/external/unifi-network-application/init-mongo.sh
+++ docker-images/external/unifi-network-application/init-mongo.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -euf
 
-if which mongosh > /dev/null 2>&1; then
-  mongo_init_bin='mongosh'
+# This condition ensures this script works with both versions of mongodb container images
+# First is the official one with `mongosh` and second the unofficial/older one with plain `mongo`
+mongo_shell=''
+if command -v mongosh >/dev/null; then
+    mongo_shell='mongosh'
+elif command -v mongo >/dev/null; then
+    mongo_shell='mongo'
 else
-  mongo_init_bin='mongo'
+    printf 'No suitable mongo shell found\n' | tee '/homelab/logs/healthcheck.log' >&2
+    exit 1
 fi
 
-"${mongo_init_bin}" <<EOF
+"${mongo_shell}" <<EOF
 use ${MONGO_AUTHSOURCE}
 db.auth("${MONGO_INITDB_ROOT_USERNAME}", "${MONGO_INITDB_ROOT_PASSWORD}")
 db.createUser({
