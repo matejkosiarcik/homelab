a79606bf672c41ae90d33f741660fd63b6513e3b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-08-29 07:04:37 +0200

    feat: move from bitnami/apache_exporter to custom debian + prometheus-apache-exporter

--- docker-images/external/apache-prometheus-exporter/entrypoint.sh
+++ docker-images/external/apache-prometheus-exporter/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -euf
 
-apache_exporter --insecure --scrape_uri="https://proxy-status:$PROXY_STATUS_PASSWORD@$APACHE_HOST/.apache/status?auto"
+prometheus-apache-exporter --insecure --scrape_uri="https://proxy-status:$PROXY_STATUS_PASSWORD@$APACHE_HOST/.apache/status?auto"
