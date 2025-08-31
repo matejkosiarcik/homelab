b8850cb966ba206a3ed43825bee3c19b36845851
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-08-31 21:27:12 +0200

    test: fix gatus app tests

--- tests/projects/tests/gatus.test.ts
+++ tests/projects/tests/gatus.test.ts
@@ -56,20 +56,20 @@ test.describe(apps.gatus.title, () => {
                         await page.goto(instance.url);
                         await expect(page.locator('.animate-spin')).toBeVisible();
                         await expect(page.locator('.animate-spin')).not.toBeVisible({ timeout: 20_000 });
-                        await expect(page.locator('#results .endpoint-group').first()).toBeVisible();
+                        await expect(page.locator('#app .endpoint-group').first()).toBeVisible();
                     });
                 }
 
                 test(`UI: Unsuccessful open - ${variant.random ? 'Random user' : `User ${variant.username}`}`, async ({ page }) => {
                     await page.setExtraHTTPHeaders({ Authorization: `Basic ${Buffer.from(`${variant.username}:${faker.string.alphanumeric(10)}`).toString('base64')}` });
                     await page.goto(instance.url);
-                    await expect(page.locator('#results .endpoint-group').first()).not.toBeVisible();
+                    await expect(page.locator('#app .endpoint-group').first()).not.toBeVisible();
                 });
             }
 
             test('UI: Unsuccessful open - No user', async ({ page }) => {
                 await page.goto(instance.url);
-                await expect(page.locator('#results .endpoint-group').first()).not.toBeVisible();
+                await expect(page.locator('#app .endpoint-group').first()).not.toBeVisible();
             });
         });
     }
