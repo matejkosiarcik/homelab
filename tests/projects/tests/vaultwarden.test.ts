06e28d8307fc95bb33922384c140286090d102b2
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-09-08 08:05:55 +0200

    test: update vaultwarden tests

--- tests/projects/tests/vaultwarden.test.ts
+++ tests/projects/tests/vaultwarden.test.ts
@@ -13,32 +13,48 @@ test.describe(apps['vaultwarden'].title, () => {
             createTcpTests(instance.url, [80, 443]);
             createFaviconTests(instance.url);
 
-            test('UI: Successful login - User test', async ({ page }) => {
-                await page.goto(instance.url);
-                await page.waitForURL(`${instance.url}/#/login`);
-                await page.locator('form input[type="email"]').waitFor({ state: 'visible', timeout: 6000 });
-                await page.locator('form input[type="email"]').fill('test@matejhome.com');
-                await page.locator('form button:has-text("Continue")').click();
-                await page.locator('form input[type="password"]').waitFor({ state: 'visible', timeout: 2000 });
-                await page.locator('form input[type="password"]').fill(getEnv(instance.url, 'TEST_PASSWORD'));
-                await page.locator('form button:has-text("Log in with master password")').click();
-                await page.waitForURL(`${instance.url}/#/vault`);
-                await page.locator('app-vault').waitFor();
-            });
+            const validUsers = [
+                {
+                    email: 'test@matejhome.com',
+                },
+            ];
+            for (const user of validUsers) {
+                test(`UI: Successful login - User ${user.email.split('@')[0]}`, async ({ page }) => {
+                    await page.goto(instance.url);
+                    await page.waitForURL(`${instance.url}/#/login`);
+                    await page.locator('form input[type="email"]').waitFor({ state: 'visible', timeout: 6000 });
+                    await page.locator('form input[type="email"]').fill(user.email);
+                    await page.locator('form button:has-text("Continue")').click();
+                    await page.locator('form input[type="password"]').waitFor({ state: 'visible', timeout: 2000 });
+                    await page.locator('form input[type="password"]').fill(getEnv(instance.url, `${user.email.split('@')[0]}_PASSWORD`));
+                    await page.locator('form button:has-text("Log in with master password")').click();
+                    await page.waitForURL(`${instance.url}/#/vault`);
+                    await page.locator('app-vault').waitFor();
+                });
+            }
 
-            test('UI: Unsuccessful login - Random user', async ({ page }) => {
-                await page.goto(instance.url);
-                await page.waitForURL(`${instance.url}/#/login`);
-                await page.locator('form input[type="email"]').waitFor({ state: 'visible', timeout: 6000 });
-                const originalUrl = page.url();
-                await page.locator('form input[type="email"]').fill(`${faker.string.alpha(8)}@matejhome.com`);
-                await page.locator('form button:has-text("Continue")').click();
-                await page.locator('form input[type="password"]').waitFor({ state: 'visible', timeout: 2000 });
-                await page.locator('form input[type="password"]').fill(faker.string.alpha(10));
-                await page.locator('form button:has-text("Log in with master password")').click();
-                await expect(page.locator('bit-error:has-text("Invalid master password")')).toBeVisible();
-                await expect(page, 'URL should not change').toHaveURL(originalUrl);
-            });
+            const invalidUsers = [
+                {
+                    title: 'Random user',
+                    email: `${faker.string.alpha(10)}@matejhome.com`,
+                },
+            ];
+            for (const user of invalidUsers) {
+                test(`UI: Unsuccessful login - ${user.title}`, async ({ page }) => {
+                    await page.goto(instance.url);
+                    await page.waitForURL(`${instance.url}/#/login`);
+                    await page.locator('form input[type="email"]').waitFor({ state: 'visible', timeout: 6000 });
+                    const originalUrl = page.url();
+                    await page.locator('form input[type="email"]').fill(user.email);
+                    await page.locator('form button:has-text("Continue")').click();
+                    await page.locator('form input[type="password"]').waitFor({ state: 'visible', timeout: 2000 });
+                    await page.locator('form input[type="password"]').fill(faker.string.alpha(10));
+                    await page.locator('form button:has-text("Log in with master password")').click();
+                    await expect(page.locator('bit-error:has-text("Invalid master password")')).toBeVisible();
+                    await expect(page, 'URL should not change').toHaveURL(originalUrl);
+                });
+            }
+
         });
     }
 });
