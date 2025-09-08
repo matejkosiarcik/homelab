99eea90f40af49e22bf44ee21805aec3ed6be0cf
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-09-08 08:21:47 +0200

    test: update uptime-kuma tests

--- tests/projects/tests/uptimekuma.test.ts
+++ tests/projects/tests/uptimekuma.test.ts
@@ -14,36 +14,38 @@ test.describe(apps.uptimekuma.title, () => {
             createFaviconTests(instance.url);
             createPrometheusTests(instance.url, { auth: 'basic', username: 'matej' });
 
-            const users = [
+            const validUsers = [
                 {
-                    username: 'matej',
+                    username: 'matej'
                 },
-                {
-                    username: faker.string.alpha(10),
-                    random: true,
-                }
             ];
-            for (const variant of users) {
-                if (!variant.random) {
-                    test(`UI: Successful login - User ${variant.username}`, async ({ page }) => {
-                        // Load page
-                        await page.goto(instance.url);
-                        await page.waitForURL(`${instance.url}/dashboard`);
-                        await expect(page.locator('.form-container form')).toBeVisible();
-                        // await expect(PageRevealEvent.getByRole('heading', { name: 'All Messages'})).not.toBeVisible();
+            for (const user of validUsers) {
+                test(`UI: Successful login - User ${user.username}`, async ({ page }) => {
+                    // Load page
+                    await page.goto(instance.url);
+                    await page.waitForURL(`${instance.url}/dashboard`);
+                    await expect(page.locator('.form-container form')).toBeVisible();
+                    // await expect(PageRevealEvent.getByRole('heading', { name: 'All Messages'})).not.toBeVisible();
 
-                        // Fill in form
-                        await page.locator('form input[autocomplete="username"]').fill(variant.username);
-                        await page.locator('form input[type="password"]').fill(getEnv(instance.url, 'ADMIN_PASSWORD'));
-                        await page.locator('form button[type="submit"]:has-text("Login")').click();
+                    // Fill in form
+                    await page.locator('form input[autocomplete="username"]').fill(user.username);
+                    await page.locator('form input[type="password"]').fill(getEnv(instance.url, `${user.username}_PASSWORD`));
+                    await page.locator('form button[type="submit"]:has-text("Log In")').click();
 
-                        // Verify login
-                        await expect(page.locator('.form-container form')).not.toBeVisible();
-                        await expect(page.getByRole('heading', { name: 'Quick Stats'})).toBeVisible();
-                    });
-                }
+                    // Verify login
+                    await expect(page.locator('.form-container form')).not.toBeVisible();
+                    await expect(page.getByRole('heading', { name: 'Quick Stats'})).toBeVisible();
+                });
+            }
 
-                test(`UI: Unsuccessful login - ${variant.random ? 'Random user' : `User ${variant.username}`}`, async ({ page }) => {
+            const invalidUsers = [
+                {
+                    title: 'Random user',
+                    username: faker.string.alpha(10),
+                },
+            ];
+            for (const user of invalidUsers) {
+                test(`UI: Unsuccessful login - ${user.title}`, async ({ page }) => {
                     // Load page
                     await page.goto(instance.url);
                     await page.waitForURL(`${instance.url}/dashboard`);
@@ -51,9 +53,9 @@ test.describe(apps.uptimekuma.title, () => {
                     await expect(page.getByRole('alert', { name: 'Incorrect username or password.' })).not.toBeVisible();
 
                     // Fill in form
-                    await page.locator('form input[autocomplete="username"]').fill(variant.username);
+                    await page.locator('form input[autocomplete="username"]').fill(user.username);
                     await page.locator('form input[type="password"]').fill(faker.string.alpha(10));
-                    await page.locator('form button[type="submit"]:has-text("Login")').click();
+                    await page.locator('form button[type="submit"]:has-text("Log In")').click();
 
                     // Verify fail
                     await expect(page.locator('[role="alert"]:has-text("Incorrect username or password.")')).toBeVisible();
