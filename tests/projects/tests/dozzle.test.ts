d3853e9bc6e3342fb900378ac83d8b659827dfa3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-09-08 20:47:47 +0200

    test: update dozzle tests

--- tests/projects/tests/dozzle.test.ts
+++ tests/projects/tests/dozzle.test.ts
@@ -13,25 +13,40 @@ test.describe(apps.dozzle.title, () => {
             createTcpTests(instance.url, [80, 443]);
             createFaviconTests(instance.url);
 
-            test('UI: Successful login - User admin', async ({ page }) => {
+            const validUsers = [
+                {
+                    username: 'admin',
+                },
+            ];
+            for (const user of validUsers) {
+                test(`UI: Successful login - User ${user.username}`, async ({ page }) => {
                 await page.goto(instance.url);
                 await page.waitForURL(`${instance.url}/login?redirectUrl=/`);
-                await page.locator('form input[name="username"]').fill('admin');
-                await page.locator('form input[name="password"]').fill(getEnv(instance.url, 'ADMIN_PASSWORD'));
+                await page.locator('form input[name="username"]').fill(user.username);
+                await page.locator('form input[name="password"]').fill(getEnv(instance.url, `${user.username}_PASSWORD`));
                 await page.locator('form button[type="submit"]:has-text("Login")').click();
                 await page.waitForURL(instance.url);
                 await expect(page.locator('a[href^="/container/"]').first()).toBeVisible();
-            });
+                });
+            }
 
-            test('UI: Unsuccessful login - Random user', async ({ page }) => {
-                await page.goto(instance.url);
-                await page.waitForURL(`${instance.url}/login?redirectUrl=/`);
-                await page.locator('form input[name="username"]').fill(faker.string.alpha(10));
-                await page.locator('form input[name="password"]').fill(faker.string.alpha(10));
-                await page.locator('form button[type="submit"]:has-text("Login")').click();
-                await page.waitForSelector('.text-error:has-text("Username or password are not valid")', { timeout: 10_000 });
-                await expect(page).toHaveURL(`${instance.url}/login?redirectUrl=/`);
-            });
+            const invalidUsers = [
+                {
+                    title: 'Random user',
+                    username: faker.string.alpha(10),
+                },
+            ];
+            for (const user of invalidUsers) {
+                test(`UI: Unsuccessful login - ${user.title}`, async ({ page }) => {
+                    await page.goto(instance.url);
+                    await page.waitForURL(`${instance.url}/login?redirectUrl=/`);
+                    await page.locator('form input[name="username"]').fill(user.username);
+                    await page.locator('form input[name="password"]').fill(faker.string.alpha(10));
+                    await page.locator('form button[type="submit"]:has-text("Login")').click();
+                    await page.waitForSelector('.text-error:has-text("Username or password are not valid")', { timeout: 10_000 });
+                    await expect(page).toHaveURL(`${instance.url}/login?redirectUrl=/`);
+                });
+            }
         });
     }
 });
