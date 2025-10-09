c9dcde2216549c3cd731aace8d7cdb5a0635b91b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-10-09 12:13:20 +0200

    test: fix groceries user

--- tests/projects/tests/groceries.test.ts
+++ tests/projects/tests/groceries.test.ts
@@ -16,7 +16,7 @@ test.describe(apps.groceries.title, () => {
             test('UI: Successful login - User homelab-test', async ({ page }) => {
                 await page.goto(instance.url);
                 await page.waitForURL(`${instance.url}/login`);
-                await page.locator('input[autocomplete="username"]').fill('homelab-test'); // Username is weird, because there is a minimum length limit
+                await page.locator('input[autocomplete="username"]').fill('homelabtest'); // Username is missing dash, because of app restrictions
                 await page.locator('input[autocomplete="current-password"]').fill(getEnv(instance.url, 'HOMELAB_TEST_PASSWORD'));
                 await page.locator('ion-button[type="submit"]:has-text("Login")').click();
                 await page.waitForURL(`${instance.url}/lists`);
