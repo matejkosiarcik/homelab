eecac18ee0d851e52cd52debb48605aff08ce933
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-10-15 19:27:08 +0200

    test: update renovatebot tests

--- tests/projects/tests/renovatebot.test.ts
+++ tests/projects/tests/renovatebot.test.ts
@@ -1,15 +1,36 @@
 import { test } from '@playwright/test';
 import { apps } from '../../utils/apps';
 import { createApiRootTest, createFaviconTests, createHttpToHttpsRedirectTests, createProxyTests, createTcpTests } from '../../utils/tests';
+import { getEnv } from '../../utils/utils';
 
 test.describe(apps.renovatebot.title, () => {
     for (const instance of apps.renovatebot.instances) {
         test.describe(instance.title, () => {
             createHttpToHttpsRedirectTests(instance.url);
             createProxyTests(instance.url);
-            createApiRootTest(instance.url);
+            createApiRootTest(instance.url, { title: 'Unauthenticated', status: 401 });
             createTcpTests(instance.url, [80, 443]);
             createFaviconTests(instance.url);
+
+            const validUsers = [
+                {
+                    username: 'matej',
+                },
+                {
+                    username: 'homelab-viewer',
+                },
+                {
+                    username: 'homelab-test',
+                },
+            ];
+            for (const user of validUsers) {
+                createApiRootTest(instance.url, {
+                    title: `Authenticated - User ${user.username}`,
+                    headers: {
+                        Authorization: `Basic ${Buffer.from(`${user.username}:${getEnv(instance.url, `${user.username}_PASSWORD`)}`).toString('base64')}`
+                    },
+                });
+            }
         });
     }
 });
