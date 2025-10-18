a86fffc9d99588e0f0c2a96e27a090f55846429b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-10-18 20:03:00 +0200

    chore: disable disabled HTTPS in openspeedtest

--- tests/projects/tests/openspeedtest.test.ts
+++ tests/projects/tests/openspeedtest.test.ts
@@ -1,27 +1,15 @@
 import { expect, test } from '@playwright/test';
 import { apps } from '../../utils/apps';
-import { createFaviconTests, createHttpsToHttpRedirectTests, createProxyTests, createTcpTests } from '../../utils/tests';
-import axios from 'axios';
+import { createApiRootTest, createFaviconTests, createHttpToHttpsRedirectTests, createProxyTests, createTcpTests } from '../../utils/tests';
 
 test.describe(apps.openspeedtest.title, () => {
     for (const instance of apps.openspeedtest.instances) {
         test.describe(instance.title, () => {
-            createHttpsToHttpRedirectTests(instance.url);
+            createHttpToHttpsRedirectTests(instance.url);
             createProxyTests(instance.url, { redirect: false });
             createTcpTests(instance.url, [80, 443]);
             createFaviconTests(instance.url);
-
-            test('API: Get root - HTTP', async () => {
-                const response = await axios.get(instance.url.replace('https://', 'http://'), { maxRedirects: 0 });
-                expect(response.status, 'Response Status').toStrictEqual(200);
-                expect(response.config.url, 'Response URL').toStrictEqual(instance.url.replace('https://', 'http://'));
-            });
-
-            test('API: Get root - HTTPS', async () => {
-                const response = await axios.get(instance.url.replace('http://', 'https://'), { maxRedirects: 0, validateStatus: () => true });
-                expect(response.status, 'Response Status').toStrictEqual(302);
-                expect(response.headers['location'], 'Response header location').toStrictEqual(instance.url.replace('https://', 'http://'));
-            });
+            createApiRootTest(instance.url);
 
             test('UI: Open', async ({ page }) => {
                 await page.goto(instance.url);
