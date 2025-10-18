a3337b86dcca3454537b14120eb71675a3e28835
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-10-18 17:22:41 +0200

    test: add openwebui tests

--- tests/projects/tests/openwebui.test.ts
+++ tests/projects/tests/openwebui.test.ts
@@ -1,19 +1,62 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { apps } from '../../utils/apps';
 import { createApiRootTest, createFaviconTests, createHttpToHttpsRedirectTests, createProxyTests, createTcpTests } from '../../utils/tests';
+import { getEnv } from '../../utils/utils';
+import { faker } from '@faker-js/faker';
 
 test.describe(apps['openwebui'].title, () => {
     for (const instance of apps['openwebui'].instances) {
         test.describe(instance.title, () => {
             createHttpToHttpsRedirectTests(instance.url);
-            // TODO: createPrometheusTests(instance.url, { auth: 'basic' });
             createProxyTests(instance.url);
             createApiRootTest(instance.url);
             createTcpTests(instance.url, [80, 443]);
             createFaviconTests(instance.url);
 
-            // TODO: Add tests for API endpoints
-            // TODO: Add tests for UI
+            const validUsers = [
+                {
+                    email: 'matej@matejhome.com',
+                },
+                {
+                    email: 'homelab-viewer@homelab.matejhome.com',
+                },
+                {
+                    email: 'homelab-test@homelab.matejhome.com',
+                },
+            ];
+            for (const user of validUsers) {
+                test(`UI: Successful login - User ${user.email.split('@')[0]}`, async ({ page }) => {
+                    await page.goto(instance.url);
+                    await page.waitForURL(`${instance.url}/auth?redirect=%2F`);
+                    await page.locator('form input#email').fill(user.email);
+                    await page.locator('form input#password').fill(getEnv(instance.url, `${user.email.split('@')[0]}_PASSWORD`));
+                    await page.locator('form button[type="submit"]:has-text("Sign in")').click();
+                    await page.waitForURL(`${instance.url}/`);
+                    await expect(page.locator('section[aria-label*="Notifications"] [data-content]:has-text("You\'re now logged in.")')).toBeVisible();
+                    await expect(page.locator('#chat-input')).toBeVisible();
+                });
+            }
+
+            const invalidUsers = [
+                {
+                    email: 'homelab-test@homelab.matejhome.com',
+                },
+                {
+                    email: `${faker.string.alpha(10)}@homelab.matejhome.com`,
+                    random: true,
+                },
+            ];
+            for (const user of invalidUsers) {
+                test(`UI: Unsuccessful login - ${user.random ? 'Random user' : `User ${user.email.split('@')[0]}`}`, async ({ page }) => {
+                    await page.goto(instance.url);
+                    await page.waitForURL(`${instance.url}/auth?redirect=%2F`);
+                    await page.locator('form input#email').fill(user.email);
+                    await page.locator('form input#password').fill(faker.string.alpha(10));
+                    await page.locator('form button[type="submit"]:has-text("Sign in")').click();
+                    await expect(page.locator('section[aria-label*="Notifications"] [data-content]:has-text("The email or password provided is incorrect. Please check for typos and try logging in again.")')).toBeVisible();
+                });
+            }
+
         });
     }
 });
