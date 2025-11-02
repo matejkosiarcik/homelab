14dc5e0771986c54f50c3ac55d37a8b6f256c012
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-11-02 09:11:12 +0100

    feat: decomission Raspberry Pi 3B

--- servers/raspberry-pi-3b/config/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -euf
-
-cd "$(dirname "$(dirname "$0")")" >/dev/null 2>&1
-git_dir="$(cd "$(dirname "$0")" >/dev/null 2>&1 && git rev-parse --show-toplevel 2>/dev/null || printf '%s\n' "$HOME/git/homelab")"
-
-sh "$git_dir/utils/others/update.sh"
