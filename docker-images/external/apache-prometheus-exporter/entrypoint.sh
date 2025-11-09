bec569bb7e7bf2a664d889fb7cfd742ea17b9e79
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-11-09 12:02:11 +0100

    feat: dynamically get apache hostname in apache-prometheus-exporter

--- docker-images/external/apache-prometheus-exporter/entrypoint.sh
+++ docker-images/external/apache-prometheus-exporter/entrypoint.sh
@@ -4,7 +4,12 @@ set -euf
 # Prepare graceful shutdown
 trap 'exit 0' TERM
 
+apache_host='apache'
+if [ "${HOMELAB_CONTAINER_VARIANT-}" != '' ] && [ "$HOMELAB_CONTAINER_VARIANT" != default ]; then
+    apache_host="$apache_host-$HOMELAB_CONTAINER_VARIANT"
+fi
+
 # Start
-prometheus-apache-exporter --insecure --scrape_uri="https://proxy-status:$PROXY_STATUS_PASSWORD@$APACHE_HOST/.apache/status?auto" &
+prometheus-apache-exporter --insecure --scrape_uri="https://proxy-status:$PROXY_STATUS_PASSWORD@$apache_host/.apache/status?auto" &
 program_pid="$!"
 wait "$program_pid"
