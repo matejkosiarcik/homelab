db6473148befa6e1bf33a15fbc0d57945540abda
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-11-16 13:47:46 +0100

    test: add tests for homelab-admin user for home-assistant

--- tests/projects/tests/homeassistant.test.ts
+++ tests/projects/tests/homeassistant.test.ts
@@ -41,6 +41,9 @@ test.describe(apps.homeassistant.title, () => {
 
             const validUsers = [
                 {
+                    username: 'homelab-admin',
+                },
+                {
                     username: 'homelab-viewer',
                 },
                 {
