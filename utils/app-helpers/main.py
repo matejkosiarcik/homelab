71a45b512fab5c1a90cdcbd267876105240ab8b8
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-12-27 13:39:09 +0100

    chore: fix python lint errors

--- utils/app-helpers/main.py
+++ utils/app-helpers/main.py
@@ -112,13 +112,13 @@ def load_full_env():
 
 
 def symlink_app():
-    compose_dir = path.join(git_dir, 'docker-compose', app_type)
-    for file in ['compose.yml', 'compose.override.yml', 'compose.prod.yml']:
+    compose_dir = path.join(git_dir, "docker-compose", app_type)
+    for file in ["compose.yml", "compose.override.yml", "compose.prod.yml"]:
         if path.exists(file):
             os.remove(file)
-    os.symlink(path.join(compose_dir, 'compose.yml'), 'compose.yml')
-    os.symlink(path.join(compose_dir, 'compose.override-dev.yml'), 'compose.override.yml')
-    os.symlink(path.join(compose_dir, 'compose.override-prod.yml'), 'compose.prod.yml')
+    os.symlink(path.join(compose_dir, "compose.yml"), "compose.yml")
+    os.symlink(path.join(compose_dir, "compose.override-dev.yml"), "compose.override.yml")
+    os.symlink(path.join(compose_dir, "compose.override-prod.yml"), "compose.prod.yml")
 
 
 def get_docker_compose_config() -> str:
