9d2abeed328c8f830172a60524a560b70c21fab3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-12-31 19:10:28 +0100

    feat: add custom gitconfig in Dockerfiles

--- utils/app-helpers/main.py
+++ utils/app-helpers/main.py
@@ -43,6 +43,10 @@ log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler())
 log.addHandler(logging.FileHandler(log_file))
 
+# Write current date.txt
+with open(path.join(git_dir, "docker-images", ".shared", "date.txt"), "w", encoding="utf-8") as file:
+    print(datetime.now().isoformat(), file=file)
+
 
 def tty_supports_color():
     return sys.stdout.isatty() and os.environ.get("TERM") not in (None, "", "dumb")
