d5a337bc05e9676dd9d9864f812c64d7fa58c187
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-01-01 12:19:27 +0100

    fix: add redis secrets for dawarich

--- utils/secrets-helpers/main.sh
+++ utils/secrets-helpers/main.sh
@@ -280,16 +280,22 @@ case "$app_dirname" in
     write_http_auth_user prometheus "$prometheus_password" prometheus
     printf 'prometheus,%s\n' "$prometheus_password" >>"$initial_output/all-credentials.csv"
 
+    # Decryptor
+    secret_key="$(load_token "$DOCKER_COMPOSE_APP_NAME" app secret-key)"
+    printf 'SECRET_KEY=%s\n' "$secret_key" >>"$initial_output/decryptor.env"
+    printf 'secret-key,%s\n' "$secret_key" >>"$initial_output/all-credentials.csv"
+
     # Database
     database_password="$(load_password "$DOCKER_COMPOSE_APP_NAME" database user)"
     printf 'DATABASE_PASSWORD=%s\n' "$database_password" >>"$initial_output/app.env"
     printf 'POSTGRES_PASSWORD=%s\n' "$database_password" >>"$initial_output/postgis.env"
     printf 'database,%s\n' "$database_password" >>"$initial_output/all-credentials.csv"
 
-    # Decryptor
-    secret_key="$(load_token "$DOCKER_COMPOSE_APP_NAME" app secret-key)"
-    printf 'SECRET_KEY=%s\n' "$secret_key" >>"$initial_output/decryptor.env"
-    printf 'secret-key,%s\n' "$secret_key" >>"$initial_output/all-credentials.csv"
+    # Redis
+    redis_password="$(load_password "$DOCKER_COMPOSE_APP_NAME" redis user)"
+    printf 'REDIS_PASSWORD=%s\n' "$redis_password" >>"$initial_output/redis.env"
+    printf 'REDIS_PASSWORD=%s\n' "$redis_password" >>"$initial_output/app.env"
+    printf 'redis,%s\n' "$redis_password" >>"$initial_output/all-credentials.csv"
 
     # Apache
     write_default_proxy_users "$DOCKER_COMPOSE_APP_NAME"
