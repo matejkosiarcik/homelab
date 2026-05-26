ae7668cd31d0bebbc182fe866c7d317372c39a9f
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-05-26 19:38:45 +0200

    feat: expand go-based healthcheck

--- docker-images/.shared/healthchecks/http-healthcheck-2/go.mod
+++ docker-images/.shared/healthchecks/http-healthcheck-2/go.mod
@@ -1,2 +1,5 @@
 module example
+
 go 1.25.5
+
+require github.com/spf13/pflag v1.0.10
--- docker-images/.shared/healthchecks/http-healthcheck-2/main.go
+++ docker-images/.shared/healthchecks/http-healthcheck-2/main.go
@@ -1,44 +1,34 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
 	"time"
+
+	"github.com/spf13/pflag"
 )
 
 func main() {
-	// Parse flags
-	urlFlag := flag.String("url", "", "URL to check (required)")
-	methodFlag := flag.String("method", "GET", "HTTP method to use (default GET)")
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s --url URL [--method METHOD]\n", os.Args[0])
-		flag.VisitAll(func(f *flag.Flag) {
-			fmt.Fprintf(os.Stderr, "  --%s\t%s\n", f.Name, f.Usage)
-		})
+	url := pflag.String("url", "", "URL to check (required)")
+	method := pflag.String("method", "GET", "HTTP method")
+	pflag.Parse()
+	if *url == "" {
+		fmt.Fprintln(os.Stderr, "--url is required")
+		os.Exit(1)
 	}
-	flag.Parse()
-	if *urlFlag == "" {
-		fmt.Fprintln(os.Stderr, "Error: --url is required")
-		flag.Usage()
-		os.Exit(2)
-	}
-	url := *urlFlag
-	method := strings.ToUpper(*methodFlag)
 
 	client := &http.Client{Timeout: 2 * time.Second}
 
 	// Perform request
-	req, err := http.NewRequest(method, url, nil)
+	req, err := http.NewRequest(*method, *url, nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error creating request: %v\n", err)
 		os.Exit(1)
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error performing healthcheck: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error performing request: %v\n", err)
 		os.Exit(1)
 	}
 
@@ -47,9 +37,10 @@ func main() {
 		_ = resp.Body.Close()
 	}
 
-	if resp.StatusCode == http.StatusOK {
+	if resp.StatusCode/100 == 2 {
 		os.Exit(0)
 	}
-	fmt.Fprintf(os.Stderr, "Unexpected healthcheck status: %s\n", resp.Status)
+
+	fmt.Fprintf(os.Stderr, "Unexpected response status: %s\n", resp.Status)
 	os.Exit(1)
 }
