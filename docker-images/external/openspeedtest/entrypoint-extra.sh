f129969b0539aff3e4f7d41c25dc848191358f76
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-05-26 12:00:41 +0200

    feat: apply new users in openspeedtest dockerfiles and fix minor issues

--- docker-images/external/openspeedtest/entrypoint-extra.sh
+++ docker-images/external/openspeedtest/entrypoint-extra.sh
@@ -2,4 +2,4 @@
 
 # Copy everything from fake to real "/etc/nginx/conf.d"
 mkdir -p /etc/nginx/conf.d
-cp -R /homelab/original/etc/nginx/conf.d /etc/nginx/conf.d
+cp -R /homelab/original/etc/nginx/conf.d/. /etc/nginx/conf.d
