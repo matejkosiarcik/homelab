5c44635ac1eb3b0897f1fbf72ddec1499a6b62d0
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-05-30 12:01:41 +0200

    test: rename subtitle for tests

--- tests/utils/apps.ts
+++ tests/utils/apps.ts
@@ -2,116 +2,116 @@ export const apps = {
     actualbudget: {
         title: 'ActualBudget',
         instances: [
-            { url: 'https://actualbudget.matejhome.com', title: 'ActualBudget' },
+            { url: 'https://actualbudget.matejhome.com', title: 'Main' },
         ],
     },
     adventurelog: {
         title: 'AdventureLog',
         instances: [
-            { url: 'https://adventurelog.matejhome.com', title: 'AdventureLog', backendUrl: 'https://adventurelog-backend.matejhome.com', },
+            { url: 'https://adventurelog.matejhome.com', title: 'Main', backendUrl: 'https://adventurelog-backend.matejhome.com', },
         ],
     },
     certbot: {
         title: 'Certbot',
         instances: [
-            { url: 'https://certbot.matejhome.com', title: 'Certbot' },
+            { url: 'https://certbot.matejhome.com', title: 'Main' },
         ],
     },
     changedetection: {
         title: 'Changedetection',
         instances: [
-            { url: 'https://changedetection.matejhome.com', title: 'Changedetection' },
+            { url: 'https://changedetection.matejhome.com', title: 'Main' },
         ]
     },
     dawarich: {
         title: 'Dawarich',
         instances: [
-            { url: 'https://dawarich.matejhome.com', title: 'Dawarich' },
+            { url: 'https://dawarich.matejhome.com', title: 'Main' },
         ]
     },
     dockercache: {
         title: 'Docker Cache',
         instances: [
-            { url: 'https://docker-cache-dockerhub.matejhome.com', title: 'Docker Cache - DockerHub' },
+            { url: 'https://docker-cache-dockerhub.matejhome.com', title: 'DockerHub' },
         ],
     },
     'docker-stats': {
         title: 'Docker Stats',
         instances: [
-            { url: 'https://docker-stats-odroid-h3.matejhome.com', title: 'Docker Host Stats - Odroid H3' },
-            { url: 'https://docker-stats-odroid-h4-ultra.matejhome.com', title: 'Docker Host Stats - Odroid H4 Ultra' },
-            { url: 'https://docker-stats-raspberry-pi-4b-2g.matejhome.com', title: 'Docker Host Stats - Raspberry Pi 4B 2GB' },
-            { url: 'https://docker-stats-raspberry-pi-4b-4g.matejhome.com', title: 'Docker Host Stats - Raspberry Pi 4B 4GB' },
+            { url: 'https://docker-stats-odroid-h3.matejhome.com', title: 'Odroid H3' },
+            { url: 'https://docker-stats-odroid-h4-ultra.matejhome.com', title: 'Odroid H4 Ultra' },
+            { url: 'https://docker-stats-raspberry-pi-4b-2g.matejhome.com', title: 'Raspberry Pi 4B 2GB' },
+            { url: 'https://docker-stats-raspberry-pi-4b-4g.matejhome.com', title: 'Raspberry Pi 4B 4GB' },
         ],
     },
     dozzle: {
         title: 'Dozzle',
         instances: [
-            { url: 'https://dozzle.matejhome.com', title: 'Dozzle server' },
+            { url: 'https://dozzle.matejhome.com', title: 'Server' },
         ],
     },
     'dozzle-agent': {
         title: 'Dozzle Agent',
         instances: [
-            { url: 'tcp://dozzle-agent-odroid-h3.matejhome.com', title: 'Dozzle agent - Odroid H3' },
-            { url: 'tcp://dozzle-agent-odroid-h4-ultra.matejhome.com', title: 'Dozzle agent - Odroid H4 Ultra' },
-            { url: 'tcp://dozzle-agent-raspberry-pi-4b-2g.matejhome.com', title: 'Dozzle agent - Raspberry Pi 4B 2GB' },
-            { url: 'tcp://dozzle-agent-raspberry-pi-4b-4g.matejhome.com', title: 'Dozzle agent - Raspberry Pi 4B 4GB' },
+            { url: 'tcp://dozzle-agent-odroid-h3.matejhome.com', title: 'Odroid H3' },
+            { url: 'tcp://dozzle-agent-odroid-h4-ultra.matejhome.com', title: 'Odroid H4 Ultra' },
+            { url: 'tcp://dozzle-agent-raspberry-pi-4b-2g.matejhome.com', title: 'Raspberry Pi 4B 2GB' },
+            { url: 'tcp://dozzle-agent-raspberry-pi-4b-4g.matejhome.com', title: 'Raspberry Pi 4B 4GB' },
         ],
     },
     gatus: {
         title: 'Gatus',
         instances: [
-            { url: 'https://gatus-1.matejhome.com', title: 'Gatus 1' },
-            { url: 'https://gatus-2.matejhome.com', title: 'Gatus 2' },
+            { url: 'https://gatus-1.matejhome.com', title: '1' },
+            { url: 'https://gatus-2.matejhome.com', title: '2' },
         ],
     },
     gitcache: {
         title: 'Git Cache',
         instances: [
-            { url: 'https://git-cache-github.matejhome.com', title: 'Git Cache - GitHub' },
+            { url: 'https://git-cache-github.matejhome.com', title: 'GitHub' },
         ],
     },
     gotify: {
         title: 'Gotify',
         instances: [
-            { url: 'https://gotify.matejhome.com', title: 'Gotify' },
+            { url: 'https://gotify.matejhome.com', title: 'Main' },
         ],
     },
     grafana: {
         title: 'Grafana',
         instances: [
-            { url: 'https://grafana.matejhome.com', title: 'Grafana' },
+            { url: 'https://grafana.matejhome.com', title: 'Main' },
         ]
     },
     groceries: {
         title: 'Groceries',
         instances: [
-            { url: 'https://groceries.matejhome.com', title: 'Groceries' },
+            { url: 'https://groceries.matejhome.com', title: 'Main' },
         ]
     },
     healthchecks: {
         title: 'Healthchecks',
         instances: [
-            { url: 'https://healthchecks.matejhome.com', title: 'Healthchecks' },
+            { url: 'https://healthchecks.matejhome.com', title: 'Main' },
         ],
     },
     homeassistant: {
         title: 'HomeAssistant',
         instances: [
-            { url: 'https://homeassistant.matejhome.com', title: 'HomeAssistant' },
+            { url: 'https://homeassistant.matejhome.com', title: 'Main' },
         ],
     },
     homepage: {
         title: 'Homepage',
         instances: [
-            { url: 'https://homepage.matejhome.com', title: 'Homepage' },
+            { url: 'https://homepage.matejhome.com', title: 'Main' },
         ],
     },
     jellyfin: {
         title: 'Jellyfin',
         instances: [
-            { url: 'https://jellyfin.matejhome.com', title: 'Jellyfin' },
+            { url: 'https://jellyfin.matejhome.com', title: 'Main' },
         ],
     },
     kiwix: {
@@ -121,161 +121,167 @@ export const apps = {
             { url: 'https://wiktionary.matejhome.com', title: 'Wiktionary' },
         ],
     },
+    libretranslate: {
+        title: 'LibreTranslate',
+        instances: [
+            { url: 'https://libretranslate.matejhome.com', title: 'Main' },
+        ],
+    },
     minio: {
         title: 'Minio',
         instances: [
-            { url: 'https://minio.matejhome.com', title: 'Minio', consoleUrl: 'https://minio-console.matejhome.com' },
+            { url: 'https://minio.matejhome.com', title: 'Main', consoleUrl: 'https://minio-console.matejhome.com' },
         ],
     },
     motioneye: {
         title: 'MotionEye',
         instances: [
-            { url: 'https://motioneye-kitchen.matejhome.com', title: 'MotionEye Kitchen' },
+            { url: 'https://motioneye-kitchen.matejhome.com', title: 'Kitchen' },
         ],
     },
     netalertx: {
         title: 'NetAlertX',
         instances: [
-            { url: 'https://netalertx.matejhome.com', title: 'NetAlertX' },
+            { url: 'https://netalertx.matejhome.com', title: 'Main' },
         ],
     },
     npmcache: {
         title: 'NPM Cache',
         instances: [
-            { url: 'https://npm-cache.matejhome.com', title: 'NPM Cache' },
+            { url: 'https://npm-cache.matejhome.com', title: 'npmjs' },
         ],
     },
     ntfy: {
         title: 'Ntfy',
         instances: [
-            { url: 'https://ntfy.matejhome.com', title: 'Ntfy' },
+            { url: 'https://ntfy.matejhome.com', title: 'Main' },
         ],
     },
     nodeexporter: {
         title: 'NodeExporter',
         instances: [
-            { url: 'https://nodeexporter-odroid-h3.matejhome.com', title: 'NodeExporter - Odroid H3' },
-            { url: 'https://nodeexporter-odroid-h4-ultra.matejhome.com', title: 'NodeExporter - Odroid H4 Ultra' },
-            { url: 'https://nodeexporter-raspberry-pi-4b-2g.matejhome.com', title: 'NodeExporter - Raspberry Pi 4B 2GB' },
-            { url: 'https://nodeexporter-raspberry-pi-4b-4g.matejhome.com', title: 'NodeExporter - Raspberry Pi 4B 4GB' },
+            { url: 'https://nodeexporter-odroid-h3.matejhome.com', title: 'Odroid H3' },
+            { url: 'https://nodeexporter-odroid-h4-ultra.matejhome.com', title: 'Odroid H4 Ultra' },
+            { url: 'https://nodeexporter-raspberry-pi-4b-2g.matejhome.com', title: 'Raspberry Pi 4B 2GB' },
+            { url: 'https://nodeexporter-raspberry-pi-4b-4g.matejhome.com', title: 'Raspberry Pi 4B 4GB' },
         ],
     },
     ollama: {
         title: 'Ollama',
         instances: [
-            { url: 'https://ollama.matejhome.com', title: 'Ollama' },
-            // TODO: Reactivete: { url: 'https://ollama-private.matejhome.com', title: 'Ollama - Private' },
+            { url: 'https://ollama.matejhome.com', title: 'Main' },
+            // TODO: Reactivete: { url: 'https://ollama-private.matejhome.com', title: 'Private' },
         ],
     },
     omadacontroller: {
         title: 'OmadaController',
         instances: [
-            { url: 'https://omadacontroller.matejhome.com', title: 'OmadaController' },
+            { url: 'https://omadacontroller.matejhome.com', title: 'Main' },
         ],
     },
     openspeedtest: {
         title: 'Openspeedtest',
         forceHttps: false,
         instances: [
-            { url: 'https://openspeedtest.matejhome.com', title: 'Openspeedtest' },
+            { url: 'https://openspeedtest.matejhome.com', title: 'Main' },
         ],
     },
     openwebui: {
         title: 'OpenWebUI',
         instances: [
-            { url: 'https://openwebui.matejhome.com', title: 'OpenWebUI' },
-            // TODO: Reactivete: { url: 'https://openwebui-private.matejhome.com', title: 'OpenWebUI - Private' },
+            { url: 'https://openwebui.matejhome.com', title: 'Main' },
+            // TODO: Reactivete: { url: 'https://openwebui-private.matejhome.com', title: 'Private' },
         ],
     },
     pihole: {
         title: 'PiHole',
         instances: [
-            { url: 'https://pihole-1-blackhole.matejhome.com', title: 'PiHole 1 Blackhole' },
-            { url: 'https://pihole-1-primary.matejhome.com', title: 'PiHole 1 Primary' },
-            { url: 'https://pihole-1-secondary.matejhome.com', title: 'PiHole 1 Secondary' },
-            { url: 'https://pihole-2-blackhole.matejhome.com', title: 'PiHole 2 Blackhole' },
-            { url: 'https://pihole-2-primary.matejhome.com', title: 'PiHole 2 Primary' },
-            { url: 'https://pihole-2-secondary.matejhome.com', title: 'PiHole 2 Secondary' },
+            { url: 'https://pihole-1-blackhole.matejhome.com', title: 'Blackhole' },
+            { url: 'https://pihole-1-primary.matejhome.com', title: 'Primary' },
+            { url: 'https://pihole-1-secondary.matejhome.com', title: 'Secondary' },
+            { url: 'https://pihole-2-blackhole.matejhome.com', title: 'Blackhole' },
+            { url: 'https://pihole-2-primary.matejhome.com', title: 'Primary' },
+            { url: 'https://pihole-2-secondary.matejhome.com', title: 'Secondary' },
         ],
     },
     prometheus: {
         title: 'Prometheus',
         instances: [
-            { url: 'https://prometheus.matejhome.com', title: 'Prometheus' },
+            { url: 'https://prometheus.matejhome.com', title: 'Main' },
         ],
     },
     renovatebot: {
         title: 'Renovatebot',
         instances: [
-            { url: 'https://renovatebot.matejhome.com', title: 'Renovatebot' },
+            { url: 'https://renovatebot.matejhome.com', title: 'Main' },
         ],
     },
     samba: {
         title: 'SMB',
         instances: [
-            { url: 'smb://samba-data.matejhome.com', title: 'SMB (data)' },
+            { url: 'smb://samba-data.matejhome.com', title: 'Main' },
         ],
     },
     smtp4dev: {
         title: 'Smtp4dev',
         instances: [
-            { url: 'https://smtp4dev.matejhome.com', title: 'Smtp4dev' },
+            { url: 'https://smtp4dev.matejhome.com', title: 'Main' },
         ],
     },
     speedtesttracker: {
         title: 'SpeedtestTracker',
         instances: [
-            { url: 'https://speedtesttracker.matejhome.com', title: 'SpeedtestTracker' },
+            { url: 'https://speedtesttracker.matejhome.com', title: 'Main' },
         ],
     },
     tvheadend: {
         title: 'Tvheadend',
         instances: [
-            { url: 'https://tvheadend.matejhome.com', title: 'Tvheadend' },
+            { url: 'https://tvheadend.matejhome.com', title: 'Main' },
         ],
     },
     unbound: {
         title: 'Unbound',
         instances: [
-            { url: 'https://unbound-1-blackhole.matejhome.com', title: 'Unbound 1 Blackhole' },
-            { url: 'https://unbound-1-default.matejhome.com', title: 'Unbound 1 Default' },
-            { url: 'https://unbound-1-guests.matejhome.com', title: 'Unbound 1 Guests' },
-            { url: 'https://unbound-1-internal.matejhome.com', title: 'Unbound 1 Internal' },
-            { url: 'https://unbound-1-iot.matejhome.com', title: 'Unbound 1 IoT' },
-            { url: 'https://unbound-1-matej.matejhome.com', title: 'Unbound 1 Matej' },
-            { url: 'https://unbound-1-monika.matejhome.com', title: 'Unbound 1 Monika' },
-            { url: 'https://unbound-2-blackhole.matejhome.com', title: 'Unbound 2 Blackhole' },
-            { url: 'https://unbound-2-default.matejhome.com', title: 'Unbound 2 Default' },
-            { url: 'https://unbound-2-guests.matejhome.com', title: 'Unbound 2 Guests' },
-            { url: 'https://unbound-2-internal.matejhome.com', title: 'Unbound 2 Internal' },
-            { url: 'https://unbound-2-iot.matejhome.com', title: 'Unbound 2 IoT' },
-            { url: 'https://unbound-2-matej.matejhome.com', title: 'Unbound 2 Matej' },
-            { url: 'https://unbound-2-monika.matejhome.com', title: 'Unbound 2 Monika' },
+            { url: 'https://unbound-1-blackhole.matejhome.com', title: '1 Blackhole' },
+            { url: 'https://unbound-1-default.matejhome.com', title: '1 Default' },
+            { url: 'https://unbound-1-guests.matejhome.com', title: '1 Guests' },
+            { url: 'https://unbound-1-internal.matejhome.com', title: '1 Internal' },
+            { url: 'https://unbound-1-iot.matejhome.com', title: '1 IoT' },
+            { url: 'https://unbound-1-matej.matejhome.com', title: '1 Matej' },
+            { url: 'https://unbound-1-monika.matejhome.com', title: '1 Monika' },
+            { url: 'https://unbound-2-blackhole.matejhome.com', title: '2 Blackhole' },
+            { url: 'https://unbound-2-default.matejhome.com', title: '2 Default' },
+            { url: 'https://unbound-2-guests.matejhome.com', title: '2 Guests' },
+            { url: 'https://unbound-2-internal.matejhome.com', title: '2 Internal' },
+            { url: 'https://unbound-2-iot.matejhome.com', title: '2 IoT' },
+            { url: 'https://unbound-2-matej.matejhome.com', title: '2 Matej' },
+            { url: 'https://unbound-2-monika.matejhome.com', title: '2 Monika' },
         ]
     },
     unificontroller: {
         title: 'UniFiController',
         instances: [
-            { url: 'https://unificontroller.matejhome.com', title: 'UniFiController' },
+            { url: 'https://unificontroller.matejhome.com', title: 'Main' },
         ],
     },
     uptimekuma: {
         title: 'UptimeKuma',
         instances: [
-            { url: 'https://uptimekuma-1.matejhome.com', title: 'UptimeKuma 1' },
-            { url: 'https://uptimekuma-2.matejhome.com', title: 'UptimeKuma 2' },
+            { url: 'https://uptimekuma-1.matejhome.com', title: '1' },
+            { url: 'https://uptimekuma-2.matejhome.com', title: '2' },
         ],
     },
     vaultwarden: {
         title: 'Vaultwarden',
         instances: [
-            { url: 'https://vaultwarden.matejhome.com', title: 'Vaultwarden' },
+            { url: 'https://vaultwarden.matejhome.com', title: 'Main' },
         ],
     },
     vikunja: {
         title: 'Vikunja',
         instances: [
-            { url: 'https://vikunja.matejhome.com', title: 'Vikunja' },
+            { url: 'https://vikunja.matejhome.com', title: 'Main' },
         ],
     },
 };
