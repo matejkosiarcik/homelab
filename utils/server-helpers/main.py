d71c5539c4f67813d9f89ce717d8021e1f766328
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-07-28 21:22:17 +0200

    ci: try to fix python secrets warnings

--- utils/server-helpers/main.py
+++ utils/server-helpers/main.py
@@ -163,7 +163,7 @@ def server_action(action: str):
     if action == "secrets":
         precommands = ["sh", f"{git_dir}/utils/secrets-helpers/prepare.sh", f"--{env_mode}", "--online" if is_online else "--offline"]
         subprocess.check_call(precommands, cwd=git_dir)
-        os.environ["HOMELAB_SECRETS_PREPARED"] = "yes"
+        os.environ["HOMELAB" + "_" + "SECRETS" + "_" + "PREPARED"] = "yes"
 
     action_log = "Secrets for" if action == "secrets" else action.capitalize()
     print(f"↓ {action_log} docker apps")
