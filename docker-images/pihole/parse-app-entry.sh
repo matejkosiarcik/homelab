e8fc225c272c4f058e78959625b87a7d9db10a9b
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-07-31 17:13:56 +0200

    feat: generate pihole domain entries from each app automatically

---
+++ docker-images/pihole/parse-app-entry.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+apppath="$(realpath "$1")"
+output="$(realpath "$2")"
+currdir="$(realpath "$PWD")"
+
+cd "$apppath"
+
+appname="$(basename "$apppath")"
+
+domain="$(yq --raw-output .network.domain ./config/config.yml)"
+if [ "$domain" = '' ] || [ "$domain" = 'null' ] || [ "$domain" = 'undefined' ]; then
+    domain="$appname.matejhome.com"
+fi
+
+ip="$(yq --raw-output .network.ip ./config/config.yml)"
+if [ "$ip" = '' ] || [ "$ip" = 'null' ] || [ "$ip" = 'undefined' ]; then
+    ip="0.0.0.0"
+fi
+
+printf '%s %s\n' "$ip" "$domain" >>"$output"
+
+cd "$currdir"
---
+++ docs/other/IPs.md
@@ -0,0 +1,8 @@
+# IP assignment
+
+On each server, the following rules should be followed:
+
+- `10.1.x.0-9` - unbound
+- `10.1.x.10-19` - pihole
+- `10.1.x.20-29` - monitoring services
+- `10.1.x.30-255` - rest
