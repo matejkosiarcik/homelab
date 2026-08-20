6a8c6077004652238a62eddd03155adfa48bc8cc
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-08-20 22:02:44 +0200

    feat: add empty.zim placeholder for kiwix apps

--- docker-images/kiwix/entrypoint.sh
+++ docker-images/kiwix/entrypoint.sh
@@ -2,9 +2,13 @@
 set -euf
 
 zim_files_count="$(find '/data' -mindepth 1 -maxdepth 1 -type f -name '*.zim' | wc -l)"
+zim_files_list="$(mktemp)"
+
 if [ "$zim_files_count" -eq '0' ]; then
-    printf 'No "*.zim" files found\n' >&2
-    exit 1
+    printf 'No "*.zim" files found, using empty placeholder.\n' >&2
+    printf '/homelab/default-data/empty.zim\0' >>"$zim_files_list"
 fi
 
-find '/data' -mindepth 1 -maxdepth 1 -type f -name '*.zim' -print0 | xargs -0 kiwix-serve --port=8080
+find '/data' -mindepth 1 -maxdepth 1 -type f -name '*.zim' -print0 >>"$zim_files_list"
+
+xargs -0 kiwix-serve --port=8080 <"$zim_files_list"
