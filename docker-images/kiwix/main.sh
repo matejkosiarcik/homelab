5e46fda0ebda98c9b5c50c6a12e9f38c1f5174ca
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-08-22 23:37:15 +0200

    chore: fix shell lint errors

--- docker-images/kiwix/main.sh
+++ docker-images/kiwix/main.sh
@@ -20,31 +20,31 @@ zim_files_list_new_shasum_file="$(mktemp)"
 
 # Get list of files and filter only valid ones
 find '/data' -mindepth 1 -maxdepth 1 -type f -name '*.zim' | sort >"$zim_files_list_new_all_file"
-cat "$zim_files_list_new_all_file" | while read -r file; do
+while read -r file; do
     # NOTE: This is the source of the script being slow, the zimcheck call
     # It could be optimized with caching the results for each file (if it is valid) and when it doesn't change (same modified date, same filesize), then we wouldn't have to call it and automatically consider the file valid
     if [ "$is_initial_run" -eq '1' ] || zimcheck --checksum "$file" >/dev/null 2>&1; then
         printf '%s\n' "$file" >>"$zim_files_list_new_valid_file"
     fi
-done
+done <"$zim_files_list_new_all_file"
 
 # Get file properties for comparisons later
-cat "$zim_files_list_new_valid_file" | while read -r file; do
+while read -r file; do
     (printf 'File: %s, Modified at: %s, Size: %s\n' "$file" "$(date -r "$file" || true)" "$(stat -c '%s bytes' -- "$file" || true)") >>"$zim_files_list_new_shasum_file"
-done
+done <"$zim_files_list_new_valid_file"
 new_shasum="$(sha256sum <"$zim_files_list_new_shasum_file")"
 
 # If the shasum is the same, abort this run
 if [ "$new_shasum" = "$last_shasum" ]; then
     printf 'Server already started and no valid changes in ZIM files detected\n' >&2
-    return;
+    return
 fi
 
 # Save shasum for next run
 printf '%s\n' "$new_shasum" >"$last_shasum_file"
 
 # Use placeholder if there are no real ZIM files
-zim_files_count="$(cat "$zim_files_list_new_valid_file" | wc -l)"
+zim_files_count="$(wc -l <"$zim_files_list_new_valid_file")"
 if [ "$zim_files_count" -eq '0' ]; then
     printf 'No ZIM files found, using placeholder\n' >&2
     printf '/homelab/default-data/empty.zim\n' >>"$zim_files_list_new_valid_file"
@@ -57,7 +57,7 @@ else
     kill "$(cat "$kiwix_pid_file")"
 fi
 
-# shellcheck disable=SC2086
+# shellcheck disable=SC2046
 kiwix-serve --port=8080 $(cat "$zim_files_list_new_valid_file") &
 kiwix_pid="$!"
 printf '%s\n' "$kiwix_pid" >"$kiwix_pid_file"
