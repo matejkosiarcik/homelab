f73e22dc77201c9f99d2a128c0e5c5e4fb9cf929
Matej Košiarčik <matej.kosiarcik@gmail.com>
2026-09-17 15:03:35 +0200

    chore: ignore shellcheck lint error in special script

--- docker-images/speedtesttracker/cron-run.sh
+++ docker-images/speedtesttracker/cron-run.sh
@@ -3,6 +3,7 @@
 
 # This file replaces default "/etc/s6-overlay/s6-rc.d/svc-cron/run" to run supercronic instead of builtin cron daemon because of user permission conflicts
 
+# shellcheck disable=SC2093
 exec \
     s6-setuidgid abc \
     env HOME=/home/homelab USER=abc LOGNAME=abc SHELL=/bin/sh \
